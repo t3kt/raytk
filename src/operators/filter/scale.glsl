620f1c3060b8f996f9a580dc898d430224868b0a
tekt <tekt@optexture.com>
2020-08-30 13:54:45 -0700

    added type macros to support conditional variations

--- src/operators/filter/scale.glsl
+++ src/operators/filter/scale.glsl
@@ -1,5 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p / THIS_Scale, ctx);
+	#ifdef THIS_RETURN_TYPE_float
+	res /= length(THIS_Scale);
+	#else
 	res.x /= length(THIS_Scale);
+	#endif
 	return res;
 }
