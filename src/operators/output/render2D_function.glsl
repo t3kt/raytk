44266de8fda5d673634c49aa52f7dbdd758cf096
tekt <tekt@optexture.com>
2020-08-30 13:02:22 -0700

    finished the 2d renderer

--- src/operators/output/render2D_function.glsl
+++ src/operators/output/render2D_function.glsl
@@ -1,10 +1,14 @@
 #define thismap inputOp1
 
 vec4 getColor(Sdf res) {
-	vec4 color = THIS_Backgroundcolor;
+	vec4 color = mix(
+		mix(THIS_Outsidecolor1, THIS_Outsidecolor2, 0.5 + 0.5*cos(res.x/THIS_Outsideperiod)),
+		mix(THIS_Insidecolor1, THIS_Insidecolor2, 0.5 + 0.5*cos(res.x/THIS_Insideperiod)),
+		step(res.x, 0));
 	if (THIS_Enableedge > 0) {
 		// TODO: fully implement edge blending / thickness
-		color = mix(color, THIS_Edgecolor, 1.0-smoothstep(0.0, THIS_Edgethickness/2.0, abs(res.x)));
+		color = mix(color, THIS_Edgecolor,
+			1.0-smoothstep(THIS_Edgethickness-THIS_Edgeblending*0.5, THIS_Edgethickness+THIS_Edgeblending*0.5, abs(res.x)));
 	}
 	return color;
 }
