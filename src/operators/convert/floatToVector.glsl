f9f8bdcb91c4bcaf5055fa5d5d64f2664048a935
tekt <tekt@optexture.com>
2020-09-06 15:18:28 -0700

    field improvements and visualizer

--- src/operators/convert/floatToVector.glsl
+++ src/operators/convert/floatToVector.glsl
@@ -1,8 +1,23 @@
-#ifdef THIS_ALL_INPUT
-#define thismap(p, ctx) vec4(inputOp1(p, ctx))
+#ifdef THIS_USE_SINGLE_SOURCE
+#ifdef THIS_INPUT_OP
+#define thismap(p, ctx) vec4(THIS_INPUT_OP(p, ctx))
+#else
+#define thismap(p, ctx) vec4(THIS_SOURCE)
+#endif
 #else
 vec4 thismap(CoordT p, ContextT ctx) {
-	float input = inputOp1(p, ctx);
+	#ifdef THIS_USE_INPUT_1
+	float input1 = inputOp1(p, ctx);
+	#endif
+	#ifdef THIS_USE_INPUT_2
+	float input2 = inputOp2(p, ctx);
+	#endif
+	#ifdef THIS_USE_INPUT_3
+	float input3 = inputOp3(p, ctx);
+	#endif
+	#ifdef THIS_USE_INPUT_4
+	float input4 = inputOp4(p, ctx);
+	#endif
 	return vec4(
 		THIS_SOURCE_X,
 		THIS_SOURCE_Y,
