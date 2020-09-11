bf04b83e8238ad1813ea84eb80a96f8d27d2bcd0
tekt <tekt@optexture.com>
2020-09-10 21:31:51 -0700

    added box sdf

--- src/operators/sdf/boxSdf.glsl
+++ src/operators/sdf/boxSdf.glsl
@@ -1,3 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return createSdf(fBox(p - THIS_Translate, THIS_Scale));
+#ifdef THIS_INF_PLANE
+	return createSdf(THIS_BOX_FUNC(
+		p.THIS_INF_PLANE - vec3(THIS_Translate).THIS_INF_PLANE,
+		vec3(THIS_Scale).THIS_INF_PLANE));
+#else
+	return createSdf(THIS_BOX_FUNC(p - THIS_Translate, THIS_Scale));
+#endif
 }
