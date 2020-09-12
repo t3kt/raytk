32ad731bb41c85fbcb6a7324871c7808c7cde791
tekt <tekt@optexture.com>
2020-09-11 23:42:35 -0700

    test cases

--- src/operators/filter/rotate.glsl
+++ src/operators/filter/rotate.glsl
@@ -3,15 +3,15 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	pR(p, radians(THIS_ROTATE));
 #endif
 #ifdef THIS_USE_AXIS
-	p *= TDRotateOnAxis(THIS_ROTATE, THIS_Axis);
+	p *= TDRotateOnAxis(radians(THIS_ROTATE), THIS_Axis);
 #endif
 #ifdef THIS_USE_EULER
 	#ifdef THIS_USE_FIELD
 	vec4 fieldVal = inputOp2(p, ctx);
 	#endif
-	p *= TDRotateOnAxis(THIS_ROT_1, THIS_AXIS_1)
-		* TDRotateOnAxis(THIS_ROT_2, THIS_AXIS_2)
-		* TDRotateOnAxis(THIS_ROT_3, THIS_AXIS_3);
+	p *= TDRotateOnAxis(radians(THIS_ROT_1), THIS_AXIS_1)
+		* TDRotateOnAxis(radians(THIS_ROT_2), THIS_AXIS_2)
+		* TDRotateOnAxis(radians(THIS_ROT_3), THIS_AXIS_3);
 #endif
 	return inputOp1(p, ctx);
 }
