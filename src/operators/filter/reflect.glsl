66b014b45841b1528126ee62c2c8ee90dc65eb3e
tekt <tekt@optexture.com>
2020-09-11 20:26:24 -0700

    testing out the reflect instancing

--- src/operators/filter/reflect.glsl
+++ src/operators/filter/reflect.glsl
@@ -1,10 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Planenormal * THIS_Shift;
+	float i = pReflect(p, THIS_Planenormal, THIS_Offset);
 #ifdef THIS_EXPOSE_ITERATION
-	ctx.iteration.x = pReflect(p, THIS_Planenormal, THIS_Offset);
+	ctx.iteration.x = (i + 1.) * .5;
 	ctx.iteration.y = 2;
-#else
-	pReflect(p, THIS_Planenormal, THIS_Offset);
 #endif
 	return inputOp1(p, ctx);
 }
