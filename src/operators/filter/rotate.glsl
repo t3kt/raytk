c2db7dcd4567ed4b543cf19a2466ac8a1b517c81
tekt <tekt@optexture.com>
2020-09-11 19:08:05 -0700

    field support in rotate and translate
    not yet tested

--- src/operators/filter/rotate.glsl
+++ src/operators/filter/rotate.glsl
@@ -1,11 +1,14 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 #ifdef THIS_USE_2D
-	pR(p, radians(THIS_Rotate));
+	pR(p, radians(THIS_ROTATE));
 #endif
 #ifdef THIS_USE_AXIS
-	p *= TDRotateOnAxis(THIS_Rotate, THIS_Axis);
+	p *= TDRotateOnAxis(THIS_ROTATE, THIS_Axis);
 #endif
 #ifdef THIS_USE_EULER
+	#ifdef THIS_USE_FIELD
+	vec4 fieldVal = inputOp2(p, ctx);
+	#endif
 	p *= TDRotateOnAxis(THIS_ROT_1, THIS_AXIS_1)
 		* TDRotateOnAxis(THIS_ROT_2, THIS_AXIS_2)
 		* TDRotateOnAxis(THIS_ROT_3, THIS_AXIS_3);
--- src/operators/filter/translate.glsl
+++ src/operators/filter/translate.glsl
@@ -1,3 +1,3 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return inputOp1(p - THIS_Translate, ctx);
+	return inputOp1(p - THIS_GET_TRANSLATE(), ctx);
 }
