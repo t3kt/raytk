5fcfa489fa95c355c8114b59751b2ea54d1592a9
tekt <tekt@optexture.com>
2020-09-13 12:41:04 -0700

    texture support working!

---
+++ src/components/outputCommon.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+def updateTextureInputs(
+		comp: 'COMP',
+		glslTop: 'TOP',
+		textureTable: 'DAT',
+		fixedInputs: 'List[TOP]' = None):
+	for conn in glslTop.inputConnectors:
+		conn.disconnect()
+	if fixedInputs:
+		for inputTop in fixedInputs:
+			if inputTop:
+				inputTop.outputConnectors[0].connect(glslTop)
+	parent().clearScriptErrors(error='texerr*')
+	for i in range(textureTable.numRows):
+		texSelect = comp.op(f'sel_texture_{i}')
+		if not texSelect:
+			parent().addScriptError(f'texerr: Too many texture sources (failed on #{i})')
+			return
+		texSelect.outputConnectors[0].connect(glslTop)
--- src/operators/field/textureField.glsl
+++ src/operators/field/textureField.glsl
@@ -1,5 +1,5 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec2 uv = (p.PLANE + THIS_Translate) / THIS_Scale;
+	vec2 uv = (p.THIS_PLANE + THIS_Translate) / THIS_Scale;
 	vec4 value = texture(THIS_texture, uv);
 	#ifdef THIS_RETURN_TYPE_float
 	return value.x;
