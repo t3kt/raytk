c6b28b0c2d6e186fa11664b9780fb8a138f84742
tekt <tekt@optexture.com>
2020-09-16 18:05:40 -0700

    new output buffer system for point map render

--- src/operators/output/pointMapRender_body.glsl
+++ src/operators/output/pointMapRender_body.glsl
@@ -1,13 +1,9 @@
 #ifdef THIS_RETURN_TYPE_Sdf
-layout (location = 0) out vec4 sdfOut;
-layout (location = 1) out vec4 colorOut;
-
 Sdf map(vec3 p) {
 	Sdf res = thismap(p, createDefaultContext());
 	res.x *= 0.5;
 	return res;
 }
-
 
 vec4 getColor(Sdf res, vec3 p) {
 	int m = int(res.material);
@@ -28,34 +24,46 @@ void main() {
 	vec4 posAndExists = texture(sTD2DInputs[0], fragCoord);
 
 	if (posAndExists.a == 0) {
+		#ifdef OUTPUT_SDF
 		sdfOut = vec4(0);
+		#endif
+		#ifdef OUTPUT_COLOR
+		colorOut = vec4(0);
+		#endif
 		return;
 	}
 	vec3 p = posAndExists.xyz;
 	Sdf res = map(p);
 
+	#ifdef OUTPUT_COLOR
 	colorOut = getColor(res, p);
+	#endif
 
+	#ifdef OUTPUT_SDF
 	sdfOut = vec4(
 		res.x,
 		res.material,
 		res.material2,
 		res.interpolant);
+	#endif
 }
 #endif
-#ifdef THIS_RETURN_TYPE_vec4
-layout (location = 0) out vec4 valueOut;
 
+#ifdef THIS_RETURN_TYPE_vec4
 void main() {
 	vec2 resolution = uTDOutputInfo.res.zw;
 	vec2 fragCoord = vUV.st;
 	vec4 posAndExists = texture(sTD2DInputs[0], fragCoord);
 
 	if (posAndExists.a == 0) {
+		#ifdef OUTPUT_VALUE
 		valueOut = vec4(0);
+		#endif
 		return;
 	}
 	vec3 p = posAndExists.xyz;
+	#ifdef OUTPUT_VALUE
 	valueOut = thismap(p, createDefaultContext());
+	#endif
 }
 #endif
