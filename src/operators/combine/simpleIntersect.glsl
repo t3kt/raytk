f605484a3d596e4a8b3af4cb89ec38485a928bf0
tekt <tekt@optexture.com>
2020-09-22 22:41:51 -0700

    up to 4 inputs for simpleIntersect

--- src/operators/combine/simpleIntersect.glsl
+++ src/operators/combine/simpleIntersect.glsl
@@ -1,3 +1,21 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return opSimpleIntersect(inputOp1(p, ctx), inputOp2(p, ctx));
+	#if THIS_INPUT_COUNT == 0
+		#ifdef THIS_RETURN_TYPE_Sdf
+			return createSdf(0);
+		#else
+			return ReturnT(0);
+		#endif
+	#else
+		ReturnT res = THIS_INPUT_1(p, ctx);
+		#if THIS_INPUT_COUNT > 1
+			res = opSimpleIntersect(res, THIS_INPUT_2(p, ctx));
+		#endif
+		#if THIS_INPUT_COUNT > 2
+			res = opSimpleIntersect(res, THIS_INPUT_3(p, ctx));
+		#endif
+		#if THIS_INPUT_COUNT > 3
+			res = opSimpleIntersect(res, THIS_INPUT_4(p, ctx));
+		#endif
+		return res;
+	#endif
 }
