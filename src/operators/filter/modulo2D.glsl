50110f30cd56277e948675f2859a429e2fead2a8
tekt <tekt@optexture.com>
2020-09-28 01:02:18 -0700

    file sync tags in all ops

--- src/operators/filter/modulo2D.glsl
+++ src/operators/filter/modulo2D.glsl
@@ -1,7 +1,7 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec2 q = p.THIS_AXIS_PLANE + THIS_Shift;
+	vec2 q = p.THIS_PLANE + THIS_Shift;
 	vec2 cell = THIS_MIRROR_FUNC(q, THIS_Size);
-	p.THIS_AXIS_PLANE = q - THIS_Offset;
+	p.THIS_PLANE = q - THIS_Offset;
 	#ifdef THIS_ITERATE_CELLS
 	ctx.iteration.x = quadrantIndex(ivec2(cell));
 	ctx.iteration.y = 4;
