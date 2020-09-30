0bd9397fa2ccac05712af5616a6f0dc8ed2216da
tekt <tekt@optexture.com>
2020-09-30 12:10:20 -0700

    fix a quantize value bug

--- src/operators/filter/quantizeValue.glsl
+++ src/operators/filter/quantizeValue.glsl
@@ -1,8 +1,10 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p, ctx);
-	#ifdef THIS_RETURN_TYPE_Sdf
+	#if defined(THIS_RETURN_TYPE_Sdf)
 	res.x = quantize(res.x, THIS_Size1, THIS_Offset1, THIS_Smoothing1);
 	return res;
+	#elif defined(THIS_RETURN_TYPE_float)
+	return quantize(res, THIS_Size1, THIS_Offset1, THIS_Smoothing1);
 	#else
 	return quantize(res, THIS_Size, THIS_Offset, THIS_Smoothing);
 	#endif
