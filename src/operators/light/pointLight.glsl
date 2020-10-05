657aa9226bdb8c2fae92251fbd6d9f20c0b67d21
tekt <tekt@optexture.com>
2020-10-04 17:42:28 -0700

    distance attentuation for pointLight

--- src/operators/light/pointLight.glsl
+++ src/operators/light/pointLight.glsl
@@ -2,6 +2,16 @@ Light thismap(vec3 p, LightContext ctx) {
 	Light light;
 	light.pos = THIS_Position;
 	light.color = THIS_Color;
+	#ifdef THIS_ATTENUATED
+	float d = length(p - light.pos);
+	float start = THIS_Attenuationstart;
+	float end = THIS_Attenuationend;
+	if (d > end) {
+		light.color = vec3(0);
+	} else if (d > start) {
+		light.color *= (1 - smoothstep(start, end, d));
+	}
+	#endif
 	return light;
 }
 
