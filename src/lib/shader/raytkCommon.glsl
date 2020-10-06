70db9a230ec6430ae6fe440e5734d1aa41715c3c
tekt <tekt@optexture.com>
2020-10-05 18:30:04 -0700

    implemented mirroring for modPolar

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -235,3 +235,23 @@ float onion(float d, float thickness) {
 vec4 onion(vec4 d, float thickness) {
 	return abs(d)-thickness;
 }
+
+float pModPolarMirror(inout vec2 p, float repetitions) {
+	float angle = 2*PI/repetitions;
+	float a = atan(p.y, p.x) + angle/2.;
+	float r = length(p);
+	float c = floor(a/angle);
+//	a = mod(a,angle) - angle/2.;
+	float a1 = mod(a, angle * 2);
+	if (a1 >= angle) {
+		a1 = angle - a1;
+	}
+	a1 = mod(a1, angle);
+	a = a1 - angle/2.;
+
+	p = vec2(cos(a), sin(a))*r;
+	// For an odd number of repetitions, fix cell index of the cell in -x direction
+	// (cell index would be e.g. -5 and 5 in the two halves of the cell):
+	if (abs(c) >= (repetitions/2)) c = abs(c);
+	return c;
+}
