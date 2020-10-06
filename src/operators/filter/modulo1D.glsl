50234d1d5127ba33b69de9655e4518b8874cf80d
tekt <tekt@optexture.com>
2020-10-05 21:47:06 -0700

    don't clamp cell to 0..1 for modulo1d

--- src/operators/filter/modulo1D.glsl
+++ src/operators/filter/modulo1D.glsl
@@ -3,7 +3,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float cell = THIS_MIRROR_FUNC(q, THIS_Size);
 	p.THIS_AXIS = q - THIS_Offset;
 	#ifdef THIS_ITERATE_CELLS
-	ctx.iteration.x = clamp(cell, 0, 1);
+	ctx.iteration.x = cell;
 	ctx.iteration.y = 2;
 	#endif
 	return inputOp1(p, ctx);
