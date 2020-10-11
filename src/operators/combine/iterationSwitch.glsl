2ea3e2d614d1ab582c8435161c2679ed4f79c7b4
tekt <tekt@optexture.com>
2020-10-10 19:52:29 -0700

    iteration switch fix

--- src/operators/combine/iterationSwitch.glsl
+++ src/operators/combine/iterationSwitch.glsl
@@ -1,11 +1,11 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	float ratio = ctx.iteration.x;
-	if (ratio != 1.0) {
-		#if defined(THIS_EXTEND_clamp)
-		ratio = clamp(ratio, 0, 1);
-		#elif defined(THIS_EXTEND_loop)
+	#if defined(THIS_EXTEND_clamp)
+	ratio = clamp(ratio, 0, 1);
+	#elif defined(THIS_EXTEND_loop)
+	if (fract(ratio) != 0) {
 		ratio = fract(ratio);
-		#endif
 	}
+	#endif
 	return mix(inputOp1(p, ctx), inputOp2(p, ctx), ratio);
 }
