1fa00337a2c836b72e7dab2633a15e835cfa56bd
tekt <tekt@optexture.com>
2020-10-10 19:50:56 -0700

    loop support for iteration switch

--- src/operators/combine/iterationSwitch.glsl
+++ src/operators/combine/iterationSwitch.glsl
@@ -1,4 +1,11 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	float ratio = clamp(ctx.iteration.x, 0, 1);
+	float ratio = ctx.iteration.x;
+	if (ratio != 1.0) {
+		#if defined(THIS_EXTEND_clamp)
+		ratio = clamp(ratio, 0, 1);
+		#elif defined(THIS_EXTEND_loop)
+		ratio = fract(ratio);
+		#endif
+	}
 	return mix(inputOp1(p, ctx), inputOp2(p, ctx), ratio);
 }
