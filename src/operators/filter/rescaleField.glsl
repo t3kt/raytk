c10c37fc1675359a5ff31a8c63b34bd3386b7140
tekt <tekt@optexture.com>
2020-10-12 19:23:25 -0700

    fixing some parameter issues in several field ops

--- src/operators/filter/rescaleField.glsl
+++ src/operators/filter/rescaleField.glsl
@@ -1,11 +1,14 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
 #if defined(THIS_RETURN_TYPE_vec4)
-#define thismap(p, ctx)  mapRange(inputOp1(p, ctx), THIS_Inputlow, THIS_Inputhigh, THIS_Outputlow, THIS_Outputhigh)
+	vec4 val = inputOp1(p, ctx);
+	return mapRange(val, THIS_Inputlow, THIS_Inputhigh, THIS_Outputlow, THIS_Outputhigh);
+	return val;
 #elif defined(THIS_RETURN_TYPE_Sdf)
-Sdf thismap(CoordT p, ContextT ctx) {
 	Sdf res = inputOp1(p, ctx);
 	res.x = mapRange(THIS_Inputlow1, THIS_Inputhigh1, THIS_Outputlow1, THIS_Outputhigh1);
 	return res;
-}
-#else
-#define thismap(p, ctx)  mapRange(inputOp1(p, ctx), THIS_Inputlow1, THIS_Inputhigh1, THIS_Outputlow1, THIS_Outputhigh1)
+#elif defined(THIS_RETURN_TYPE_float)
+	float val = inputOp1(p, ctx);
+	return mapRange(val, THIS_Inputlow1, THIS_Inputhigh1, THIS_Outputlow1, THIS_Outputhigh1);
 #endif
+}
