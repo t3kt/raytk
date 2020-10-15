d688432f10f4baa36e3c703149704ebc5ceabee4
tekt <tekt@optexture.com>
2020-10-14 20:28:35 -0700

    added field material

---
+++ src/operators/material/fieldMat.glsl
@@ -0,0 +1,9 @@
+Sdf thismap(CoordT p, ContextT ctx) {
+	Sdf res = inputOp1(p, ctx);
+	res.material = THISMAT;
+	return res;
+}
+
+vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
+	return inputOp2(p, matCtx).rgb;
+}
