225abaeffdcfde8bd162ead026da3e191add0e3f
tekt <tekt@optexture.com>
2020-10-22 19:39:21 -0700

    added extrude

---
+++ src/operators/convert/extrude.glsl
@@ -0,0 +1,6 @@
+ReturnT thismap(vec3 p, ContextT ctx) {
+	Sdf res = inputOp1(p.THIS_PLANE, ctx);
+	vec2 w = vec2(res.x, abs(p.THIS_AXIS - THIS_Offset) - THIS_Height);
+	res.x = min(max(w.x,w.y), 0.0) + length(max(w, 0.0));
+	return res;
+}
