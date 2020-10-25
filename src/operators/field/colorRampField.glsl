675b8a17afe0ed5039d45b4f00fd58c6e4e17996
tekt <tekt@optexture.com>
2020-10-24 19:28:42 -0700

    cleanup in colorRampField

--- src/operators/field/colorRampField.glsl
+++ src/operators/field/colorRampField.glsl
@@ -3,9 +3,5 @@ vec4 thismap(CoordT p, ContextT ctx) {
 	#ifdef THIS_CLAMP_RANGE
 	amt = clamp(amt, 0., 1.);
 	#endif
-	vec4 color1 = THIS_Color1;
-	vec4 color2 = THIS_Color2;
-	return mapRange(
-		vec4(amt), vec4(0.), vec4(1.), color1, color2);
-//	return mix(THIS_Color1, THIS_Color2, amt);
+	return mix(THIS_Color1, THIS_Color2, amt);
 }
