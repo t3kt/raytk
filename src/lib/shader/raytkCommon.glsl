533820222168c2db316385041fe7ffded467a5c2
tekt <tekt@optexture.com>
2020-10-29 18:54:53 -0700

    fix const bug in raytkCommon

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -281,8 +281,8 @@ float ndot(vec2 a, vec2 b ) { return a.x*b.x - a.y*b.y; }
 float remapAlmostIdentity( float x, float m, float n )
 {
 	if( x>m ) return x;
-	const float a = 2.0*n - m;
-	const float b = 2.0*m - 3.0*n;
-	const float t = x/m;
+	float a = 2.0*n - m;
+	float b = 2.0*m - 3.0*n;
+	float t = x/m;
 	return (a*t + b)*t*t + n;
 }
