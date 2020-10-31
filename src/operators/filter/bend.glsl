1d2953523715e3828d03fbff23157012cb489e5e
tekt <tekt@optexture.com>
2020-10-30 18:14:30 -0700

    added twist and fixed bend

--- src/operators/filter/bend.glsl
+++ src/operators/filter/bend.glsl
@@ -1,8 +1,8 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec3 q = p.THIS_AXIS_PLANE_SWIZZLE;
+	vec3 q = p.THIS_SWIZZLE;
 	q.x += THIS_Shift;
 	q = opCheapBendPos(q, THIS_Amount);
 	q.x -= THIS_Shift;
-	p.THIS_AXIS_PLANE_SWIZZLE = q;
+	p.THIS_SWIZZLE = q;
 	return inputOp1(p, ctx);
 }
---
+++ src/operators/filter/twist.glsl
@@ -0,0 +1,8 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	vec3 q = p.THIS_SWIZZLE;
+	q.x += THIS_Shift;
+	q = opTwistPos(q, THIS_Amount);
+	q.x -= THIS_Shift;
+	p.THIS_SWIZZLE = q;
+	return inputOp1(p, ctx);
+}
