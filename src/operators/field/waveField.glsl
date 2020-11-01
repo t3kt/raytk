94639577fe38e75df490f77f478f655101a0253b
tekt <tekt@optexture.com>
2020-11-01 13:03:11 -0800

    got waveField working

--- src/operators/field/waveField.glsl
+++ src/operators/field/waveField.glsl
@@ -1,9 +1,18 @@
-float thismap(CoordT p, ContextT ctx) {
-	#ifdef THIS_USE_INPUT
-	float q = inputOp1(p, ctx);
+ReturnT thismap(CoordT p, ContextT ctx) {
+	#if defined(THIS_USE_INPUT)
+		#ifdef THIS_RETURN_TYPE_Sdf
+		Sdf res = inputOp1(p, ctx);
+		float val = THIS_FUNC((res.x / THIS_Period) + THIS_Phase);
+		res.x = (val * THIS_Amplitude) + THIS_Offset;
+		return res;
+		#else
+		ReturnT q = inputOp1(p, ctx);
+		#endif
+	#elif defined(THIS_COORD_TYPE_float)
+		float q = p;
 	#else
-	float q = p.THIS_AXIS;
+		float q = p.THIS_AXIS;
 	#endif
-	float val = THIS_FUNC((p / THIS_Period) + THIS_Phase);
+	ReturnT val = THIS_FUNC((q / THIS_Period) + THIS_Phase);
 	return (val * THIS_Amplitude) + THIS_Offset;
 }
