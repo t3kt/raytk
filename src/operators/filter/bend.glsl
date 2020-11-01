9b26bd6fb3d074464a43cd19da29caa892e212d6
tekt <tekt@optexture.com>
2020-10-31 19:36:04 -0700

    added field support to bend, and some more coord type support elsewhere

--- src/operators/filter/bend.glsl
+++ src/operators/filter/bend.glsl
@@ -1,7 +1,28 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 q = p.THIS_SWIZZLE;
+	#ifdef THIS_USE_FIELD
+		#if defined(THIS_FIELD_COORD_TYPE_float)
+		float fieldP = q.x;
+		#elif defined(THIS_FIELD_COORD_TYPE_vec2)
+		vec2 fieldP = q.xy;
+		#elif defined(THIS_FIELD_COORD_TYPE_vec3)
+		vec3 fieldP = q;
+		#else
+		#error unsupportedFieldCoordType
+		#endif
+		
+		#if defined(THIS_FIELD_RETURN_TYPE_Sdf)
+		float amt = inputOp2(fieldP, ctx).x;
+		#elif defined(THIS_FIELD_RETURN_TYPE_float)
+		float amt = inputOp2(fieldP, ctx);
+		#else
+		#error unsupportedInputReturnType
+		#endif
+	#else
+	float amt = THIS_Amount;
+	#endif
 	q.x += THIS_Shift;
-	q = opCheapBendPos(q, THIS_Amount);
+	q = opCheapBendPos(q, amt);
 	q.x -= THIS_Shift;
 	p.THIS_SWIZZLE = q;
 	return inputOp1(p, ctx);
