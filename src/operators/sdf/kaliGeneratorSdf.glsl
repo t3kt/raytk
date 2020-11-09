2eb354f95b985f2e78d7170a0dee9452aa3c8740
tekt <tekt@optexture.com>
2020-11-08 20:31:35 -0800

    adding some parameters to kali generator sdf

--- src/operators/sdf/kaliGeneratorSdf.glsl
+++ src/operators/sdf/kaliGeneratorSdf.glsl
@@ -5,13 +5,14 @@ ReturnT thismap(vec3 p, ContextT ctx) {
 	p.xz = abs (0.5 - mod ((2./3.) * p.xz, 1.));
 	float scale = 1.;
 	float f;
+	vec3 stepT = THIS_Steptranslate;
 	for (int j = 0; j < int(THIS_Steps); j ++) {
-		p = abs (p) - vec3 (-0.02, 1.98, -0.02);
+		p = abs (p) - stepT;
 		f = 2. / clamp (dot (p, p), 0.4, 1.);
 		p = f * p - vec3 (0.5, 1., 0.4);
 		scale *= f;
 		pR(p.zx, THIS_Fractalangle);
 	}
-	d = (fBox(p, vec3(0.1, 5.0, 0.1)) - 0.1)/scale;
+	d = (fBox(p, THIS_Boxsize) - THIS_Boxround)/scale;
 	return createSdf(d);
 }
