0337fe32f4a1ed27a54e8e0d8cd684380a7ef4da
tekt <tekt@optexture.com>
2020-11-08 20:12:44 -0800

    added customRender
    not yet tested

---
+++ src/operators/output/customRender_defaultBodyTemplate.glsl
@@ -0,0 +1,18 @@
+void main() {
+	vec2 resolution = uTDOutputInfo.res.zw;
+	vec2 fragCoord = vUV.st;//*resolution;
+	fragCoord.x *= uTDOutputInfo.res.z/uTDOutputInfo.res.w;
+	vec2 p = fragCoord*2. - vec2(1.);
+
+	#if defined(THIS_RETURN_TYPE_float)
+	vec4 color = vec4(thismap(p, createDefaultContext()));
+	#elif defined(THIS_RETURN_TYPE_vec4)
+	vec4 color = thismap(p, createDefaultContext);
+	#else
+	vec4 color = vec4(0.);
+	#endif
+	
+	#ifdef OUTPUT_COLOR
+	colorOut = TDOutputSwizzle(color);
+	#endif
+}
