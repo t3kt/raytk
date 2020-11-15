bdeef8aa01970c2d451619738df39f50defa010a
tekt <tekt@optexture.com>
2020-11-14 18:05:30 -0800

    added remapping to reflect
    not yet supported for arbitrary reflection plane though

--- src/operators/filter/reflect.glsl
+++ src/operators/filter/reflect.glsl
@@ -1,6 +1,27 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+#ifdef THIS_CUSTOM
 	p -= THIS_Planenormal * THIS_Shift;
-	float i = pReflect(p, THIS_Planenormal, THIS_Offset);
+	vec3 planeNormal = THIS_Planenormal;
+	float t = dot(p, planeNormal)+THIS_Offset;
+
+	#ifdef THIS_USE_BLEND_FUNC
+	t = sgn(t) * inputOp2(abs(t), ctx);
+	#endif
+
+	if (t < 0) {
+		p = p - (2.*t) * planeNormal;
+	}
+	float i = sgn(t);
+#else
+	float q = abs(p.THIS_AXIS - THIS_Shift);
+	#ifdef THIS_USE_BLEND_FUNC
+	if (q <= 1.) {
+		q = inputOp2(q, ctx);
+	}
+//	q *= inputOp2(q, ctx);
+	#endif
+	p.THIS_AXIS = q;
+#endif
 #ifdef THIS_EXPOSE_ITERATION
 	ctx.iteration.x = (i + 1.) * .5;
 	ctx.iteration.y = 2;
