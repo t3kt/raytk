15c247658859ea737fc023e8ebd63d4d3ef4c9b5
tekt <tekt@optexture.com>
2020-11-15 17:44:25 -0800

    multi point distance field

---
+++ src/operators/field/multiPointDistanceField.glsl
@@ -0,0 +1,7 @@
+vec4 thismap(CoordT p, ContextT ctx) {
+	return vec4(
+		length(p - THIS_Point1),
+		length(p - THIS_Point2),
+		length(p - THIS_Point3),
+		length(p - THIS_Point4));
+}
---
+++ src/operators/field/multiPointDistanceField.md
@@ -0,0 +1,3 @@
+# multiPointDistanceField (field)
+
+A vector field that provides the distance from 4 specific points in space (one for each part of the vector).
