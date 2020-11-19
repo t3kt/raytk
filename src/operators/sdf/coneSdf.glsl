517557932c7a1e6a1c3b08e24f50554f16ace13f
tekt <tekt@optexture.com>
2020-11-18 20:47:44 -0800

    added radius + height field support to coneSdf

--- src/operators/sdf/coneSdf.glsl
+++ src/operators/sdf/coneSdf.glsl
@@ -1,3 +1,13 @@
 Sdf thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HEIGHT_FIELD
+	float height = THIS_Height * inputOp1(p, ctx);
+	#else
+	float height = THIS_Height;
+	#endif
+	#ifdef THIS_RADIUS_FIELD
+	float radiusMod = inputOp2(p, ctx);
+	#else
+	const float radiusMod = 1.;
+	#endif
 	return createSdf(THIS_EXPR);
 }
