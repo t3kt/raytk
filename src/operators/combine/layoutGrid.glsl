ba0683f9c3af82111f181e367d5d388fbe752b91
t3kt <tekt@immerse.studio>
2020-12-05 17:36:56 -0800

    added grid to layout combine op
    closes #69

--- src/operators/combine/layoutGrid.glsl
+++ src/operators/combine/layoutGrid.glsl
@@ -21,7 +21,17 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 			float cellPos = p.THIS_PLANE_P2 + THIS_Size2 * 0.5;
 			int cell = clamp(int(pModInterval1(cellPos, cellSize, 0, THIS_INPUT_COUNT - 1.)), 0, THIS_INPUT_COUNT - 1);
 			p.THIS_PLANE_P2 = cellPos;
-		#else
+		#elif defined(THIS_GRID_ROWS)
+			vec2 cellSize = THIS_Size * 0.5;
+			int cell;
+			if (THIS_INPUT_COUNT == 3 && p.THIS_PLANE_P2 < 0.) {
+				cell = 2;
+				p.THIS_PLANE += cellSize;
+			} else {
+				cell = quadrantIndex(ivec2(sgn(p.THIS_PLANE)) * ivec2(1, -1));
+				p.THIS_PLANE -= cellSize * sgn(p.THIS_PLANE);
+			}
+	#else
 			#error invalidLayout
 		#endif
 		p /= scale;
