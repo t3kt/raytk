73f625cef462897634e115d08ecfe50db1e77fd2
t3kt <tekt@immerse.studio>
2020-12-16 21:19:53 -0800

    got moduloDistance working
    closes #59

--- src/operators/filter/moduloDistance.glsl
+++ src/operators/filter/moduloDistance.glsl
@@ -1,36 +1,34 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	#if defined(THIS_COORD_TYPE_vec2)
-	p -= THIS_Preoffset;
-	p -= THIS_Center;
+	p += THIS_Center;
 	float d = length(p);
 	#elif defined(THIS_MODE_spherical)
-	p -= THIS_Center;
+	p += THIS_Center;
 	float d = length(p);
 	#else
-	p.THIS_PLANE -= THIS_Offset;
+	p.THIS_PLANE += THIS_Center;
 	float d = length(p.THIS_PLANE);
 	#endif
 
 	#ifdef THIS_MIRROR_mirror
 	float cell = pModMirror1(d, THIS_Length);
 	#else
-	float cell = pMod1(d, THIS_Length);
+	float cell = pMod1(d, THIS_Length*.5);
 	#endif
 
 	#if defined(THIS_COORD_TYPE_vec2)
 	float a = atan(p.y, p.x);
-	p = d * vec2(cos(a), sin(a));
-	p += THIS_Center;
+	p = d * vec2(cos(a), sin(a)) - THIS_Center;
 	#elif defined(THIS_MODE_spherical)
 	float alpha = atan(p.y, p.x);
 	float polar = atan(p.x, p.z);
 	p = d * vec3(
 		sin(polar) * cos(alpha),
 		sin(polar) * sin(alpha),
-		cos(polar));
+		cos(polar)) - THIS_Center;
 	#else
 	float a = atan(p.THIS_PLANE_P2, p.THIS_PLANE_P1);
-	p.THIS_PLANE = d * vec2(cos(a), sin(a));
+	p.THIS_PLANE = d * vec2(cos(a), sin(a)) - THIS_Center;
 	#endif
 
 	#if defined(THIS_ITERATE_CELLS) && defined(THIS_CONTEXT_TYPE_Context)
