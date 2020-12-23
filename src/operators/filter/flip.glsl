b1d6a0bf287242285ebe1ad3d43da441e46500a6
t3kt <tekt@immerse.studio>
2020-12-23 14:04:36 -0800

    more macro cleanup
    see #73, #87

--- src/operators/filter/flip.glsl
+++ src/operators/filter/flip.glsl
@@ -5,21 +5,21 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q.THIS_AXIS -= THIS_Offset;
 	p.THIS_AXIS += THIS_Offset;
 	p.THIS_AXIS -= THIS_Shift;
-	#if defined(THIS_MERGE_none)
-		#ifdef THIS_ITERATE_SIDES
+	#if defined(THIS_Mergetype_none)
+		#ifdef THIS_Iterateonsides
 		ctx.iteration = vec4(1, 2, 0, 0);
 		#endif
 		return inputOp1(q, ctx);
 	#else
-		#ifdef THIS_ITERATE_SIDES
+		#ifdef THIS_Iterateonsides
 		ctx.iteration = vec4(0, 2, 0, 0);
 		#endif
 		Sdf res1 = inputOp1(p, ctx);
-		#ifdef THIS_ITERATE_SIDES
+		#ifdef THIS_Iterateonsides
 		ctx.iteration.x = 1;
 		#endif
 		Sdf res2 = inputOp1(q, ctx);
-		#ifdef THIS_MERGE_smoothUnion
+		#ifdef THIS_Mergetype_smoothUnion
 			return opSmoothUnionM(res1, res2, THIS_Mergeradius);
 		#else
 			return opSimpleUnion(res1, res2);
--- src/operators/filter/mirrorOctant.glsl
+++ src/operators/filter/mirrorOctant.glsl
@@ -3,7 +3,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 cell = pMirrorOctant(q, THIS_Size);
 	pR(q, THIS_Rotateaxis);
 	p.THIS_PLANE = q - THIS_Offset;
-	#ifdef THIS_ITERATE_CELLS
+	#ifdef THIS_Iterateoncells
 	ctx.iteration.x = quadrantIndex(ivec2(cell));
 	ctx.iteration.y = 4;
 	#endif
