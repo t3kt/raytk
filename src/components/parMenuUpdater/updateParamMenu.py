db69a0100d99b8ad4bb0594606d936b08c3c1e91
t3kt <tekt@immerse.studio>
2020-12-24 13:20:06 -0800

    pull par menu updating into its own component
    it's still included in expressionSwitcher but it can now also be used independently.

--- src/components/parMenuUpdater/updateParamMenu.py
+++ src/components/parMenuUpdater/updateParamMenu.py
@@ -4,12 +4,20 @@ def update():
 	par = host.par[name] if name else None
 	if par is None:
 		return
-	dat = op('table')
+	dat = parent().par.Table.eval()
+	if not dat or dat.numRows < 2:
+		return
 	par.menuNames = dat.col('name')[1:]
 	par.menuLabels = dat.col('label')[1:]
+
+
+def onValueChange(par, prev):
+	if par.name == 'Autoupdate' and par:
+		update()
 
 def onTableChange(dat):
 	update()
 
 def onPulse(par):
-	update()
+	if par.name == 'Update':
+		update()
