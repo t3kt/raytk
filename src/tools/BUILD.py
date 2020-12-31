ab2efae3bfeb1ac6f9a4f4db4d7c4f80b8cf2074
t3kt <tekt@immerse.studio>
2020-12-31 13:28:47 -0800

    remove the old create menu
    closes #167

--- src/tools/BUILD.py
+++ src/tools/BUILD.py
@@ -9,15 +9,10 @@ context = args[0]  # type: BuildTaskContext
 def runStage(stage: int):
 	if stage == 0:
 		context.runBuildScript(
-			op('createMenu/BUILD'),
-			thenRun=runStage,
-			runArgs=[stage + 1])
-	elif stage == 1:
-		context.runBuildScript(
 			op('inspector/BUILD'),
 			thenRun=runStage,
 			runArgs=[stage + 1])
-	elif stage == 2:
+	elif stage == 1:
 		context.runBuildScript(
 			op('palette/BUILD'),
 			thenRun=context.finishTask,
