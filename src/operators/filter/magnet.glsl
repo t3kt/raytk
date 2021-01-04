a7f03ea9dad0da1a12134543792c4a77ecdc85f4
t3kt <tekt@immerse.studio>
2021-01-03 16:14:21 -0800

    macro cleanup in magnet

--- src/operators/filter/magnet.glsl
+++ src/operators/filter/magnet.glsl
@@ -1,18 +1,18 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#if !defined(THIS_MAGNET)
+	#if !defined(THIS_HAS_INPUT_2)
 		CoordT center = THIS_Center;
 		float d = length(p - center);
-	#elif defined(THIS_MAGNET_TYPE_Sdf)
+	#elif defined(inputOp2_RETURN_TYPE_Sdf)
 		CoordT center = THIS_Center;
-		float d = THIS_MAGNET(p, ctx).x;
-	#elif defined(THIS_MAGNET_TYPE_float)
+		float d = inputOp2(p, ctx).x;
+	#elif defined(inputOp2_RETURN_TYPE_float)
 		CoordT center = THIS_Center;
-		float d = THIS_MAGNET(p, ctx);
-	#elif defined(THIS_MAGNET_TYPE_vec4)
+		float d = inputOp2(p, ctx);
+	#elif defined(inputOp2_RETURN_TYPE_vec4)
 		#if defined(THIS_COORD_TYPE_vec2)
-			CoordT center = THIS_MAGNET(p, ctx).xy;
+			CoordT center = inputOp2(p, ctx).xy;
 		#elif defined(THIS_COORD_TYPE_vec3)
-			CoordT center = THIS_MAGNET(p, ctx).xyz;
+			CoordT center = inputOp2(p, ctx).xyz;
 		#else
 			#error invalidCoordType
 		#endif
@@ -24,9 +24,9 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float radius = THIS_Radius;
 	float low = radius;
 	float high = radius - THIS_Fade / 2.;
-	#if defined(THIS_EASE)
+	#if defined(THIS_HAS_INPUT_3)
 	// There's probably a better way to do this..
-		d = THIS_EASE(clamp(map01(d, low, high), 0., 1.), ctx);
+		d = inputOp3(clamp(map01(d, low, high), 0., 1.), ctx);
 	#else
 	d = smoothstep(low, high, d);
 	#endif
