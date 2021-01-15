8ccb9a563c219de584a20f338d7105e95db58025
t3kt <tekt@immerse.studio>
2021-01-14 19:56:57 -0800

    added rotateaxis field input to mirrorOctant
    closes #201

--- src/operators/filter/mirrorOctant.glsl
+++ src/operators/filter/mirrorOctant.glsl
@@ -1,11 +1,34 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 q = p.THIS_PLANE;
 	vec2 cell = pMirrorOctant(q, THIS_Size);
-	pR(q, THIS_Rotateaxis);
-	p.THIS_PLANE = q - THIS_Offset;
 	#ifdef THIS_Iterateoncells
 	ctx.iteration.x = quadrantIndex(ivec2(cell));
 	ctx.iteration.y = 4;
 	#endif
+	float r = THIS_Rotateaxis;
+
+	#ifdef THIS_HAS_INPUT_2
+	#if defined(inputOp2_COORD_TYPE_float)
+	float q2 = length(p.THIS_PLANE);
+	#elif defined(inputOp2_COORD_TYPE_vec2)
+	vec2 q2 = p.THIS_PLANE;
+	#elif defined(inputOp2_COORD_TYPE_vec3)
+	vec3 q2 = p;
+	#else
+	#error invalidRotateAxisCoordType
+	#endif
+
+	#if defined(inputOp2_RETURN_TYPE_Sdf)
+	r += radians(inputOp2(q2, ctx).x);
+	#elif defined(inputOp2_RETURN_TYPE_float)
+	r += radians(inputOp2(q2, ctx));
+	#else
+	#error invalidRotateAxisReturnType
+	#endif
+	#endif
+
+	pR(q, r);
+
+	p.THIS_PLANE = q - THIS_Offset;
 	return inputOp1(p, ctx);
 }
--- src/operators/filter/mirrorOctant.md
+++ src/operators/filter/mirrorOctant.md
@@ -1,3 +1,18 @@
-# mirrorOctant (filter)
-
 Mirror coordinates across two axes and the diagonals.
+
+## Parameters
+
+* `Enable`
+* `Axis`: Axis that faces the plane where coordinates are mirrored.
+  * `x`
+  * `y`
+  * `z`
+* `Size`: Spacing of the reflection planes.
+* `Offset`: Shifts the input before applying reflection.
+* `Rotateaxis`: Rotates the input before applying reflection.
+* `Iterateoncells`: Enables upstream operators to check which cell in the reflection grid a point is in.
+
+## Inputs
+
+* `definition_in`: 
+* `rotate_axis_field_definition_in` *Rotate Axis Field*: Value field used to vary the `Rotateaxis`. If the field is a 1D field, it is given the distance from the center. If it is a 2D field, it is given the position along the mirror axes. If it is a 3D field, it is given the raw position. The value is converted to radians and *added* to the `Rotateaxis` parameter.
