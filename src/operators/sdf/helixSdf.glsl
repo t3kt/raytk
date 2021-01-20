7dfd6085de25e15ab1c762974d8b3d844b5acd27
t3kt <tekt@immerse.studio>
2021-01-20 00:10:56 -0800

    Added field inputs for radius and thickness in helixSdf
    closes #223
    closes #222

--- src/operators/sdf/helixSdf.glsl
+++ src/operators/sdf/helixSdf.glsl
@@ -1,6 +1,24 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Translate;
 	p = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
+	float thickness = THIS_Thickness;
+	float radius = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_1
+	#if defined(inputOp1_COORD_TYPE_float)
+	thickness *= inputOp1(p.y, ctx);
+	#elif defined(inputOp1_COORD_TYPE_vec2)
+	thickness *= inputOp1(vec2(p.y, degrees(atan(p.z, p.x))), ctx);
+	#elif defined(inputOp1_COORD_TYPE_vec3)
+	thickness *= inputOp1(p, ctx);
+	#endif
+	#endif
+	#ifdef THIS_HAS_INPUT_2
+	#if defined(inputOp2_COORD_TYPE_float)
+	radius *= inputOp2(p.y, ctx);
+	#elif defined(inputOp2_COORD_TYPE_vec3)
+	radius *= inputOp2(p, ctx);
+	#endif
+	#endif
 	return createSdf(sdHelix(
-		p, THIS_Radius, THIS_Thickness, THIS_Spread, THIS_Dualspread * THIS_Radius));
+		p, radius, thickness, THIS_Spread, THIS_Dualspread * radius));
 }
---
+++ src/operators/sdf/helixSdf.md
@@ -0,0 +1,17 @@
+## Parameters
+
+* `Enable`
+* `Axis`
+  * `x`
+  * `y`
+  * `z`
+* `Translate`
+* `Radius`
+* `Thickness`
+* `Spread`
+* `Dualspread`
+
+## Inputs
+
+* `radius_field_definition_in` *Radius Field*: Field used to multiply the `Radius` parameter. If it uses 1D coordinates, it is provided the position along the axis. If it uses 3D coordinates, it uses the absolute position.
+* `thickness_field_definition_in` *Thickness Field*: Field used to multiply the `Thickness` parameter. If it uses 1D coordinates, it is provided the position along the axis. If it uses 3D coordinates, it uses the absolute position.
