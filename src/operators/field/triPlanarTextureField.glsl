c61097d8da99a4d68be7d01ddd449dc618603513
t3kt <tekt@immerse.studio>
2021-01-22 16:32:07 -0800

    added triplanar texture field
    see #233, #2

---
+++ src/operators/field/triPlanarTextureField.glsl
@@ -0,0 +1,32 @@
+ReturnT thismap(CoordT p, MaterialContext ctx) {
+	vec3 uv = p;
+	uv = (uv - THIS_Translate) / THIS_Scale;
+	#if defined(THIS_Extendmode_hold)
+	uv = clamp(uv, 0, 1);
+	#elif defined(THIS_Extendmode_repeat)
+	uv = fract(uv);
+	#elif defined(THIS_Extendmode_mirror)
+	uv = modZigZag(uv);
+	#elif defined(THIS_Extendmode_zero)
+		if (uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1 || uv.z < 0 || uv.z > 1) {
+			#if defined(THIS_RETURN_TYPE_float)
+			return 0;
+			#else
+			return vec4(0);
+			#endif
+		}
+	#endif
+	vec3 n = abs(ctx.normal);
+	n *= n;
+	vec3 colXY = texture(THIS_texture, uv.xy).rgb;
+	vec3 colYZ = texture(THIS_texture, uv.yz).rgb;
+	vec3 colZX = texture(THIS_texture, uv.zx).rgb;
+
+	vec3 col = colXY * n.z + colYZ * n.x + colZX * n.y;
+	vec4 value = vec4(col, 1);
+	#ifdef THIS_RETURN_TYPE_float
+	return value.x;
+	#else
+	return value;
+	#endif
+}
