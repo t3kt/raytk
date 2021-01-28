cdfeb665aa6b52d00a559d0c55de54a36fc25a37
t3kt <tekt@immerse.studio>
2021-01-27 21:54:58 -0800

    add colorizeSdf2d
    This is part of separating colorization from render2D. See #18

---
+++ src/operators/material/colorizeSdf2d.glsl
@@ -0,0 +1,17 @@
+vec4 thismap(vec2 p, ContextT ctx) {
+	Sdf res = inputOp1(p, ctx);
+	float d = res.x;
+	vec4 color;
+
+	if (d > 0.) {
+		color = mix(THIS_Outsidecolor1, THIS_Outsidecolor2, (1. + sin(d / THIS_Outsideperiod)) * 0.5);
+	} else {
+		color = mix(THIS_Insidecolor1, THIS_Insidecolor2, (1. + sin(d / THIS_Insideperiod)) * 0.5);
+	}
+	#ifdef THIS_Enableedge
+	float thickness = THIS_Edgethickness;
+	float blending = THIS_Edgeblending;
+	color = mix(color, THIS_Edgecolor, 1.0 - smoothstep((thickness-blending)*0.5, (thickness+blending)*0.5, abs(d)));
+	#endif
+	return color;
+}
