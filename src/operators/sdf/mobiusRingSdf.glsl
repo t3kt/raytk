a32db125445a47ff47ec91d6ec2b67f04af7a44c
t3kt <tekt@immerse.studio>
2021-01-30 16:43:13 -0800

    add axis to mobiusRingSdf
    closes #246

--- src/operators/sdf/mobiusRingSdf.glsl
+++ src/operators/sdf/mobiusRingSdf.glsl
@@ -1,13 +1,22 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	float t = THIS_Thickness;
+	// standard axis swizzle macros don't match what this needs
+	#if defined(THIS_Axis_x)
+	p = p.yxz;
+	#elif defined(THIS_Axis_y)
+	p = p.yzx;
+	#elif defined(THIS_Axis_z)
+	#else
+	#error invalidAxis
+	#endif
 	#ifdef THIS_THICK_FIELD
-		#if defined(THIS_THICK_COORD_TYPE_float)
-			t *= THIS_THICK_FIELD(atan(p.z, p.x) / TAU, ctx);
-		#elif defined(THIS_THICK_COORD_TYPE_vec3)
-			t *= THIS_THICK_FIELD(p, ctx);
-		#else
-			#error invalidRadiusCoordType
-		#endif
+	#if defined(THIS_THICK_COORD_TYPE_float)
+	t *= THIS_THICK_FIELD(atan(p.z, p.x) / TAU, ctx);
+	#elif defined(THIS_THICK_COORD_TYPE_vec3)
+	t *= THIS_THICK_FIELD(p, ctx);
+	#else
+	#error invalidRadiusCoordType
+	#endif
 	#endif
 	return createSdf(sdMobiusRing(p - THIS_Translate, THIS_Radius, t, THIS_Rounding, THIS_Twist, THIS_Twistphase));
 }
