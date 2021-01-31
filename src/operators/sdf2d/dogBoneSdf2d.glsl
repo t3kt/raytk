d402b4e5b216884b3039b38a77b6a1ce1880ed51
t3kt <tekt@immerse.studio>
2021-01-30 20:37:11 -0800

    dogBoneSdf2d
    closes #253

---
+++ src/operators/sdf2d/dogBoneSdf2d.glsl
@@ -0,0 +1,24 @@
+// "DogBone SDF" by Martijn Steinrucken aka BigWings/CountFrolic - 2019
+// https://www.shadertoy.com/view/wld3D4
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	p -= THIS_Translate;
+	float w = THIS_Length * 0.5;
+	float r = THIS_Radius;
+	float b = THIS_Bulge;
+
+	if(abs(b)<1e-7) b = 1e-7;	// prevent division by 0
+	float sb = sign(b);
+
+	p = abs(p);
+
+	vec2 ep = p-vec2(w, 0);			// end point
+	float dE = length(ep)-r;		// distance to end circle
+	float y = (w*w-r*r)/(2.*r*b);	// height of center circle
+	vec2 cp = vec2(p.x, p.y-y);		// position of center circle
+	vec2 ec = sb*(ep-cp);			// vec from end point to center point
+	float rc = length(ec)-r*sb;		// radius of center circle
+	float dC = sb*(rc-length(cp));	// distance to center circle
+
+	return createSdf(ec.x*ep.y-ec.y*ep.x < 0. ? dE : dC);
+}
