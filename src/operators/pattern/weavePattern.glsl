40124289c2a66b7e4dfb42a6b02de912c0798473
t3kt <tekt@immerse.studio>
2021-01-30 23:50:02 -0800

    added weavePattern
    see #256.
    haven't yet added the variations

---
+++ src/operators/pattern/weavePattern.glsl
@@ -0,0 +1,8 @@
+#define THIS_S(x,y)   abs(fract(x)-.5) < THIS_Thickness  ? .7 + .3* sin(3.14* (y + ceil(x) )) : 0.
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	p -= THIS_Translate;
+	p /= THIS_Size;
+
+	return max(THIS_S(p.x,p.y), THIS_S(-p.y,p.x));
+}
