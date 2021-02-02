d77e8a8fac68be53115726e94fd97d3deeb6c9cb
t3kt <tekt@immerse.studio>
2021-02-01 20:42:15 -0800

    better default state for when render2d doesn't have an input

--- src/operators/output/render2D_function.glsl
+++ src/operators/output/render2D_function.glsl
@@ -14,5 +14,11 @@ vec4 getColor(Sdf res) {
 #endif
 
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#if defined(THIS_HAS_INPUT_1)
 	return inputOp1(p, ctx);
+	#elif defined(THIS_RETURN_TYPE_Sdf)
+	return createNonHitSdf();
+	#else
+	return ReturnT(0.);
+	#endif
 }
