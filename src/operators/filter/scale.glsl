856fcc0bf9283f803441178781dcb07788066d99
t3kt <tekt@immerse.studio>
2021-02-04 20:38:06 -0800

    added uniform scale option and fixed scale adjustment bug
    closes #267

--- src/operators/filter/scale.glsl
+++ src/operators/filter/scale.glsl
@@ -1,27 +1,42 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#if defined(THIS_Scaletype_uniform)
+	float scale = THIS_Uniformscale;
+	#elif defined(THIS_Scaletype_separate)
 	CoordT scale = THIS_Scale;
+	#else
+	#error invalidScaleType
+	#endif
 	#ifdef THIS_HAS_INPUT_2
 		#if defined(inputOp2_RETURN_TYPE_Sdf)
 			scale *= inputOp2(p, ctx).x;
 		#elif defined(inputOp2_RETURN_TYPE_float)
 			scale *= inputOp2(p, ctx);
 		#elif defined(inputOp2_RETURN_TYPE_vec4)
-			#if defined(THIS_COORD_TYPE_vec2)
-				scale *= inputOp2(p, ctx).xy;
-			#elif defined(THIS_COORD_TYPE_vec3)
-				scale *= inputOp2(p, ctx).xyz;
+			#ifdef THIS_Scaletype_uniform
+				#error scaleTypeNotSupportedForVec4Field
 			#else
-				#error invalidCoordType
+				#if defined(THIS_COORD_TYPE_vec2)
+					scale *= inputOp2(p, ctx).xy;
+				#elif defined(THIS_COORD_TYPE_vec3)
+					scale *= inputOp2(p, ctx).xyz;
+				#else
+					#error invalidCoordType
+				#endif
 			#endif
 		#else
 			#error invalidFieldReturnType
 		#endif
 	#endif
 	ReturnT res = inputOp1(p / scale, ctx);
-	#ifdef THIS_RETURN_TYPE_float
-	res /= length(scale);
+	#ifdef THIS_Scaletype_uniform
+	float adjust = scale;
 	#else
-	res.x /= length(scale);
+	float adjust = vmin(scale);
+	#endif
+	#ifdef THIS_RETURN_TYPE_float
+	res *= adjust;
+	#else
+	res.x *= adjust;
 	#endif
 	return res;
 }
