2b6b81ba5f493ef84c91c8c5f9053bb60cfc34f5
t3kt <tekt@immerse.studio>
2021-02-16 20:30:55 -0800

    clean up and fix iterationField
    see #310

--- src/operators/field/iterationField.glsl
+++ src/operators/field/iterationField.glsl
@@ -1,13 +1,7 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#if defined(THIS_Iterationmode_index)
-	return ctx.iteration.x;
-	#elif defined(THIS_Iterationmode_scaledindex)
-	if (ctx.iteration.y != 0.) {
-		return ctx.iteration.x / ctx.iteration.y;
-	} else {
-		return ctx.iteration.x;
-	}
-	#elif defined(THIS_Iterationmode_full)
+	#if defined(THIS_Format_full)
 	return ctx.iteration;
+	#else
+	return ctx.iteration.THIS_Format;
 	#endif
 }
