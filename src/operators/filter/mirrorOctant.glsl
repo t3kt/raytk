26ffd09e96e8a962807a1e98b73b7356c915069b
t3kt <tekt@immerse.studio>
2021-02-16 17:54:47 -0800

    add offset field for mirrorOctant
    closes #307

--- src/operators/filter/mirrorOctant.glsl
+++ src/operators/filter/mirrorOctant.glsl
@@ -8,27 +8,51 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float r = THIS_Rotateaxis;
 
 	#ifdef THIS_HAS_INPUT_2
-	#if defined(inputOp2_COORD_TYPE_float)
-	float q2 = length(p.THIS_PLANE);
-	#elif defined(inputOp2_COORD_TYPE_vec2)
-	vec2 q2 = p.THIS_PLANE;
-	#elif defined(inputOp2_COORD_TYPE_vec3)
-	vec3 q2 = p;
-	#else
-	#error invalidRotateAxisCoordType
-	#endif
+  {
+		#if defined(inputOp2_COORD_TYPE_float)
 
-	#if defined(inputOp2_RETURN_TYPE_Sdf)
-	r += radians(inputOp2(q2, ctx).x);
-	#elif defined(inputOp2_RETURN_TYPE_float)
-	r += radians(inputOp2(q2, ctx));
-	#else
-	#error invalidRotateAxisReturnType
-	#endif
-	#endif
+		float q2 = length(p.THIS_PLANE);
+		#elif defined(inputOp2_COORD_TYPE_vec2)
+		vec2 q2 = p.THIS_PLANE;
+		#elif defined(inputOp2_COORD_TYPE_vec3)
+		vec3 q2 = p;
+		#else
+		#error invalidRotateAxisCoordType
+		#endif
 
+		#if defined(inputOp2_RETURN_TYPE_Sdf)
+		r += radians(inputOp2(q2, ctx).x);
+		#elif defined(inputOp2_RETURN_TYPE_float)
+		r += radians(inputOp2(q2, ctx));
+		#else
+		#error invalidRotateAxisReturnType
+		#endif
+	}
+	#endif
 	pR(q, r);
 
-	p.THIS_PLANE = q - THIS_Offset;
+	vec2 offset = THIS_Offset;
+
+	#ifdef THIS_HAS_INPUT_3
+	{
+		#if defined(inputOp3_COORD_TYPE_float)
+		float q3 = p.THIS_AXIS;
+		#elif defined(inputOp3_COORD_TYPE_vec3)
+		vec3 q3 = p;
+		#else
+		#error invalidOffsetCoordType
+		#endif
+
+		#if defined(inputOp3_RETURN_TYPE_float)
+		offset += vec2(inputOp3(q3, ctx));
+		#elif defined(inputOp3_RETURN_TYPE_vec4)
+		offset += inputOp3(q3, ctx).xy;
+		#else
+		#error invalidOffsetReturnType
+		#endif
+	}
+	#endif
+
+	p.THIS_PLANE = q - offset;
 	return inputOp1(p, ctx);
 }
