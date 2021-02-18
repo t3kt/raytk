851cf860e659e08cf37cfc6392eecf5e258a2f07
t3kt <tekt@immerse.studio>
2021-02-17 19:58:49 -0800

    initial implementation of rangeTransform
    only supports translate for now.
    still in beta.
    see #282

---
+++ src/operators/filter/rangeTransform.glsl
@@ -0,0 +1,18 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	float i = mapRange(ctx.iteration.x, THIS_Indexrange1, THIS_Indexrange2, 0., 1.);
+	#if defined(THIS_Extendmode_linear)
+	#elif defined(THIS_Extendmode_clamp)
+	i = clamp(i, 0., 1.);
+	#elif defined(THIS_Extendmode_loop)
+	i = fract(i);
+	#else
+	#error invalidExtendMode
+	#endif
+
+	#ifdef THIS_Enabletranslate
+	CoordT t = mix(THIS_Translate1, THIS_Translate2, i);
+	p -= t;
+	#endif
+
+	return inputOp1(p, ctx);
+}
