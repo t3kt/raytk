4337e1a5cad2d451bdde72712d89e3021297ea54
t3kt <tekt@immerse.studio>
2021-02-19 20:09:02 -0800

    added field inputs for prismSdf
    closes #326

--- src/operators/sdf/prismSdf.glsl
+++ src/operators/sdf/prismSdf.glsl
@@ -1,4 +1,14 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_1
+	float h = inputOp1(p, ctx);
+	#else
+	float h = THIS_Height;
+	#endif
+	#ifdef THIS_HAS_INPUT_2
+	float r = inputOp2(p, ctx);
+	#else
+	float r = THIS_Radius;
+	#endif
 	p -= THIS_Translate;
 	p = vec3(p.THIS_PLANE_P2, p.THIS_PLANE_P1, p.THIS_AXIS);
 	return createSdf(THIS_EXPR);
