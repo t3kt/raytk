33b7f65723ca33fdd78677c86675959268d82b9c
t3kt <tekt@immerse.studio>
2021-02-25 19:23:36 -0800

    cleanup in bend

--- src/operators/filter/bend.glsl
+++ src/operators/filter/bend.glsl
@@ -1,19 +1,19 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec3 q = p.THIS_SWIZZLE;
-	#ifdef THIS_USE_FIELD
-		#if defined(THIS_FIELD_COORD_TYPE_float)
+	vec3 q = p.THIS_Direction;
+	#ifdef THIS_HAS_INPUT_2
+		#if defined(inputOp2_COORD_TYPE_float)
 		float fieldP = q.x;
-		#elif defined(THIS_FIELD_COORD_TYPE_vec2)
+		#elif defined(inputOp2_COORD_TYPE_vec2)
 		vec2 fieldP = q.xy;
-		#elif defined(THIS_FIELD_COORD_TYPE_vec3)
+		#elif defined(inputOp2_COORD_TYPE_vec3)
 		vec3 fieldP = q;
 		#else
 		#error unsupportedFieldCoordType
 		#endif
 		
-		#if defined(THIS_FIELD_RETURN_TYPE_Sdf)
+		#if defined(inputOp2_RETURN_TYPE_Sdf)
 		float amt = inputOp2(fieldP, ctx).x;
-		#elif defined(THIS_FIELD_RETURN_TYPE_float)
+		#elif defined(inputOp2_RETURN_TYPE_float)
 		float amt = inputOp2(fieldP, ctx);
 		#else
 		#error unsupportedInputReturnType
@@ -24,6 +24,6 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q.x += THIS_Shift;
 	q = opCheapBendPos(q, amt);
 	q.x -= THIS_Shift;
-	p.THIS_SWIZZLE = q;
+	p.THIS_Direction = q;
 	return inputOp1(p, ctx);
 }
