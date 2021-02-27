9f9b029c6704fbfd327098fa92707480205a92e9
t3kt <tekt@immerse.studio>
2021-02-27 13:46:37 -0800

    added axis and thickness for discSdf
    closes #352

--- src/operators/sdf/discSdf.glsl
+++ src/operators/sdf/discSdf.glsl
@@ -1,3 +1,4 @@
 Sdf thismap(CoordT p, ContextT ctx) {
-	return createSdf(fDisc(p - THIS_Translate, THIS_Radius));
+	p = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
+	return createSdf(fDisc(p - THIS_Translate, THIS_Radius) - THIS_Thickness);
 }
