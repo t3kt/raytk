d641868b9bec88fc0ee248204f512c75e0f6f4ae
t3kt <tekt@immerse.studio>
2021-02-27 23:03:59 -0800

    local position support for fieldMat
    see #341

--- src/operators/material/fieldMat.glsl
+++ src/operators/material/fieldMat.glsl
@@ -1,9 +1,20 @@
 Sdf thismap(CoordT p, ContextT ctx) {
 	Sdf res = inputOp1(p, ctx);
+	#if defined(THIS_Uselocalpos) && defined(RAYTK_USE_MATERIAL_POS)
+	assignMaterialWithPos(res, THISMAT, p);
+	#else
 	assignMaterial(res, THISMAT);
+	#endif
 	return res;
 }
 
 vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
-	return inputOp2(p, matCtx).rgb;
+	vec3 mp = getPosForMaterial(p, matCtx);
+	#if defined(inputOp2_RETURN_TYPE_vec4)
+	return inputOp2(mp, matCtx).rgb;
+	#elif defined(inputOp2_RETURN_TYPE_float)
+	return vec3(inputOp2(mp, matCtx));
+	#else
+	#error invalidFieldReturnType
+	#endif
 }
