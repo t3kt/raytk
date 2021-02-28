4cddc52db8611547e334e79d3e3e45b0b9047cee
t3kt <tekt@immerse.studio>
2021-02-28 11:13:06 -0800

    Fix bug in amount parameter in round
    Also switched the behavior of the field input to ADD instead of MULTIPLY. This will change behavior after update.
    
    closes #356

--- src/operators/filter/round.glsl
+++ src/operators/filter/round.glsl
@@ -2,7 +2,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p, ctx);
 	float amt = THIS_Amount;
 	#ifdef THIS_HAS_INPUT_2
-	amt *= inputOp2(p, ctx);
+	amt += inputOp2(p, ctx);
 	#endif
 	#ifdef THIS_RETURN_TYPE_Sdf
 	res.x -= amt;
