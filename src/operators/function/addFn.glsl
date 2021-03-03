ff3d57a1b8db689d7350111b6a851c6b7cbff54e
t3kt <tekt@immerse.studio>
2021-03-02 20:16:15 -0800

    fix issues in addFn with input handling
    closes #363

--- src/operators/function/addFn.glsl
+++ src/operators/function/addFn.glsl
@@ -2,12 +2,12 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#if THIS_INPUT_COUNT == 0
 		return THIS_Add;
 	#elif THIS_INPUT_COUNT == 1
-		return THIS_Add + inputOp1(p, ctx);
+		return THIS_Add + THIS_INPUT_1(p, ctx);
 	#elif THIS_INPUT_COUNT == 2
-		return THIS_Add + inputOp1(p, ctx) + inputOp2(p, ctx);
+		return THIS_Add + THIS_INPUT_1(p, ctx) + THIS_INPUT_2(p, ctx);
 	#elif THIS_INPUT_COUNT == 3
-		return THIS_Add + inputOp1(p, ctx) + inputOp2(p, ctx) + inputOp3(p, ctx);
+		return THIS_Add + THIS_INPUT_1(p, ctx) + THIS_INPUT_2(p, ctx) + THIS_INPUT_3(p, ctx);
 	#elif THIS_INPUT_COUNT == 4
-		return THIS_Add + inputOp1(p, ctx) + inputOp2(p, ctx) + inputOp3 + inputOp4(p, ctx);
+		return THIS_Add + THIS_INPUT_1(p, ctx) + THIS_INPUT_2(p, ctx) + THIS_INPUT_3 + THIS_INPUT_4(p, ctx);
 	#endif
 }
---
+++ src/operators/function/addFn.md
@@ -0,0 +1,13 @@
+Adds the returned values produced by all of the connected input functions.
+
+## Parameters
+
+* `Enable`
+* `Add`: Adds an additional value to the inputs.
+
+## Inputs
+
+* `definition_in`: 
+* `definition_in_2`: 
+* `definition_in_3`: 
+* `definition_in_4`: 
