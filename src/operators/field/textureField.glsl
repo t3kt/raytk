eb1def366d5d2fffb17c633e534b94b1d0632f04
t3kt <tekt@immerse.studio>
2021-03-04 20:52:33 -0800

    Fix textureField positioning
    see #368

--- src/operators/field/textureField.glsl
+++ src/operators/field/textureField.glsl
@@ -10,13 +10,13 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#endif
 	uv = (uv - THIS_Translate) / THIS_Scale;
 	#if defined(THIS_Extendmode_hold)
-	uv = clamp(uv, 0, 1);
+	uv = clamp(uv, -0.5, 0.5);
 	#elif defined(THIS_Extendmode_repeat)
-	uv = fract(uv);
+	uv = fract(uv+0.5)-0.5;
 	#elif defined(THIS_Extendmode_mirror)
-	uv = modZigZag(uv);
+	uv = modZigZag(uv+0.5)-0.5;
 	#elif defined(THIS_Extendmode_zero)
-		if (uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1) {
+		if (uv.x < -0.5 || uv.x > 0.5 || uv.y < -0.5 || uv.y > 0.5) {
 			#if defined(THIS_RETURN_TYPE_float)
 			return 0;
 			#else
@@ -24,7 +24,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 			#endif
 		}
 	#endif
-	vec4 value = texture(THIS_texture, uv);
+	vec4 value = texture(THIS_texture, uv + 0.5);
 	#ifdef THIS_RETURN_TYPE_float
 	return value.x;
 	#else
