9ce31844af5dbb92f1ba5a5834ffb54f7d63f105
t3kt <tekt@immerse.studio>
2021-03-06 14:29:11 -0800

    fix modulo2d quadrant iterate mode
    closes #373

--- src/operators/filter/modulo2D.glsl
+++ src/operators/filter/modulo2D.glsl
@@ -5,7 +5,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#if defined(THIS_Iterationtype_cellcoord)
 	setIterationIndex(ctx, cell);
 	#elif defined(THIS_Iterationtype_tiledquadrant)
-	setIterationIndex(ctx, quadrantIndex(mod(ivec2(cell), 2)));
+	setIterationIndex(ctx, quadrantIndex(ivec2(mod(ivec2(cell), 2))));
 	#elif defined(THIS_Iterationtype_alternatingcoord)
 	setIterationCell(ctx, mod(cell, 2.));
 	#endif
