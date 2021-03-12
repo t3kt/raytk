27381478da6b4d5be5afb41a41ce69443481ec5e
t3kt <tekt@immerse.studio>
2021-03-11 19:58:23 -0800

    added pivot support in rotate
    closes #175.
    It doesn't seem work as a way to curl space. But it is still useful for things like iteration.

--- src/operators/filter/rotate.glsl
+++ src/operators/filter/rotate.glsl
@@ -1,17 +1,30 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_Usepivot
+		#ifdef THIS_HAS_INPUT_3
+			#ifdef THIS_COORD_TYPE_vec2
+			CoordT pivot = adaptAsVec2(inputOp3(p, ctx));
+			#else
+			CoordT pivot = adaptAsVec3(inputOp3(p, ctx));
+			#endif
+		#else
+			CoordT pivot = THIS_Pivot;
+		#endif
+	#endif
 #if defined(THIS_COORD_TYPE_vec2) || defined(THIS_Rotatemode_axis)
 	float r = THIS_Rotate;
 	#ifdef THIS_HAS_INPUT_2
-		#if defined(inputOp2_RETURN_TYPE_float)
-			r += inputOp2(p, ctx);
-		#else
-			r += inputOp2(p, ctx).x;
-		#endif
+		r += adaptAsFloat(inputOp2(p, ctx));
+	#endif
+	#ifdef THIS_Usepivot
+	p -= pivot;
 	#endif
 	#ifdef THIS_COORD_TYPE_vec2
 		pR(p, radians(r));
 	#else
 		p *= TDRotateOnAxis(radians(r), THIS_Axis);
+	#endif
+	#ifdef THIS_Usepivot
+	p += pivot;
 	#endif
 #elif defined(THIS_Rotatemode_euler)
 	vec3 r = vec3(THIS_ROT_1, THIS_ROT_2, THIS_ROT_3);
@@ -27,9 +40,15 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		#error invalidFieldReturnType
 		#endif
 	#endif
+	#ifdef THIS_Usepivot
+	p -= pivot;
+	#endif
 	p *= TDRotateOnAxis(radians(r[0]), THIS_AXIS_1)
 		* TDRotateOnAxis(radians(r[1]), THIS_AXIS_2)
 		* TDRotateOnAxis(radians(r[2]), THIS_AXIS_3);
+	#ifdef THIS_Usepivot
+	p += pivot;
+	#endif
 #else
 	#error invalidRotateMode
 #endif
