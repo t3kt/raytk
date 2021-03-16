0a04a672a1f68807703a5d2cd0e88680330c6d3c
t3kt <tekt@immerse.studio>
2021-03-15 20:27:21 -0700

    iteration support in modulo3D
    closes #317

--- src/operators/filter/modulo3D.glsl
+++ src/operators/filter/modulo3D.glsl
@@ -2,5 +2,10 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 q = p + THIS_Shift;
 	vec3 cell = pMod3(q, THIS_Size);
 	p = q - THIS_Offset;
+	#if defined(THIS_Iterationtype_cellcoord)
+	setIterationCell(ctx, cell);
+	#elif defined(THIS_Iterationtype_alternatingcoord)
+	setIterationCell(ctx, mod(cell, 2.));
+	#endif
 	return inputOp1(p, ctx);
 }
