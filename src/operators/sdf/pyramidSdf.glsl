a2453a980a688c372fe1edc7717c69313445fd5d
t3kt <tekt@immerse.studio>
2021-03-18 19:51:07 -0700

    add axis setting and test for pyramidSdf
    closes #394

--- src/operators/sdf/pyramidSdf.glsl
+++ src/operators/sdf/pyramidSdf.glsl
@@ -1,3 +1,5 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return createSdf(sdPyramid((p - THIS_Translate) / vec3(THIS_Width, 1., THIS_Width), THIS_Height));
+	p -= THIS_Translate;
+	p.yzx = p.THIS_AXIS_PLANE_SWIZZLE;
+	return createSdf(sdPyramid(p / vec3(THIS_Width, 1., THIS_Width), THIS_Height));
 }
