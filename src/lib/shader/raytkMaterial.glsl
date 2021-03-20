efe4364dd60a76e56ff63d909e92101a307e34c0
t3kt <tekt@immerse.studio>
2021-03-20 12:01:38 -0700

    added oren-nayar material
    closes #407

--- src/lib/shader/raytkMaterial.glsl
+++ src/lib/shader/raytkMaterial.glsl
@@ -48,3 +48,23 @@ float checkersGradBox(in vec2 p)
 
 	return coarse+fine*0.5;//step(fract(p.x), 0.55)-step(fract(p.x), 0.5)+step(fract(p.y), 0.55)-step(fract(p.y), 0.5);
 }
+float orenNayarDiffuse(
+	vec3 lightDirection,
+	vec3 viewDirection,
+	vec3 surfaceNormal,
+	float roughness,
+	float albedo) {
+
+	float LdotV = dot(lightDirection, viewDirection);
+	float NdotL = dot(lightDirection, surfaceNormal);
+	float NdotV = dot(surfaceNormal, viewDirection);
+
+	float s = LdotV - NdotL * NdotV;
+	float t = mix(1.0, max(NdotL, NdotV), step(0.0, s));
+
+	float sigma2 = roughness * roughness;
+	float A = 1.0 + sigma2 * (albedo / (sigma2 + 0.13) + 0.5 / (sigma2 + 0.33));
+	float B = 0.45 * sigma2 / (sigma2 + 0.09);
+
+	return albedo * max(0.0, NdotL) * (A + B * s / t) / PI;
+}
---
+++ src/operators/material/orenNayarMat.glsl
@@ -0,0 +1,56 @@
+// Based on https://github.com/glslify/glsl-diffuse-oren-nayar
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	Sdf res = inputOp1(p, ctx);
+	#if defined(THIS_Uselocalpos) && defined(RAYTK_USE_MATERIAL_POS)
+	assignMaterialWithPos(res, THISMAT, p);
+	#else
+	assignMaterial(res, THISMAT);
+	#endif
+	return res;
+}
+
+vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
+	vec3 lightDir = normalize(matCtx.light.pos);
+	vec3 viewDir = normalize(-matCtx.ray.dir);
+
+	vec3 baseColor = THIS_Basecolor;
+	#ifdef THIS_HAS_INPUT_3
+	{
+		vec3 mp = getPosForMaterial(p, matCtx);
+		#if defined(inputOp3_RETURN_TYPE_vec4)
+		baseColor *= inputOp3(mp, matCtx).rgb;
+		#elif defined(inputOp3_RETURN_TYPE_float)
+		baseColor *= vec3(inputOp3(mp, matCtx));
+		#else
+		#error invalidColorFieldReturnType
+		#endif
+	}
+	#endif
+
+	float occ = calcAO(p, matCtx.normal);
+	float diffAmt = orenNayarDiffuse(
+		lightDir,
+		viewDir,
+		matCtx.normal,
+		THIS_Roughness,
+		THIS_Albedo
+	);
+
+	vec3 col = baseColor;
+	col += matCtx.light.color * diffAmt * THIS_Diffuse;
+
+	float shadedAmt = 1.0;
+	#ifdef THIS_Enableshadow
+	#ifdef THIS_HAS_INPUT_2
+	shadedAmt = inputOp2(p+matCtx.normal*0.001, matCtx);
+	#else
+	shadedAmt = calcShadow(p+matCtx.normal*0.001, matCtx);
+	#endif
+	#endif
+	col *= shadedAmt;
+
+	col *= mix(vec3(0.5), vec3(1.5), occ);
+	return col;
+}
+
