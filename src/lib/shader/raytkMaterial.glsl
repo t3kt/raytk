8071b7c2b57b62a6519472d0d1285170443810b1
t3kt <tekt@immerse.studio>
2021-03-20 13:46:49 -0700

    basic implementation of ggx material
    see #412

--- src/lib/shader/raytkMaterial.glsl
+++ src/lib/shader/raytkMaterial.glsl
@@ -70,3 +70,19 @@ float orenNayarDiffuse(
 
 	return albedo * max(0.0, NdotL) * (A + B * s / t) / PI;
 }
+
+// https://github.com/glslify/glsl-ggx
+float ggx (vec3 N, vec3 V, vec3 L, float roughness, float F0) {
+	float alpha = roughness*roughness;
+	vec3 H = normalize(L - V);
+	float dotLH = max(0.0, dot(L,H));
+	float dotNH = max(0.0, dot(N,H));
+	float dotNL = max(0.0, dot(N,L));
+	float alphaSqr = alpha * alpha;
+	float denom = dotNH * dotNH * (alphaSqr - 1.0) + 1.0;
+	float D = alphaSqr / (3.141592653589793 * denom * denom);
+	float F = F0 + (1.0 - F0) * pow(1.0 - dotLH, 5.0);
+	float k = 0.5 * alpha;
+	float k2 = k * k;
+	return dotNL * D * F / (dotLH*dotLH*(1.0-k2)+k2);
+}
---
+++ src/operators/material/ggxMat.glsl
@@ -0,0 +1,32 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	Sdf res = inputOp1(p, ctx);
+	#if defined(THIS_Uselocalpos) && defined(RAYTK_USE_MATERIAL_POS)
+	assignMaterialWithPos(res, THISMAT, p);
+	#else
+	assignMaterial(res, THISMAT);
+	#endif
+	return res;
+}
+
+vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
+	vec3 lightDir = normalize(p - matCtx.light.pos);
+	vec3 viewDir = normalize(-matCtx.ray.dir);
+
+	float roughness = THIS_Roughness;
+	float f0 = THIS_F0;
+
+	vec3 baseColor = THIS_Basecolor;
+	float occ = calcAO(p, matCtx.normal);
+	float diffAmt = ggx(
+		matCtx.normal,
+		viewDir,
+		lightDir,
+		roughness,
+		f0);
+
+	vec3 col = baseColor;
+	col += matCtx.light.color * diffAmt * THIS_Diffuse;
+
+	col *= mix(vec3(0.5), vec3(1.5), occ);
+	return col;
+}
