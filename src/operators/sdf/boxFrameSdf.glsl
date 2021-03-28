c291fdbb29075dadd6cef8c212655b1d9958898c
t3kt <tekt@immerse.studio>
2021-03-28 16:15:33 -0700

    add field inputs for boxFrameSdf
    closes #433

--- src/operators/sdf/boxFrameSdf.glsl
+++ src/operators/sdf/boxFrameSdf.glsl
@@ -1,3 +1,11 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return createSdf(sdBoundingBox(p - THIS_Translate, THIS_Scale, THIS_Thickness));
+	vec3 scale = THIS_Scale;
+	#ifdef THIS_HAS_INPUT_1
+	scale *= vec3(inputOp1(p, ctx));
+	#endif
+	float thickness = THIS_Thickness;
+	#ifdef THIS_HAS_INPUT_2
+	thickness *= inputOp2(p, ctx);
+	#endif
+	return createSdf(sdBoundingBox(p - THIS_Translate, scale, thickness));
 }
