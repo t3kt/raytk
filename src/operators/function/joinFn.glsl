937c938e2bdecf98a5b9159936b7a88771d77187
t3kt <tekt@immerse.studio>
2021-04-04 15:41:34 -0700

    docs, tests, and fixes for joinFn

--- src/operators/function/joinFn.glsl
+++ src/operators/function/joinFn.glsl
@@ -1,26 +1,50 @@
 ReturnT thismap(float p, ContextT ctx) {
+	ReturnT res;
+	initDefVal(res);
 	#if THIS_INPUT_COUNT == 0
-		return 0.;
 	#elif THIS_INPUT_COUNT == 1
-		return THIS_INPUT_1(p, ctx);
-	#else
-		float n = float(THIS_INPUT_COUNT);
-		float i = p * n;
-
+		res = THIS_INPUT_1(p, ctx);
+	#elif defined(THIS_Fitrange)
+	{
+		float i = p * float(THIS_INPUT_COUNT);
 		if (i <= 1.) {
-			return THIS_INPUT_1(i, ctx);
+			res = THIS_INPUT_1(i, ctx);
 		}
-		if (i <= 2.) {
-			return THIS_INPUT_2(i - 1., ctx);
+		else if (i <= 2.) {
+			res = THIS_INPUT_2(i - 1., ctx);
 		}
 		#if THIS_INPUT_COUNT > 2
-			if (i <= 3.) {
+		else if (i <= 3.) {
 			return THIS_INPUT_3(i - 2., ctx);
 		}
 		#endif
 		#if THIS_INPUT_COUNT > 3
-		return THIS_INPUT_3(i - 4., ctx);
+		else {
+			res = THIS_INPUT_4(i - 3., ctx);
+		}
 		#endif
-		return 0.;
+	}
+	#else
+	{
+		float i = floor(p);
+		if (i < 1) {
+			res = THIS_INPUT_1(fract(p), ctx);
+		} else if (i < 2) {
+			res = THIS_INPUT_2(fract(p), ctx);
+		}
+		#if THIS_INPUT_COUNT > 2
+		else if (i == 2) {
+			res = THIS_INPUT_3(fract(p), ctx);
+		}
+		#endif
+		#if THIS_INPUT_COUNT > 3
+		else if (i == 3) {
+			res = THIS_INPUT_4(fract(p), ctx);
+		}
+		#endif
+		else {
+		}
+	}
 	#endif
+	return res;
 }
---
+++ src/operators/function/joinFn.md
@@ -0,0 +1,13 @@
+Joins functions end on end.
+
+## Parameters
+
+* `Enable`
+* `Fitrange`: When enabled, the combined function is scaled back to a 0..1 range. Otherwise, coordinates 0..1 go to the first function, 1..2 to the second, etc.
+
+## Inputs
+
+* `definition_in_1`: 
+* `definition_in_2`: 
+* `definition_in_3`: 
+* `definition_in_4`: 
