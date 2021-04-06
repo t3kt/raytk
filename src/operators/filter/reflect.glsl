b3ae7a1241e94aa562433c3d4e62d50d80c61b24
t3kt <tekt@immerse.studio>
2021-04-05 19:12:43 -0700

    added 2d support for reflect
    closes #439

--- src/operators/filter/reflect.glsl
+++ src/operators/filter/reflect.glsl
@@ -1,10 +1,17 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec3 planeNormal = normalize(THIS_AXIS_VEC);
-	p -= planeNormal * THIS_Shift * THIS_DIR;
-	float t = dot(p, planeNormal);
+	vec3 planeNormal = THIS_AXIS_VEC;
+	#ifdef THIS_COORD_TYPE_vec2
+	vec3 q = vec3(p, 0.);
+	planeNormal.z = 0.;
+	#else
+	vec3 q = p;
+	#endif
+	planeNormal = normalize(planeNormal);
+	q -= planeNormal * THIS_Shift * THIS_DIR;
+	float t = dot(q, planeNormal);
 
 	if (t < 0) {
-		p = p - (2.*t) * planeNormal;
+		q = q - (2.*t) * planeNormal;
 	}
 	float i = sgn(t) * THIS_DIR;
 	#if defined(THIS_Iterationtype_sign)
@@ -13,6 +20,11 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	//  (-1, 1)  --> (1, 0)
 	setIterationIndex(ctx, (i < 0) ? 1: 0);
 	#endif
-	p -= planeNormal * THIS_Offset * THIS_DIR;
+	q -= planeNormal * THIS_Offset * THIS_DIR;
+	#ifdef THIS_COORD_TYPE_vec2
+	p = q.xy;
+	#else
+	p = q;
+	#endif
 	return inputOp1(p, ctx);
 }
