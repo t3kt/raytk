e1c62efc35fd2536722e6e77b4bbf780d28bf252
t3kt <tekt@immerse.studio>
2021-04-20 20:38:11 -0700

    cleanup and improvements in coordTo2D
    see #214.
    not yet supporting 1D input though (see #475)

--- src/operators/convert/coordTo2D.glsl
+++ src/operators/convert/coordTo2D.glsl
@@ -1,3 +1,9 @@
 ReturnT thismap(vec2 p, ContextT ctx) {
-	return inputOp1(vec3(THIS_PART_X, THIS_PART_Y, THIS_PART_Z), ctx);
+	#ifdef THIS_Mode_custom
+	vec3 q = vec3(THIS_PART_X, THIS_PART_Y, THIS_PART_Z);
+	#else
+	vec3 q = vec3(THIS_Planeoffset);
+	q.THIS_Mode = p;
+	#endif
+	return inputOp1(q, ctx);
 }
