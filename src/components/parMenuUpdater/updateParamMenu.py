cf9a884fe38e1168b6b26cfbc5ae372e222fdef9
t3kt <tekt@immerse.studio>
2021-04-21 22:43:49 -0700

    avoid some warnings during the build process by checking attempted access to Hostop params

--- src/components/parMenuUpdater/updateParamMenu.py
+++ src/components/parMenuUpdater/updateParamMenu.py
@@ -1,14 +1,17 @@
 def update():
-	host = parent().par.Hostop.eval()
-	name = parent().par.Param.eval() if host else None
-	par = host.par[name] if name else None
-	if par is None:
-		return
-	dat = parent().par.Table.eval()
-	if not dat or dat.numRows < 2:
-		return
-	par.menuNames = dat.col('name')[1:]
-	par.menuLabels = dat.col('label')[1:]
+	try:
+		host = parent().par.Hostop.eval()
+		name = parent().par.Param.eval() if host else None
+		par = host.par[name] if name else None
+		if par is None:
+			return
+		dat = parent().par.Table.eval()
+		if not dat or dat.numRows < 2:
+			return
+		par.menuNames = dat.col('name')[1:]
+		par.menuLabels = dat.col('label')[1:]
+	except BaseException as e:
+		print(f'Error attempting to update paramMenu in {parent()}: {e}')
 
 
 def onValueChange(par, prev):
