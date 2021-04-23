92ec924c932670d2564694db25102ef9e9d098ad
t3kt <tekt@immerse.studio>
2021-04-22 20:08:59 -0700

    add 1d support in coordTo2D
    see #475

--- src/operators/convert/coordTo2D.glsl
+++ src/operators/convert/coordTo2D.glsl
@@ -5,5 +5,13 @@ ReturnT thismap(vec2 p, ContextT ctx) {
 	vec3 q = vec3(THIS_Planeoffset);
 	q.THIS_Mode = p;
 	#endif
+	#if defined(inputOp1_COORD_TYPE_float)
+	return inputOp1(q.x, ctx);
+	#elif defined(inputOp1_COORD_TYPE_vec3)
 	return inputOp1(q, ctx);
+	#elif defined(inputOp1_COORD_TYPE_vec2)
+	return inputOp1(q.xy, ctx);
+	#else
+	#error invalidInputCoordType
+	#endif
 }
