b394210cab44a7eaffede7ea7cfc2e75e2cda159
t3kt <tekt@immerse.studio>
2021-05-02 16:00:09 -0700

    fixed type handling issues in reshapeValues

--- src/operators/filter/reshapeValues.glsl
+++ src/operators/filter/reshapeValues.glsl
@@ -1,16 +1,25 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	ReturnT res = inputOp1(p, ctx);
+	ReturnT res;
+	inputOp1_ReturnT res1 = inputOp1(p, ctx);
 	#if !defined(THIS_HAS_INPUT_2)
+	res = res1;
 	#elif defined(THIS_RETURN_TYPE_float)
-	res = inputOp2(res, ctx);
+	res = ReturnT(inputOp2(inputOp2_CoordT(res1), ctx));
 	#elif defined(THIS_RETURN_TYPE_vec4)
-	res = ReturnT(
-		inputOp2(res.x, ctx),
-		inputOp2(res.y, ctx),
-		inputOp2(res.z, ctx),
-		inputOp2(res.w, ctx));
+	{
+		#ifdef inputOp2_RETURN_TYPE_vec4
+		res = inputOp2(res1, ctx);
+		#else
+		res = ReturnT(
+			inputOp2(res1.x, ctx),
+			inputOp2(res1.y, ctx),
+			inputOp2(res1.z, ctx),
+			inputOp2(res1.w, ctx));
+		#endif
+	}
 	#elif defined(THIS_RETURN_TYPE_Sdf)
-	res.x = inputOp2(res.x, ctx);
+	res = res1;
+	res.x = inputOp2(res1.x, ctx);
 	#else
 	#error invalidReturnType
 	#endif
