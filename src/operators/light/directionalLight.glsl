a2b2b5b950f64273a8277f84031b0832403d9fb8
t3kt <tekt@immerse.studio>
2021-05-02 16:03:04 -0700

    added color field input in directionalLight

--- src/operators/light/directionalLight.glsl
+++ src/operators/light/directionalLight.glsl
@@ -2,6 +2,9 @@ Light thismap(vec3 p, LightContext ctx) {
 	Light light;
 	light.pos = p + THIS_Direction;
 	light.color = THIS_Color * clamp(dot(ctx.normal, THIS_Direction), 0., 1.);
+	#ifdef THIS_HAS_INPUT_1
+	light.color *= fillToVec3(inputOp1(p, ctx));
+	#endif
 	return light;
 }
 
---
+++ src/operators/light/directionalLight.md
@@ -0,0 +1,13 @@
+A directional light.
+
+The light always comes from the specified direction, rather than from a point.
+
+## Parameters
+
+* `Direction`
+* `Intensity`
+* `Color`
+
+## Inputs
+
+* `color_field_definition_in`: 
