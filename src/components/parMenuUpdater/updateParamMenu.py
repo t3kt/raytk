295d85123208290fb1524ad255fcf9debc60fa3a
t3kt <tekt@immerse.studio>
2021-05-01 18:28:56 -0700

    add support for updating multiple pars in parMenuUpdater

--- src/components/parMenuUpdater/updateParamMenu.py
+++ src/components/parMenuUpdater/updateParamMenu.py
@@ -1,15 +1,22 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
 def update():
 	try:
 		host = parent().par.Hostop.eval()
-		name = parent().par.Param.eval() if host else None
-		par = host.par[name] if name else None
-		if par is None:
+		if not host:
 			return
 		dat = parent().par.Table.eval()
 		if not dat or dat.numRows < 2:
 			return
-		par.menuNames = dat.col('name')[1:]
-		par.menuLabels = dat.col('label')[1:]
+		names = [c.val for c in dat.col('name')[1:]]
+		labels = [c.val for c in dat.col('label')[1:]]
+		pars = host.pars(*(mod.tdu.split(parent().par.Param)))
+		for par in pars:
+			par.menuNames = names
+			par.menuLabels = labels
 	except BaseException as e:
 		print(f'Error attempting to update paramMenu in {parent()}: {e}')
 
