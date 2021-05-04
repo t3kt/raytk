998f4df4751992f7f52d2ace563ea64d8e03d5c1
t3kt <tekt@immerse.studio>
2021-05-03 19:21:43 -0700

    fix 2d coord issue and some cleanup in helixSdf

--- src/lib/shader/raytkSdf.glsl
+++ src/lib/shader/raytkSdf.glsl
@@ -295,24 +295,6 @@ float sdCappedCone(vec3 p, float h, float r1, float r2)
 	return s*sqrt( min(dot2(ca),dot2(cb)) );
 }
 
-//Eiffie
-vec2 sdHelixCoords(vec3 p, float r1, float m, float dualSpread) {
-	float halfm = m*.5,
-	b = mod(p.y, PI*m) - PI*halfm,
-	a = abs(atan(p.x, p.z) * halfm - b);
-	if (a > PI*halfm) a = PI*m - a;
-
-	//optimisation from Shane
-	p.xy = vec2(length(p.xz) - r1, a);
-	p.x = abs(p.x) - dualSpread;
-	return p.xy;
-}
-
-float sdHelix(vec3 p, float r1, float r2, float m, float dualSpread) {
-	vec2 q = sdHelixCoords(p, r1, m, dualSpread);
-	return length(q) - r2;
-}
-
 float sdSegment( in vec2 p, in vec2 a, in vec2 b )
 {
 	vec2 pa = p-a, ba = b-a;
--- src/operators/sdf/helixSdf.glsl
+++ src/operators/sdf/helixSdf.glsl
@@ -1,44 +1,76 @@
+#ifndef RAYTK_HELIX
+#define RAYTK_HELIX
+
+//Eiffie
+vec2 sdHelixCoords(vec3 p, float r1, float m, float dualSpread) {
+	float halfm = m*.5,
+	b = mod(p.y, PI*m) - PI*halfm,
+	a = abs(atan(p.x, p.z) * halfm - b);
+	if (a > PI*halfm) a = PI*m - a;
+
+	//optimisation from Shane
+	p.xy = vec2(length(p.xz) - r1, a);
+	p.x = abs(p.x) - dualSpread;
+	return p.xy;
+}
+
+#endif
+
 ReturnT thismap(CoordT p, ContextT ctx) {
+	ReturnT res;
 	p -= THIS_Translate;
 	p = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
 	float thickness = THIS_Thickness;
 	float radius = THIS_Radius;
 	#ifdef THIS_HAS_INPUT_1
-	#if defined(inputOp1_COORD_TYPE_float)
-	thickness *= inputOp1(p.y, ctx);
-	#elif defined(inputOp1_COORD_TYPE_vec2)
-	thickness *= inputOp1(vec2(p.y, degrees(atan(p.z, p.x))), ctx);
-	#elif defined(inputOp1_COORD_TYPE_vec3)
-	thickness *= inputOp1(p, ctx);
-	#endif
+  {
+		#if defined(inputOp1_COORD_TYPE_float)
+		thickness *= inputOp1(p.y, ctx);
+		#elif defined(inputOp1_COORD_TYPE_vec2)
+		thickness *= inputOp1(vec2(p.y, atan(p.z, p.x)), ctx);
+		#elif defined(inputOp1_COORD_TYPE_vec3)
+		thickness *= inputOp1(p, ctx);
+		#else
+		#error invalidThicknessCoordType
+		#endif
+	}
 	#endif
 	#ifdef THIS_HAS_INPUT_2
-	#if defined(inputOp2_COORD_TYPE_float)
-	radius *= inputOp2(p.y, ctx);
-	#elif defined(inputOp2_COORD_TYPE_vec3)
-	radius *= inputOp2(p, ctx);
-	#endif
+	{
+		#if defined(inputOp2_COORD_TYPE_float)
+		radius *= inputOp2(p.y, ctx);
+		#elif defined(inputOp2_COORD_TYPE_vec3)
+		radius *= inputOp2(p, ctx);
+		#else
+		#error invalidRadiusCoordType
+		#endif
+	}
 	#endif
 	vec2 q = sdHelixCoords(p, radius, THIS_Spread, THIS_Dualspread * radius);
 	#if !defined(THIS_HAS_INPUT_3)
-	return createSdf(length(q) - thickness);
+	res = createSdf(length(q) - thickness);
 	#elif defined(inputOp3_RETURN_TYPE_Sdf)
+	{
 		#if defined(THIS_RETURN_TYPE_Sdf)
-			return inputOp3(q, ctx);
+		res = inputOp3(q, ctx);
 		#elif defined(THIS_RETURN_TYPE_float)
-			return createSdf(inputOp3(q, ctx));
+		res = createSdf(inputOp3(q, ctx));
 		#else
-			#error invalidShapeReturnType
+		#error invalidShapeReturnType
 		#endif
+	}
 	#elif defined(inputOp3_RETURN_TYPE_float)
+	{
 		#if defined(THIS_RETURN_TYPE_float)
-			return inputOp3(q, ctx);
+		res = inputOp3(q, ctx);
 		#elif defined(THIS_RETURN_TYPE_Sdf)
-			return inputOp3(q, ctx).x;
+		res = inputOp3(q, ctx).x;
 		#else
-			#error invalidShapeReturnType
+		#error invalidShapeReturnType
 		#endif
+	}
 	#else
-	return createSdf(length(q) - thickness);
+	res = createSdf(length(q) - thickness);
 	#endif
+	return res;
 }
