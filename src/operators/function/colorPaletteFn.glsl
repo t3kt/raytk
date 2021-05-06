1c852439ccc576e2fed83aeb19b8f2f1feece17e
t3kt <tekt@immerse.studio>
2021-05-05 20:19:03 -0700

    add period and phase parameters in colorPalette
    closes #486

--- src/operators/function/colorPaletteFn.glsl
+++ src/operators/function/colorPaletteFn.glsl
@@ -1,3 +1,3 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return vec4(cosPalette(p, THIS_Color1, THIS_Color2, THIS_Color3, THIS_Color4), 1.0);
+	return vec4(cosPalette((p - THIS_Phase) / THIS_Period, THIS_Color1, THIS_Color2, THIS_Color3, THIS_Color4), 1.0);
 }
