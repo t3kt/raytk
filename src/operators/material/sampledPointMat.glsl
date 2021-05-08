1ac307a04408626516cc883847ee9db45cf14d28
t3kt <tekt@immerse.studio>
2021-05-08 12:49:10 -0700

    Added edge color support to sampledPointMat
    see #492

--- src/operators/material/sampledPointMat.glsl
+++ src/operators/material/sampledPointMat.glsl
@@ -15,17 +15,37 @@ vec3 THIS_getColor(CoordT p, MaterialContext matCtx) {
 	restoreIterationFromMaterial(matCtx, THIS_iterationCapture);
 	vec3 mp = getPosForMaterial(p, matCtx);
 	vec3 col = vec3(0.);
-	vec3 fillColor = THIS_Fillcolor;
-	#ifdef THIS_HAS_INPUT_2
+	#ifdef THIS_Enablefill
 	{
-		#if defined(inputOp2_COORD_TYPE_float)
-		float q = matCtx.result.x;
-		#else
-		inputOp2_CoordT q = inputOp2_asCoordT(mp);
-		#endif
-		fillColor *= fillToVec3(inputOp2(q, matCtx));
+		vec3 fillColor = THIS_Fillcolor;
+		#ifdef THIS_HAS_INPUT_2
+		{
+			#if defined(inputOp2_COORD_TYPE_float)
+			float q = -matCtx.result.x;
+			#else
+			inputOp2_CoordT q = inputOp2_asCoordT(mp);
+			#endif
+			fillColor *= fillToVec3(inputOp2(q, matCtx));
+		}
+			#endif
+		col += fillColor * (1.0 - smoothstep(0, THIS_Blending, max(matCtx.result.x, 0.)));
 	}
 	#endif
-	col += fillColor * smoothstep(-THIS_Blending/2., THIS_Blending/2., -matCtx.result.x);
+	#ifdef THIS_Enableedge
+	{
+		vec3 edgeColor = THIS_Edgecolor;
+		#ifdef THIS_HAS_INPUT_3
+		{
+			#if defined(inputOp3_COORD_TYPE_float)
+			float q = -matCtx.result.x;
+			#else
+			inputOp3_CoordT q = inputOp3_asCoordT(mp);
+			#endif
+			edgeColor *= fillToVec3(inputOp3(q, matCtx));
+		}
+		#endif
+		col += edgeColor * (1.0 - smoothstep(THIS_Edgethickness - THIS_Blending / 2., THIS_Edgethickness + THIS_Blending/2., abs(matCtx.result.x)));
+	}
+	#endif
 	return col;
 }
