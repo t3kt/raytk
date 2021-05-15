64d4b1714728b5e08e234cdd360d57a7c3e4d404
t3kt <tekt@immerse.studio>
2021-05-15 14:55:37 -0700

    unidirectional limit support in modulo1D
    closes #309

--- src/operators/filter/modulo1D.glsl
+++ src/operators/filter/modulo1D.glsl
@@ -1,15 +1,39 @@
+
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	float q = p.THIS_Axis + THIS_Shift;
-	#ifdef THIS_Uselimit
-	float cell = THIS_FUNC(q, THIS_Size, THIS_Limitstart, THIS_Limitstop);
-	#else
-	float cell = THIS_FUNC(q, THIS_Size);
+	float size = THIS_Size;
+	float halfsize = size*0.5;
+	float c = floor((q + halfsize)/size);
+	q = mod(q+halfsize, size) - halfsize;
+	#if defined(THIS_Uselimit)
+	{
+		float start = THIS_Limitstart;
+		float stop = THIS_Limitstop;
+		#if defined(THIS_Limittype_stop) || defined(THIS_Limittype_both)
+		if (c > stop) {
+			q += size * (c - stop);
+			c = stop;
+		}
+			#endif
+		#if defined(THIS_Limittype_start) || defined(THIS_Limittype_both)
+		if (c < start) {
+			q += size * (c - start);
+			c = start;
+		}
+		#endif
+	}
 	#endif
+
+	#ifdef THIS_Mirrortype_mirror
+	q *= mod(c, 2.0)*2 - 1;
+	#endif
+
 	p.THIS_Axis = q - THIS_Offset;
 	#if defined(THIS_Iterationtype_cellcoord)
-	setIterationIndex(ctx, cell);
+	setIterationIndex(ctx, c);
 	#elif defined(THIS_Iterationtype_alternatingcoord)
-	setIterationIndex(ctx, mod(cell, 2.));
+	setIterationIndex(ctx, mod(c, 2.));
 	#endif
 	return inputOp1(p, ctx);
 }
