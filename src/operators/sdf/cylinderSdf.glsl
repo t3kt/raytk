8a87997d4c5ac6bc11a82bc809c21ab368a2e8ab
t3kt <tekt@immerse.studio>
2021-05-16 19:11:27 -0700

    Add field inputs for cylinderSdf
    closes #508

--- src/operators/sdf/cylinderSdf.glsl
+++ src/operators/sdf/cylinderSdf.glsl
@@ -1,5 +1,17 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Translate;
-	p = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
-	return createSdf(fCylinder(p, THIS_Radius, THIS_Height));
+	vec3 q = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
+	float r = THIS_Radius;
+	#if defined(THIS_HAS_INPUT_1) && defined(inputOp1_COORD_TYPE_float)
+	r *= inputOp1(q.y, ctx);
+	#elif defined(THIS_HAS_INPUT_1) && defined(inputOp1_COORD_TYPE_vec3)
+	r *= inputOp1(p, ctx);
+	#endif
+	float h = THIS_Height;
+	#if defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_float)
+	h *= inputOp2(q.y, ctx);
+	#elif defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_vec3)
+	h *= inputOp2(p, ctx);
+	#endif
+	return createSdf(fCylinder(q, r, h));
 }
