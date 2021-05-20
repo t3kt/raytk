531c87edacee9f8d90f4a6911bea04c00defc831
t3kt <tekt@immerse.studio>
2021-05-19 18:29:02 -0700

    improvements in archSdf
    closes #510

--- src/operators/sdf/archSdf.glsl
+++ src/operators/sdf/archSdf.glsl
@@ -14,14 +14,18 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 
 	#ifdef THIS_Enableframe
 	{
-		float d0 = sdRoundedBox(p.xy, vec2(THIS_Width, THIS_Height), r);
+		float d0 = sdRoundedBox(p.xy, vec2(THIS_Width, THIS_Height + THIS_Framethickness*2.), r);
 		// Onion
 		float d1 = onion(d0, THIS_Framethickness);
+
+		// Crop  -p.y + THIS_Offset
+		float d2 = -p.y -THIS_Height;
+
 		// Extrude
 		vec2 w = vec2(d1, abs(p.z) - THIS_Framedepth);
 		d1 = min(max(w.x, w.y), 0.0) + length(max(w, 0.0));
 
-		d = min(d, d1);
+		d = min(d, max(d1, d2));
 	}
 	#endif
 
