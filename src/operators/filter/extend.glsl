6943ea7aaebc8c4e10ce18c8d4c74fbf52a56ccc
t3kt <tekt@immerse.studio>
2021-05-20 20:44:39 -0700

    cleanup in elongate and extend

--- src/operators/filter/extend.glsl
+++ src/operators/filter/extend.glsl
@@ -1,8 +1,4 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#if defined(THIS_COORD_TYPE_float)
-	p = clamp(p, THIS_Centerx-THIS_Sizex*0.5, THIS_Centerx+THIS_Sizex*0.5);
-	#else
-	p = clamp(p, THIS_Center-THIS_Size*0.5, THIS_Center+THIS_Size*0.5);
-	#endif
+	p = clamp(p, THIS_asCoordT(THIS_Center - THIS_Size * 0.5), THIS_asCoordT(THIS_Center + THIS_Size * 0.5));
 	return inputOp1(p, ctx);
 }
