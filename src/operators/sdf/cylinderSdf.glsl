71c00318cfc6eb4dd0b1b7f590654ecc003ecedf
t3kt <tekt@immerse.studio>
2021-05-26 17:43:11 -0700

    added infinite height option in cylinderSdf
    close #520

--- src/operators/sdf/cylinderSdf.glsl
+++ src/operators/sdf/cylinderSdf.glsl
@@ -7,11 +7,21 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#elif defined(THIS_HAS_INPUT_1) && defined(inputOp1_COORD_TYPE_vec3)
 	r *= inputOp1(p, ctx);
 	#endif
-	float h = THIS_Height;
-	#if defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_float)
-	h *= inputOp2(q.y, ctx);
-	#elif defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_vec3)
-	h *= inputOp2(p, ctx);
+	ReturnT res;
+	#ifdef THIS_Infiniteheight
+	{
+		res = createSdf(fCylinder(q * vec3(1., 0., 1.), r, 1.));
+	}
+	#else
+	{
+		float h = THIS_Height;
+		#if defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_float)
+		h *= inputOp2(q.y, ctx);
+		#elif defined(THIS_HAS_INPUT_2) && defined(inputOp2_COORD_TYPE_vec3)
+		h *= inputOp2(p, ctx);
+		#endif
+		res = createSdf(fCylinder(q, r, h));
+	}
 	#endif
-	return createSdf(fCylinder(q, r, h));
+	return res;
 }
