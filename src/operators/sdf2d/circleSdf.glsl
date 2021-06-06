41fb58a7be22110b901f0ce3340a5dfb5f0d1700
t3kt <tekt@immerse.studio>
2021-06-05 20:26:20 -0700

    uv coords for some 2d sdfs
    see #526

--- src/operators/sdf2d/circleSdf.glsl
+++ src/operators/sdf2d/circleSdf.glsl
@@ -1,3 +1,14 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return createSdf(length(p - THIS_Translate)-THIS_Radius);
+	p -= THIS_Translate;
+	ReturnT res = createSdf(length(p)-THIS_Radius);
+	#if defined(THIS_Uvmode_cartesian)
+	assignUV(res, vec3(map01(p, -vec2(THIS_Radius/2.), vec2(THIS_Radius/2.)), 0.));
+	#elif defined(THIS_Uvmode_polar)
+	assignUV(res, vec3(
+		length(p) / THIS_Radius,
+		atan(p.y, p.x),
+		0.
+	));
+	#endif
+	return res;
 }
