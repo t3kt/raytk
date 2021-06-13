69bc765f31c0c93db9e71e48c59998268d42edec
t3kt <tekt@immerse.studio>
2021-06-13 14:56:14 -0700

    added mirrorQuadrant
    close #558

---
+++ src/operators/filter/mirrorQuadrant.glsl
@@ -0,0 +1,60 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	vec2 q = p.THIS_PLANE;
+	vec2 cell = sgn(q);
+	pMirror(q.x, THIS_Size1);
+	pMirror(q.y, THIS_Size2);
+	#if defined(THIS_Iterationtype_sign)
+	setIterationCell(ctx, cell);
+	#elif defined(THIS_Iterationtype_index)
+	setIterationIndex(ctx, quadrantIndex(ivec2(cell)));
+	#endif
+
+	float r = THIS_Rotateaxis;
+	#ifdef THIS_HAS_INPUT_2
+	{
+		#if defined(inputOp2_COORD_TYPE_float)
+		float q2 = length(p.THIS_PLANE);
+		#elif defined(inputOp2_COORD_TYPE_vec2)
+		vec2 q2 = p.THIS_PLANE;
+		#elif defined(inputOp2_COORD_TYPE_vec3)
+		vec3 q2 = p;
+		#else
+		#error invalidRotateAxisCoordType
+		#endif
+
+		#if defined(inputOp2_RETURN_TYPE_Sdf)
+		r += radians(inputOp2(q2, ctx).x);
+		#elif defined(inputOp2_RETURN_TYPE_float)
+		r += radians(inputOp2(q2, ctx));
+		#else
+		#error invalidRotateAxisReturnType
+		#endif
+	}
+		#endif
+	pR(q, r);
+
+	vec2 offset = THIS_Offset;
+
+	#ifdef THIS_HAS_INPUT_3
+	{
+		#if defined(inputOp3_COORD_TYPE_float)
+		float q3 = p.THIS_AXIS;
+		#elif defined(inputOp3_COORD_TYPE_vec3)
+		vec3 q3 = p;
+		#else
+		#error invalidOffsetCoordType
+		#endif
+
+		#if defined(inputOp3_RETURN_TYPE_float)
+		offset += vec2(inputOp3(q3, ctx));
+		#elif defined(inputOp3_RETURN_TYPE_vec4)
+		offset += inputOp3(q3, ctx).xy;
+		#else
+		#error invalidOffsetReturnType
+		#endif
+	}
+		#endif
+
+	p.THIS_PLANE = q - offset;
+	return inputOp1(p, ctx);
+}
