a43b39e832af37106fa955fb0ff4c3f0dea6b96b
t3kt <tekt@immerse.studio>
2021-06-22 20:56:00 -0700

    add field inputs for extrude
    close #567

--- src/operators/convert/extrude.glsl
+++ src/operators/convert/extrude.glsl
@@ -2,11 +2,22 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p.THIS_PLANE, ctx);
 	#ifndef THIS_Infiniteheight
 	{
-		vec2 w = vec2(res.x, abs(p.THIS_AXIS - THIS_Offset) - THIS_Height);
+		#ifdef THIS_HAS_INPUT_2
+		float h = inputOp2(p, ctx);
+		#else
+		float h = THIS_Height;
+		#endif
+		#ifdef THIS_HAS_INPUT_3
+		float o = inputOp3(p, ctx);
+		#else
+		float o = THIS_Offset;
+		#endif
+
+		vec2 w = vec2(res.x, abs(p.THIS_AXIS - o) - h);
 		res.x = min(max(w.x,w.y), 0.0) + length(max(w, 0.0));
 		#if defined(RAYTK_USE_UV) && defined(THIS_Uvmode_depth)
 		// Replace UV.Y if UV has been set already
-		float w0 = map01(p.THIS_AXIS - THIS_Offset, -THIS_Height/2., THIS_Height/2.);
+		float w0 = map01(p.THIS_AXIS - o, -h/2., h/2.);
 		res.uv.y = mix(res.uv.y, w.y, w0);
 		#endif
 	}
