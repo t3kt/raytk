bad794fadf78fddca958b65434c0de6df431f40a
t3kt <tekt@immerse.studio>
2021-06-24 18:54:55 -0700

    ratio-based iteration options for extrude and revolve
    sort of stretching the term "iteration".
    close #556, #567

--- src/operators/convert/revolve.glsl
+++ src/operators/convert/revolve.glsl
@@ -2,6 +2,9 @@ ReturnT thismap(vec3 p, ContextT ctx) {
 	p.THIS_AXIS -= THIS_Axisoffset;
 	vec2 q = vec2(length(p.THIS_PLANE) - THIS_Radialoffset, p.THIS_AXIS);
 	float a = atan(p.THIS_PLANE_P2, p.THIS_PLANE_P1) / TAU;
+	#ifdef THIS_Iterationtype_ratio
+	setIterationIndex(ctx, a);
+	#endif
 	#ifdef THIS_HAS_INPUT_2
 	float r = inputOp2(a, ctx);
 	pR(q, radians(r));
