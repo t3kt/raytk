c8f3e011aa49e9558285c07ebb3a4c36ef299668
t3kt <tekt@immerse.studio>
2021-07-01 22:46:00 -0700

    added field inputs to rectangleSdf
    see #588

--- src/operators/sdf2d/rectangleSdf.glsl
+++ src/operators/sdf2d/rectangleSdf.glsl
@@ -1,9 +1,17 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	p -= THIS_Translate;
-	vec2 d = abs(p)-THIS_Scale;
+	CoordT t = THIS_Translate;
+	CoordT s = THIS_Scale;
+	#ifdef THIS_HAS_INPUT_1
+	s *= THIS_asCoordT(inputOp1(p, ctx));
+	#endif
+	#ifdef THIS_HAS_INPUT_2
+	t += THIS_asCoordT(inputOp2(p, ctx));
+	#endif
+	p -= t;
+	vec2 d = abs(p)-s;
 	ReturnT res = createSdf(length(max(d,0.0)) + min(max(d.x,d.y),0.0));
 	#ifdef RAYTK_USE_UV
-	assignUV(res, vec3(map01(p, -THIS_Scale/2., THIS_Scale/2.), 0.));
+	assignUV(res, vec3(map01(p, -s/2., s/2.), 0.));
 	#endif
 	return res;
 }
