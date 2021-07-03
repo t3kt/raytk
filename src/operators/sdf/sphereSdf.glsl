20664248382f246b1dfa0792830d5bb7f027ecd0
t3kt <tekt@immerse.studio>
2021-07-02 17:45:45 -0700

    UVs for sphereSdf
    see #526

--- src/operators/sdf/sphereSdf.glsl
+++ src/operators/sdf/sphereSdf.glsl
@@ -3,5 +3,18 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#ifdef THIS_HAS_INPUT_1
 	r *= inputOp1(p, ctx);
 	#endif
-	return createSdf(length(p - THIS_Translate)-r);
+	p -= THIS_Translate;
+	ReturnT res = createSdf(length(p)-r);
+	#if defined(THIS_Uvmode_bounds)
+	assignUV(res, map01(p, vec3(-r), vec3(r)));
+	#elif defined(THIS_Uvmode_spherical)
+	assignUV(
+		res,
+		vec3(
+			res.x,
+			acos(p.z / res.x),
+			atan(p.y, p.x)
+		));
+	#endif
+	return res;
 }
