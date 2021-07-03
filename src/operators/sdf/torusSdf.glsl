1f9f6480114397c4e0a8aae28e1cc63099d71e2c
t3kt <tekt@immerse.studio>
2021-07-03 12:57:45 -0700

    got torusSdf uvs working
    see #526

--- src/operators/sdf/torusSdf.glsl
+++ src/operators/sdf/torusSdf.glsl
@@ -17,34 +17,14 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = createSdf(fTorus(p, THIS_Thickness, THIS_Radius));
 	#endif
 	#ifdef THIS_Uvmode_torus
-	vec3 uv;
-	float a = atan(p.x, p.z);
-	uv.x = a/TAU + 0.5;
-	/*
-	x = r * cos(a)
-	y = r * sin(a)
-
-
-
-	*/
-
-//	vec2 offset = vec2(
-//		THIS_Radius * cos(a),
-//		THIS_Radius * sin(a)
-//	);
-//	p.xz -= offset;
-	pR(p.xz, -a);
-	p.x -= THIS_Radius;
-
-//	uv.y = pow(length(p.yx), 6);
-	uv.y = 1.0-step(0.9, length(p));
-
-
-//	uv.y = atan(p.y, p.x)/TAU + 0.5;
-//	uv.z = length(length(p.xz) - THIS_Thickness);
+	float d0 = length(p.xz) - THIS_Radius;
 	assignUV(
 		res,
-		uv
+		vec3(
+			atan(p.x, p.z)/TAU + 0.5, // around axis
+			atan(d0, p.y), // around core
+			length(vec2(d0, p.y)) / THIS_Thickness // dist from core
+		)
 	);
 	#endif
 	return res;
