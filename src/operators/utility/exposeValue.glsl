46b11b1c21fd5aa7bd23a4e30be2fa083c00b0e1
t3kt <tekt@immerse.studio>
2021-07-11 16:31:17 -0700

    some work on exposeValue
    not yet working fully.
    
    see #499

--- src/operators/utility/exposeValue.glsl
+++ src/operators/utility/exposeValue.glsl
@@ -1,33 +1,45 @@
-bool THIS_check() {
-	#if !defined(THIS_Includeprimary) || !defined(THIS_Includeshadow) || !defined(THIS_Includereflect) || !defined(THIS_Includematerial) || !defined(THIS_Includeocclusion)
-	int s = getStage();
-	return false ||
-	#ifdef THIS_Includeprimary
-	(s == RAYTK_STAGE_PRIMARY) ||
+bool THIS_check(ReturnT res, ContextT ctx) {
+	if (getStage() != THIS_RENDER_STAGE) return false;
+	#if defined(THIS_Onlyonsurfacehit) && defined(THIS_RETURN_TYPE_Sdf)
+	if (isNonHitSdf(res)) return false;
 	#endif
-	#ifdef THIS_Includeshadow
-	(s == RAYTK_STAGE_SHADOW) ||
-	#endif
-	#ifdef THIS_Includereflect
-	(s == RAYTK_STAGE_REFLECT) ||
-	#endif
-	#ifdef THIS_Includematerial
-	(s == RAYTK_STAGE_MATERIAL) ||
-	#endif
-	#ifdef THIS_Includeocclusion
-	(s == RAYTK_STAGE_OCCLUSION) ||
-	#endif
-	false
-	;
-	#else
 	return true;
-	#endif
 }
 
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p, ctx);
-	if (THIS_check()) {
+	if (THIS_check(res, ctx)) {
+		#ifdef THIS_HAS_INPUT_2
+		inputOp2_ReturnT valRes = inputOp2(p, ctx);
+		#else
+		ReturnT valRes = res;
+		#endif
+		vec4 val;
+		#if defined(THIS_Valuesource_coords)
+		{
+			val = vec4(adaptAsVec3(p), 1.);
+		}
+		#elif defined(THIS_Valuesource_fieldvalue)
+		{
+			#if defined(THIS_RETURN_TYPE_vec4) || defined(THIS_RETURN_TYPE_float)
+			val = adaptAsVec4(res);
+			#else
+			#error fieldValueNotSupportedForReturnType
+			#endif
+		}
+		#elif defined(THIS_Valuesource_iteration)
+		{
+			#if defined(THIS_CONTEXT_TYPE_Context) || defined(THIS_CONTEXT_TYPE_MaterialContext)
+			val = extractIteration(ctx);
+			#else
+			#error iterationNotSupportedForContextType
+			#endif
+		}
+		#else
+		#error invalidValueSource
+		#endif
 
+		THIS_Outputbuffer = val;
 	}
 	return res;
 }
