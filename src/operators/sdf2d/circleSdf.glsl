b66384d85d363d858552ff5548ceaa75859cd399
t3kt <tekt@immerse.studio>
2021-07-20 21:09:10 -0700

    added field input for circleSdf
    see #588

--- src/operators/sdf2d/circleSdf.glsl
+++ src/operators/sdf2d/circleSdf.glsl
@@ -1,11 +1,15 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Translate;
-	ReturnT res = createSdf(length(p)-THIS_Radius);
+	float r = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_1
+	r *= inputOp1(p, ctx);
+	#endif
+	ReturnT res = createSdf(length(p)-r);
 	#if defined(THIS_Uvmode_cartesian)
-	assignUV(res, vec3(map01(p, -vec2(THIS_Radius/2.), vec2(THIS_Radius/2.)), 0.));
+	assignUV(res, vec3(map01(p, -vec2(r/2.), vec2(r/2.)), 0.));
 	#elif defined(THIS_Uvmode_polar)
 	assignUV(res, vec3(
-		length(p) / THIS_Radius,
+		length(p) / r,
 		atan(p.y, p.x),
 		0.
 	));
