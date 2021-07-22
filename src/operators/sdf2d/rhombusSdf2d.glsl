87b5f4b9426ed21d7b885e7d55ce14a0b66f0369
t3kt <tekt@immerse.studio>
2021-07-21 18:24:37 -0700

    added field inputs for rhombusSdf2d, roundedRectangleSdf2d
    see #588

--- src/operators/sdf2d/rhombusSdf2d.glsl
+++ src/operators/sdf2d/rhombusSdf2d.glsl
@@ -1,5 +1,8 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 b = THIS_Scale;
+	#ifdef THIS_HAS_INPUT_1
+	b *= fillToVec2(inputOp1(p, ctx));
+	#endif
 	vec2 q = abs(p);
 	float h = clamp((-2.0*ndot(q,b)+ndot(b,b))/dot(b,b),-1.0,1.0);
 	float d = length( q - 0.5*b*vec2(1.0-h,1.0+h) );
