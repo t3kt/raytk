7baee40c981e6dae96e546f355af937359f90503
t3kt <tekt@immerse.studio>
2021-08-01 13:56:54 -0700

    finished the kink operator

--- src/operators/filter/kink.glsl
+++ src/operators/filter/kink.glsl
@@ -1,10 +1,18 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 q = p.THIS_Direction;
-	float amt = THIS_Amount;
-	float c1 = THIS_C1;
-	float c2 = THIS_C2;
+	float amt = -THIS_Amount + 1.;
+	float c1 = THIS_Offset;
+	float c2 = -THIS_Spread;
+
+	#ifdef THIS_Side_pos
+	q.y *= -1.;
+	#endif
 
 	q = opKink(q, vec2(c1, c2), amt);
+
+	#ifdef THIS_Side_pos
+	q.y *= -1.;
+	#endif
 
 	p.THIS_Direction = q;
 
