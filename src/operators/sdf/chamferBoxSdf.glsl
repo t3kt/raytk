494dca8b5fd6e12a3baaef5a2c8340e4e55238ff
t3kt <tekt@immerse.studio>
2021-08-13 14:18:23 -0700

    added field inputs to chamferBoxSdf
    close #627

--- src/operators/sdf/chamferBoxSdf.glsl
+++ src/operators/sdf/chamferBoxSdf.glsl
@@ -2,6 +2,20 @@
 // https://www.shadertoy.com/view/3lBGzt
 
 ReturnT thismap(CoordT p, ContextT ctx) {
-	p = abs(p - THIS_Translate) + vec3(THIS_Chamfer) + vec3(THIS_Round);
-	return createSdf(sdOctahedron(max(vec3(0), p-THIS_Scale * THIS_Uniformscale), THIS_Chamfer) - THIS_Round);
+	vec3 s = THIS_Scale;
+	#ifdef THIS_HAS_INPUT_1
+	s *= fillToVec3(inputOp1(p, ctx));
+	#endif
+	#ifdef THIS_HAS_INPUT_2
+	float c = inputOp2(p, ctx);
+	#else
+	float c = THIS_Chamfer;
+	#endif
+	#ifdef THIS_HAS_INPUT_3
+	float r = inputOp3(p, ctx);
+	#else
+	float r = THIS_Round;
+	#endif
+	p = abs(p - THIS_Translate) + vec3(c) + vec3(r);
+	return createSdf(sdOctahedron(max(vec3(0), p-s * THIS_Uniformscale), c) - r);
 }
