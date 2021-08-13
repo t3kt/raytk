c248e451e1a1a1017458a10c559c6137d670a948
t3kt <tekt@immerse.studio>
2021-08-12 23:13:35 -0700

    clean up scale
    close #623

--- src/operators/filter/scale.glsl
+++ src/operators/filter/scale.glsl
@@ -1,34 +1,21 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	#if defined(THIS_Scaletype_uniform)
 	float scale = THIS_Uniformscale;
+	#ifdef THIS_HAS_INPUT_2
+	scale *= adaptAsFloat(inputOp2(p, ctx));
+	#endif
+	float adjust = scale;
 	#elif defined(THIS_Scaletype_separate)
 	CoordT scale = THIS_asCoordT(THIS_Scale);
+	#ifdef THIS_HAS_INPUT_2
+	scale *= THIS_asCoordT(fillToVec3(inputOp2(p, ctx)));
+	#endif
+	float adjust = vmin(scale);
 	#else
 	#error invalidScaleType
 	#endif
-	#ifdef THIS_HAS_INPUT_2
-		#if defined(inputOp2_RETURN_TYPE_Sdf) || defined(inputOp2_RETURN_TYPE_float)
-			scale *= adaptAsFloat(inputOp2(p, ctx));
-		#elif defined(inputOp2_RETURN_TYPE_vec4)
-			#ifdef THIS_Scaletype_uniform
-				#error scaleTypeNotSupportedForVec4Field
-			#else
-				#if defined(THIS_COORD_TYPE_vec2) || defined(THIS_COORD_TYPE_vec3)
-					scale *= THIS_asCoordT(inputOp2(p, ctx));
-				#else
-					#error invalidCoordType
-				#endif
-			#endif
-		#else
-			#error invalidFieldReturnType
-		#endif
-	#endif
+
 	ReturnT res = inputOp1(p / scale, ctx);
-	#if defined(THIS_Scaletype_uniform) || defined(THIS_COORD_TYPE_float)
-	float adjust = scale;
-	#else
-	float adjust = vmin(scale);
-	#endif
 	#ifdef THIS_RETURN_TYPE_float
 	res *= adjust;
 	#else
