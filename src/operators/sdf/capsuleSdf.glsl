214978d64cd33e123cb858ac45f5bd7bf3719b9c
t3kt <tekt@immerse.studio>
2021-08-13 14:29:04 -0700

    added field inputs to capsuleSdf

--- src/operators/sdf/capsuleSdf.glsl
+++ src/operators/sdf/capsuleSdf.glsl
@@ -1,3 +1,17 @@
 Sdf thismap(CoordT p, ContextT ctx) {
-	return createSdf(fCapsule(p - THIS_Translate, THIS_Endpoint1, THIS_Endpoint2, THIS_Radius));
+	#ifdef THIS_HAS_INPUT_1
+	vec3 pt1 = inputOp1(p, ctx).xyz;
+	#else
+	vec3 pt1 = THIS_Endpoint1;
+	#endif
+	#ifdef THIS_HAS_INPUT_2
+	vec3 pt2 = inputOp2(p, ctx).xyz;
+	#else
+	vec3 pt2 = THIS_Endpoint2;
+	#endif
+	float r = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_3
+	r *= inputOp3(p, ctx);
+	#endif
+	return createSdf(fCapsule(p - THIS_Translate, pt1, pt2, r));
 }
