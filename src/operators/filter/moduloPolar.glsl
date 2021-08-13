44b7d73e0af417dceecc0ea141d9e1b84db99918
t3kt <tekt@immerse.studio>
2021-08-12 20:47:52 -0700

    add field inputs and iteration modes to moduloPolar
    breaking change: replaced the `Iterateoncells` toggle with `Iterationtype` menu.
    
    close #624, close #622

--- src/operators/filter/moduloPolar.glsl
+++ src/operators/filter/moduloPolar.glsl
@@ -1,11 +1,25 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 q = p.THIS_PLANE;
-	pR(q, THIS_Rotate);
+	float r = THIS_Rotate;
+	#ifdef THIS_HAS_INPUT_4
+	r += radians(inputOp4(p, ctx));
+	#endif
+	pR(q, r);
 	float cell = THIS_EXPR;
-	pR(q, THIS_Prerotate);
-	p.THIS_PLANE = q - THIS_Offset;
-	#ifdef THIS_Iterateoncells
+	float pr = THIS_Prerotate;
+	#ifdef THIS_HAS_INPUT_2
+	pr += radians(inputOp2(p, ctx));
+	#endif
+	pR(q, pr);
+	vec2 o = THIS_Offset;
+	#ifdef THIS_HAS_INPUT_3
+	o += inputOp3(p, ctx).xy;
+	#endif
+	p.THIS_PLANE = q - o;
+	#if defined(THIS_Iterationtype_index)
 	setIterationIndex(ctx, cell);
+	#elif defined(THIS_Iterationtype_ratio)
+	setIterationIndex(ctx, cell / (THIS_Repetitions - 1.));
 	#endif
 	return inputOp1(p, ctx);
 }
