73db9f81fd1bc0fa3957af63b049c09ea962312a
t3kt <tekt@immerse.studio>
2021-08-22 19:41:05 -0700

    add sphere uv mapping
    see #526

--- src/operators/sdf/sphereSdf.glsl
+++ src/operators/sdf/sphereSdf.glsl
@@ -7,7 +7,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = createSdf(length(p)-r);
 	#if defined(THIS_Uvmode_bounds)
 	assignUV(res, map01(p, vec3(-r), vec3(r)));
-	#elif defined(THIS_Uvmode_spherical)
+	#elif defined(THIS_Uvmode_sphericalpolar)
 	assignUV(
 		res,
 		vec3(
@@ -15,6 +15,17 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 			acos(p.z / res.x),
 			atan(p.y, p.x)
 		));
+	#elif defined(THIS_Uvmode_sphere)
+	CoordT q = p / r;
+	// https://www.shadertoy.com/view/3dVSzm
+	assignUV(
+		res,
+		vec3(
+			0.5 + atan(q.z, q.x) / TAU,
+			0.5 - asin(q.y) / PI,
+			0.
+		)
+	);
 	#endif
 	return res;
 }
