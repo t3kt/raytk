9b8ae254087c91e5e568c4472e393ef5e68156d7
t3kt <tekt@immerse.studio>
2021-08-24 19:37:21 -0700

    initial implementation of pbrMat
    needs lots of cleanup, and proper irradiance / reflectance mapping.
    
    based on https://www.shadertoy.com/view/XssBDr.
    
    see #636

--- src/lib/shader/raytkMaterial.glsl
+++ src/lib/shader/raytkMaterial.glsl
@@ -219,3 +219,52 @@ float attenuateLight(float attenScale, float attenBias, float attenRolloff, floa
 	float finalAtten = pow(lightAtten, attenRolloff);
 	return finalAtten;
 }
+
+// PBR demo by piluve
+// https://www.shadertoy.com/view/XssBDr
+
+// Returns the color of the current point. Uses the Cook-Torrance for the
+// specular BRDF and Lambertian for the diffuse BRDF
+vec3 pbrShading(vec3 p) {
+	return vec3(0.);
+}
+
+// Fresnel-Schlick approximation
+vec3 pbr_fresnel(float ndv,vec3 F0)
+{
+	return F0 + (1.0 - F0) * pow(1.0 - ndv,5.0);
+}
+// Fresnel-Schlick approximation (with roughness)
+vec3 pbr_fresnelRoughness(float ndv,vec3 F0, float r)
+{
+	return F0 + (max(vec3(1.0 - r), F0) - F0) * pow(1.0 - ndv, 5.0);
+}
+// Schlick-GGX term
+float pbr_geoTerm(float d,float r)
+{
+	float k = (r + 1.0);
+	k = (k * k) / 8.0;
+	float nom = d;
+	float denom = d * (1.0 - k) + k;
+	return nom / denom;
+}
+
+// Smith's geometry method
+float pbr_geometry(float ndv,float ndl,float r)
+{
+	float ggx2 = pbr_geoTerm(ndv, r);
+	float ggx1 = pbr_geoTerm(ndl, r);
+
+	return ggx1 * ggx2;
+}
+
+// Trowbridge-Reitz GGX normal distribution function
+float pbr_distribution(float ndh,float r)
+{
+	// Input r should be r * r!
+	float r2 = pow(r,4.0);
+	float ndh2 = pow(ndh,2.0);
+	float nom = r2;
+	float denom = ndh2 * (r2 - 1.0) + 1.0;
+	return nom / (PI * denom * denom);
+}
---
+++ src/operators/material/pbrMat.glsl
@@ -0,0 +1,123 @@
+vec4 THIS_iterationCapture = vec4(0.);
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	Sdf res = inputOp1(p, ctx);
+	#if defined(THIS_Uselocalpos) && defined(RAYTK_USE_MATERIAL_POS)
+	assignMaterialWithPos(res, THISMAT, p);
+	#else
+	assignMaterial(res, THISMAT);
+	#endif
+	captureIterationFromMaterial(THIS_iterationCapture, ctx);
+	return res;
+}
+
+/*
+	Returns the irradiance map (diffuse IBL).
+	Its 50% hack
+*/
+vec3 THIS_IrradianceMap(vec3 n) {
+	// TODO: ....
+//	return texture(iChannel1,n).xyz;
+	return vec3(0.5);
+}
+
+/*
+	Returns the reflectance map (specular IBL)
+	Its 100% hack, a version that blurs the cubemap should give
+	much better results at hight roughness.
+*/
+vec3 THIS_ReflectanceMap(vec3 refl,float roughness,vec3 n)
+{
+	// TODO: ....
+//	vec3 blurMap = THIS_IrradianceMap(n);
+//	vec3 reflecMap = textureLod(iChannel0,refl,4.0 * roughness).xyz;
+//	return mix(reflecMap,blurMap,roughness);
+	return vec3(0.5);
+}
+
+vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
+	restoreIterationFromMaterial(matCtx, THIS_iterationCapture);
+	vec3 lightDir = normalize(p - matCtx.light.pos);
+	vec3 viewDir = normalize(-matCtx.ray.dir);
+
+	float roughness = max(THIS_Roughness, 0.005); // TODO: DO THIS WITH PARAM LIMITS?
+	float metallic = THIS_Metallic;
+	// TODO: Albedo is a vec3?
+	vec3 albedo = vec3(THIS_Albedo);
+
+	vec3 F0 = vec3(0.04);					// Base normal incidence for
+	// non-conductors (average of some materials)
+	F0 = mix(F0,albedo,metallic);	// If it is a metal, take the normal incidence (color)
+	// from the albedo as metals should not have albedo color
+
+	// TODO: CLEANUP ALIASES
+	vec3 eye = normalize(matCtx.ray.pos - p);
+	vec3 n = matCtx.normal;
+	vec3 r = reflect(-eye,n);
+	float ndv = max(dot(n,eye),0.0);
+
+	const int kLights = 1;
+	float sAcum = float(kLights);
+	vec3 acum = vec3(0.0);
+
+	// TODO clean up loop?
+	for (int i = 0; i < kLights; i++) {
+		// Per-light parameters
+//		vec3 lp = lights[i].Position;
+		vec3 lp = matCtx.light.pos;
+		vec3 ld = normalize(lp - p);
+		vec3 h = normalize(ld + eye);
+		float ndl = max(dot(n,ld),0.0);
+		float ndh = max(dot(n,h),0.0);
+
+		// Diffuse
+		vec3 diffuseBRDF = albedo / PI;
+
+		// Specular
+		float D = pbr_distribution(ndh,roughness);
+		float G = pbr_geometry(ndv,ndl,roughness);
+		vec3 F = pbr_fresnel(ndv,F0);
+
+		vec3 specularBRDFNom = D * G * F;
+		float specularBRDFDenom = 4.0 * max(ndv * ndl, 0.0) + 0.001; 	// add bias to prevent
+		// division by 0
+		vec3 specularBRDF = specularBRDFNom / specularBRDFDenom;
+
+		// Outgoing light can't exced 1
+		vec3 kS = F;
+		vec3 kD = 1.0 - kS;
+		kD *= 1.0 - metallic;
+
+		vec3 finalCol = (kD * diffuseBRDF + specularBRDF);
+		finalCol = finalCol * ndl * matCtx.light.color;
+		acum += finalCol;
+
+		// Shadow
+//		vec3 sDir = normalize(lp - p);
+//		sAcum -= Shadow(p,sDir);
+		#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
+		sAcum *= matCtx.shadedLevel;
+		#endif
+	}
+
+	// IBL
+	vec3 F = pbr_fresnelRoughness(ndv,F0,roughness);
+	vec3 kS = F;
+	vec3 kD = 1.0 - kS;
+	kD *= 1.0 - metallic;
+	vec3 totalIBL = vec3(0.0);
+
+	// Diffuse IBL
+	vec3 diffuseIBL = THIS_IrradianceMap(n) * albedo * kD * 1.0;
+
+	// Specular IBL
+	vec3 specularIBL = THIS_ReflectanceMap(r,roughness,n) * F;
+
+	totalIBL = kD * diffuseIBL + specularIBL;
+
+	// Shadowing
+	float finalShadow = max(sAcum / float(kLights),0.02);
+
+	return (acum + totalIBL) * max(finalShadow,0.15);
+}
+
