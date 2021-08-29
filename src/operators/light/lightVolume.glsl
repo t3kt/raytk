d1532dd78600bef706faaa31b4ed3264298fad5a
t3kt <tekt@immerse.studio>
2021-08-28 17:13:47 -0700

    added color field input to lightVolume
    see #637, #11

--- src/operators/light/lightVolume.glsl
+++ src/operators/light/lightVolume.glsl
@@ -1,8 +1,8 @@
 float THIS_getLevel(CoordT p, ContextT ctx) {
 	if (THIS_Level <= 0.) return 0.;
 	#ifdef THIS_HAS_INPUT_1
-		float d = inputOp1(p, ctx).x;
-		return THIS_Level * step(0., -d);
+		float d = inputOp1(p, ctx).x - THIS_Offset;
+		return THIS_Level * (1.0 - smoothstep(0., THIS_Blending, max(d, 0.)));
 	#else
 	return THIS_Level;
 	#endif
@@ -11,13 +11,19 @@ float THIS_getLevel(CoordT p, ContextT ctx) {
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 col = vec3(0.);
 	float level = THIS_getLevel(p, ctx);
-	if (level >= 0.) {
+	if (level > 0.) {
 		#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
 		MaterialContext matCtx = createMaterialContext();
 		matCtx.ray = ctx.ray;
 		level *= calcShadedLevel(p, matCtx);
 		#endif
 	}
-	col = THIS_Color * level;
+	if (level > 0.) {
+		col = THIS_Color;
+		#ifdef THIS_HAS_INPUT_2
+		col *= fillToVec3(inputOp2(p, ctx));
+		#endif
+		col *= level;
+	}
 	return vec4(col, 1.);
 }
