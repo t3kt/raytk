41b33aab1e701b906d45a054f69fefd331ee41ba
t3kt <tekt@immerse.studio>
2021-08-30 19:32:10 -0700

    remove deprecated file

--- src/operators/output/raymarchRender3D_onOpWireChange.py
+++
@@ -1,6 +0,0 @@
-# This really shouldn't be necessary but for some reason the connection
-# change doesn't trigger the switch DAT to cook automatically.
-
-def onWireChange(changeOp):
-	op('default_camera_switch').cook(force=True)
-	
