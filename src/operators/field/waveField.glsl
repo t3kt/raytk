f339ed0da1754d1960e3b3a8a71b2b3882e5c032
t3kt <tekt@immerse.studio>
2021-09-04 23:27:01 -0700

    add phase input for waveField
    close #644

--- src/operators/field/waveField.glsl
+++ src/operators/field/waveField.glsl
@@ -1,11 +1,11 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#ifdef THIS_HAS_INPUT_2
-	float period = THIS_Period * inputOp2(p, ctx);
+	#ifdef THIS_HAS_INPUT_periodField
+	float period = THIS_Period * inputOp_periodField(p, ctx);
 	#else
 	float period = THIS_Period;
 	#endif
-	#if defined(THIS_HAS_INPUT_1)
-		float q = inputOp1(p, ctx);
+	#if defined(THIS_HAS_INPUT_coordField)
+		float q = inputOp_coordField(p, ctx);
 	#elif defined(THIS_COORD_TYPE_float)
 		float q = p;
 	#elif defined(THIS_Axis_dist)
@@ -13,7 +13,12 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#else
 		float q = p.THIS_Axis;
 	#endif
-	q = (q / period) + THIS_Phase;
+	#ifdef THIS_HAS_INPUT_phaseField
+	float phase = THIS_Phase + inputOP_phaseField(p, ctx);
+	#else
+	float phase = THIS_Phase;
+	#endif
+	q = (q / period) + phase;
 	ReturnT res;
 	BODY();
 	return (res * THIS_Amplitude) + THIS_Offset;
