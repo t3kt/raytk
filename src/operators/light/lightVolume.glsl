5f67e31e1410b2b160dfa6fb6c5ff3f6e82c645d
t3kt <tekt@immerse.studio>
2021-09-06 18:09:47 -0700

    option to use surface color from bounds volume in lightVolume
    see #637

--- src/operators/light/lightVolume.glsl
+++ src/operators/light/lightVolume.glsl
@@ -1,7 +1,11 @@
-float THIS_getLevel(CoordT p, ContextT ctx) {
+float THIS_getLevel(CoordT p, ContextT ctx, out vec4 surfaceColor) {
 	if (THIS_Level <= 0.) return 0.;
-	#ifdef THIS_HAS_INPUT_1
-		float d = inputOp1(p, ctx).x - THIS_Offset;
+	#ifdef THIS_HAS_INPUT_boundVolume
+		Sdf res = inputOp_boundVolume(p, ctx);
+		#if defined(THIS_Usesdfcolor) && defined(RAYTK_USE_SURFACE_COLOR)
+		surfaceColor = res.color;
+		#endif
+		float d = res.x - THIS_Offset;
 		return THIS_Level * (1.0 - max(0., smoothstep(-THIS_Blending*0.5, THIS_Blending*0.5, d)));
 	#else
 	return THIS_Level;
@@ -10,7 +14,8 @@ float THIS_getLevel(CoordT p, ContextT ctx) {
 
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 col = vec3(0.);
-	float level = THIS_getLevel(p, ctx);
+	vec4 surfaceColor = vec4(0.);
+	float level = THIS_getLevel(p, ctx, surfaceColor);
 	if (level > 0.) {
 		#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
 		level *= ctx.shadedLevel;
@@ -21,8 +26,11 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		#ifdef THIS_Uselightcolor
 		col *= ctx.light.color;
 		#endif
-		#ifdef THIS_HAS_INPUT_2
-		col *= fillToVec3(inputOp2(p, ctx));
+		#ifdef THIS_Usesdfcolor
+		col *= mix(vec3(1.), surfaceColor.rgb, surfaceColor.a);
+		#endif
+		#ifdef THIS_HAS_INPUT_colorField
+		col *= fillToVec3(inputOp_colorField(p, ctx));
 		#endif
 		col *= level;
 	}
