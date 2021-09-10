1b8f896d0e6f69c996fed675f187d6d5ed1b5f8c
t3kt <tekt@immerse.studio>
2021-09-09 19:31:25 -0700

    move GDF stuff out of hg_sdf
    see #13

--- src/lib/shader/hg_sdf.glsl
+++ src/lib/shader/hg_sdf.glsl
@@ -193,52 +193,6 @@ float fCone(vec3 p, float radius, float height) {
 	return d;
 }
 
-#ifdef RAYTK_USE_GDF
-
-const vec3 GDFVectors[19] = vec3[](
-	normalize(vec3(1, 0, 0)),
-	normalize(vec3(0, 1, 0)),
-	normalize(vec3(0, 0, 1)),
-
-	normalize(vec3(1, 1, 1 )),
-	normalize(vec3(-1, 1, 1)),
-	normalize(vec3(1, -1, 1)),
-	normalize(vec3(1, 1, -1)),
-
-	normalize(vec3(0, 1, PHI+1)),
-	normalize(vec3(0, -1, PHI+1)),
-	normalize(vec3(PHI+1, 0, 1)),
-	normalize(vec3(-PHI-1, 0, 1)),
-	normalize(vec3(1, PHI+1, 0)),
-	normalize(vec3(-1, PHI+1, 0)),
-
-	normalize(vec3(0, PHI, 1)),
-	normalize(vec3(0, -PHI, 1)),
-	normalize(vec3(1, 0, PHI)),
-	normalize(vec3(-1, 0, PHI)),
-	normalize(vec3(PHI, 1, 0)),
-	normalize(vec3(-PHI, 1, 0))
-);
-
-// Version with variable exponent.
-// This is slow and does not produce correct distances, but allows for bulging of objects.
-float fGDF(vec3 p, float r, float e, int begin, int end) {
-	float d = 0;
-	for (int i = begin; i <= end; ++i)
-		d += pow(abs(dot(p, GDFVectors[i])), e);
-	return pow(d, 1/e) - r;
-}
-
-// Version with without exponent, creates objects with sharp edges and flat faces
-float fGDF(vec3 p, float r, int begin, int end) {
-	float d = 0;
-	for (int i = begin; i <= end; ++i)
-		d = max(d, abs(dot(p, GDFVectors[i])));
-	return d - r;
-}
-
-#endif // RTK_USE_GDF
-
 // Rotate around a coordinate axis (i.e. in a plane perpendicular to that axis) by angle <a>.
 // Read like this: R(p.xz, a) rotates "x towards z".
 // This is fast if <a> is a compile-time constant and slower (but still practical) if not.
