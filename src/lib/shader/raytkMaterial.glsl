c65537b55f4830f66bb110ad181e9cb0a34d9f38
t3kt <tekt@immerse.studio>
2021-09-09 18:39:01 -0700

    move some phong code out of raytkMaterial

--- src/lib/shader/raytkMaterial.glsl
+++ src/lib/shader/raytkMaterial.glsl
@@ -23,31 +23,6 @@ bool resultUsesShadow(Sdf res)
 	#endif
 }
 
-vec3 phongContribForLight(
-	vec3 diffColor, vec3 specColor, float alpha, vec3 p, vec3 eye,
-	vec3 lightPos, vec3 lightIntensity, vec3 norm, float occ) {
-	vec3 lightVec = normalize(lightPos - p);
-	vec3 V = normalize(eye - p);
-	vec3 R = normalize(reflect(-lightVec, norm));
-	float diffuse = clamp(dot(norm, lightVec), 0., 1.);
-
-	float dotLN = dot(lightVec, norm);
-//	dotLN = diffuse;
-	float dotRV = dot(norm, V);
-
-	if (dotLN < 0.0) {
-		// Light not visible from this point on the surface, so add no color
-		return vec3(0.0);
-	}
-
-	if (dotRV < 0.0) {
-		// Light reflection in opposite direction as viewer, apply only diffuse
-		// component
-		return lightIntensity * (diffColor * dotLN) *sqrt(occ);
-	}
-	return lightIntensity * (diffColor * dotLN + specColor * pow(dotRV, alpha)) *sqrt(occ);
-}
-
 // http://iquilezles.org/www/articles/checkerfiltering/checkerfiltering.htm
 float checkersGradBox(in vec2 p)
 {
--- src/operators/material/phongMat.glsl
+++ src/operators/material/phongMat.glsl
@@ -16,16 +16,32 @@ vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
 	#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
 	sunShadow = matCtx.shadedLevel;
 	#endif
-	vec3 col = THIS_Ambientcolor + phongContribForLight(
-		THIS_Diffusecolor * sunShadow,
-		THIS_Specularcolor * sunShadow,
-		THIS_Shine,
-		p,
-		matCtx.ray.pos,
-		matCtx.light.pos,
-		matCtx.light.color,
-		matCtx.normal,
-		calcAO(p, matCtx.normal) // AO
-	);
-	return col;
+	vec3 diffColor = THIS_Diffusecolor * sunShadow;
+	vec3 specColor = THIS_Specularcolor * sunShadow;
+	float alpha = THIS_Shine;
+	vec3 eye = matCtx.ray.pos;
+	vec3 lightPos = matCtx.light.pos;
+	vec3 lightIntensity = matCtx.light.color;
+	vec3 norm = matCtx.normal;
+	float occ = calcAO(p, matCtx.normal);
+	vec3 lightVec = normalize(lightPos - p);
+	vec3 V = normalize(eye - p);
+	vec3 R = normalize(reflect(-lightVec, norm));
+	float diffuse = clamp(dot(norm, lightVec), 0., 1.);
+	float dotLN = dot(lightVec, norm);
+
+	if (dotLN < 0.0) {
+		// Light not visible from this point on the surface, so add no color
+		return vec3(0.0);
+	}
+	float dotRV = dot(norm, V);
+	vec3 col;
+	if (dotRV < 0.0) {
+		// Light reflection in opposite direction as viewer, apply only diffuse
+		// component
+		col = lightIntensity * (diffColor * dotLN) *sqrt(occ);
+	} else {
+		col = lightIntensity * (diffColor * dotLN + specColor * pow(dotRV, alpha)) *sqrt(occ);
+	}
+	return col + THIS_Ambientcolor;
 }
