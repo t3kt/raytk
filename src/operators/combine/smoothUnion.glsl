5e19bb1dd0377c44c2a5a36174ccbcd97a14945e
t3kt <tekt@immerse.studio>
2021-09-11 15:44:15 -0700

    update input processing in combine ops

--- src/operators/combine/smoothUnion.glsl
+++ src/operators/combine/smoothUnion.glsl
@@ -1,5 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res1 = inputOp1(p, ctx);
 	ReturnT res2 = inputOp2(p, ctx);
-	return opSmoothUnionM(res1, res2, THIS_GET_AMOUNT());
+	float r = THIS_Amount;
+	#ifdef THIS_HAS_INPUT_radiusField
+	r *= inputOp_radiusField(p, ctx);
+	#endif
+	return opSmoothUnionM(res1, res2, r);
 }
