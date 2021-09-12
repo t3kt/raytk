1ae5b91884bcc9c0182622be56d19335f953cffa
t3kt <tekt@immerse.studio>
2021-09-11 17:30:06 -0700

    add besteach mode for multi-input handling

--- src/components/multiInputHandler/multiInputHandler.py
+++ src/components/multiInputHandler/multiInputHandler.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import raytkTypes
 
 # noinspection PyUnreachableCode
@@ -17,7 +17,7 @@ def buildTypeSettings(dat: 'scriptDAT'):
 			category,
 			mode,
 		])
-		if mode in ('common', 'bestcommon'):
+		if mode in ('common', 'bestcommon', 'besteach'):
 			scopeIndices = _parseInputScope(
 				parent().par[parPrefix + 'typereductionscope'].eval(),
 				maxInputs=len(allInputDefs))
@@ -32,33 +32,46 @@ def buildTypeSettings(dat: 'scriptDAT'):
 				dat[category, 'replacement'] = ' '.join(commonTypes)
 			elif mode == 'bestcommon':
 				preferredTypes = tdu.split(parent().par[parPrefix + 'typepreference'])
-				dat[category, 'replacement'] = ''
-				for prefType in preferredTypes:
-					if prefType in commonTypes:
-						dat[category, 'replacement'] = prefType
-						break
+				dat[category, 'replacement'] = _firstMatch(preferredTypes, commonTypes) or ''
+			elif mode == 'besteach':
+				preferredTypes = tdu.split(parent().par[parPrefix + 'typepreference'])
+				dat[category, 'replacement'] = ' '.join(preferredTypes)
+
+def _firstMatch(vals, matchVals):
+	for val in vals:
+		if val in matchVals:
+			return val
 
 def buildValidationErrors(dat: 'DAT', mergedDefs: 'DAT', typeSettings: 'DAT'):
 	dat.clear()
 	processedInputDefs = [
 		table
-		for table in ops('definition_?')
+		for table in ops('proc_definition_?')
 		if table.numRows > 1
 	]
+
+	def addRow(level, msg):
+		if not dat.numRows:
+			dat.appendRow(['path', 'level', 'message'])
+		dat.appendRow([parent().path, level, msg])
+
 	minCount = int(parent().par.Minimuminputs)
 	if len(processedInputDefs) < minCount:
-		dat.appendRow(['path', 'level', 'message'])
-		dat.appendRow([parent().path, 'error', f'Only {len(processedInputDefs)} provided, {minCount} required'])
+		addRow('error', f'Only {len(processedInputDefs)} provided, {minCount} required')
 	if mergedDefs.numRows < 2:
 		return
 	for typeCategory in raytkTypes.allTypeCategories:
-		repl = _getTypeReplacement(typeSettings, typeCategory)
-		if repl is None:
-			continue
-		if repl == '':
-			if not dat.numRows:
-				dat.appendRow(['path', 'level', 'message'])
-			dat.appendRow([parent().path, 'error', f'Inputs have no matching {typeCategory}'])
+		if typeSettings[typeCategory, 'mode'].val == 'besteach':
+			for inDef in processedInputDefs:
+				current = inDef[1, typeCategory]
+				if not current:
+					addRow('error', f'No matching {typeCategory} for input {inDef.digits}')
+		else:
+			repl = _getTypeReplacement(typeSettings, typeCategory, None)
+			if repl is None:
+				continue
+			if repl == '':
+				addRow('error', f'Inputs have no matching {typeCategory}')
 
 def _parseInputScope(val: str, maxInputs: int) -> List[int]:
 	val = val.strip()
@@ -72,11 +85,17 @@ def applyTypeSettings(dat: 'scriptDAT', index: int, typeSettings: 'DAT'):
 	for category in raytkTypes.allTypeCategories:
 		if str(index) not in typeSettings[category, 'scope'].val.split(' '):
 			continue
-		repl = _getTypeReplacement(typeSettings, category)
+		repl = _getTypeReplacement(typeSettings, category, dat[1, category])
 		if repl is not None:
 			dat[1, category] = repl
 
-def _getTypeReplacement(typeSettings: 'DAT', category: str):
-	if typeSettings[category, 'mode'].val not in ('common', 'bestcommon'):
+def _getTypeReplacement(typeSettings: 'DAT', category: str, current: 'Optional[Cell]'):
+	mode = typeSettings[category, 'mode'].val
+	replacement = typeSettings[category, 'replacement'].val
+	if mode == 'besteach':
+		preferredTypes = tdu.split(replacement)
+		supportedTypes = tdu.split(current)
+		return _firstMatch(preferredTypes, supportedTypes)
+	if mode not in ('common', 'bestcommon'):
 		return None
-	return typeSettings[category, 'replacement'].val
+	return replacement
