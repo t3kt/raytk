980bb58d577b77b9b2e43892ba55b041e0654963
t3kt <tekt@immerse.studio>
2021-09-14 23:54:29 -0700

    adding in support for particle types
    see #652

--- src/lib/shader/raytkParticles.glsl
+++ src/lib/shader/raytkParticles.glsl
@@ -16,9 +16,22 @@ const int PARTICLE_STATE_DEAD = 0;
 const int PARTICLE_STATE_ALIVE = 1;
 const float LIFE_INFINITE = -1.0;
 
-Particle createParticle(vec3 p, vec3 d, float life) {
+Particle createParticle(vec3 p, vec3 d) {
 	return Particle(
-		p, d,
-		vec3(0.), vec3(0.), vec3(0.), vec3(0.),
-		0., life, PARTICLE_STATE_ALIVE);
+		p, d, vec3(0.), vec3(0.), vec3(0.), vec3(0.), 0., 0., PARTICLE_STATE_DEAD
+	);
+}
+Particle createParticle() { return createParticle(vec3(0.), vec3(0.)); }
+
+void initDefVal(out Particle val) {
+	val = createParticle(vec3(0.), vec3(0.));
+}
+
+struct ParticleContext {
+	Context context;
+	Particle initParticle;
+};
+
+ParticleContext createParticleContext(Context ctx) {
+	return ParticleContext(ctx, createParticle());
 }
