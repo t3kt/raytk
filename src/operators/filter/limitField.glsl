89832a34a8109f2c0717210f1acf588aeb455c8b
t3kt <tekt@immerse.studio>
2021-09-14 18:12:39 -0700

    update type handling and remove the "Off" mode in limitField

--- src/operators/filter/limitField.glsl
+++ src/operators/filter/limitField.glsl
@@ -1,24 +1,26 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT val = inputOp1(p, ctx);
+	ReturnT low = THIS_asReturnT(THIS_Low);
+	ReturnT high = THIS_asReturnT(THIS_High);
 	#if defined(THIS_Limittype_off)
 		return val;
 	#elif defined(THIS_Limittype_clamp)
 		#ifdef THIS_RETURN_TYPE_Sdf
-		val.x = clamp(val.x, THIS_LOW, THIS_HIGH);
+		val.x = clamp(val.x, low, high);
 		#else
-		val = clamp(val, THIS_LOW, THIS_HIGH);
+		val = clamp(val, low, high);
 		#endif
 	#elif defined(THIS_Limittype_zigzag)
 		#ifdef THIS_RETURN_TYPE_Sdf
-		val.x = modZigZag(val.x, THIS_LOW, THIS_HIGH);
+		val.x = modZigZag(val.x, low, high);
 		#else
-		val = modZigZag(val, THIS_LOW, THIS_HIGH);
+		val = modZigZag(val, low, high);
 		#endif
 	#elif defined(THIS_Limittype_loop)
 		#ifdef THIS_RETURN_TYPE_Sdf
-		val.x = wrapRange(val.x, THIS_LOW, THIS_HIGH);
+		val.x = wrapRange(val.x, low, high);
 		#else
-		val = wrapRange(val, THIS_LOW, THIS_HIGH);
+		val = wrapRange(val, low, high);
 		#endif
 	#else
 		#error invalidLimitType
