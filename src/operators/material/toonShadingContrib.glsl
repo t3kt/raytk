ea3af3b0064bca2c69e76d56457fc7f8425ee8e2
t3kt <tekt@immerse.studio>
2021-09-16 22:51:40 -0700

    lots more inputHandlers and typeSpecs

--- src/operators/material/toonShadingContrib.glsl
+++ src/operators/material/toonShadingContrib.glsl
@@ -3,7 +3,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 viewDir = normalize(-ctx.ray.dir);
 	vec3 norm = normalize(ctx.normal);
 	float NdotL = (dot(lightDir, norm) + 1.0) * 0.5;
-	vec3 val = fillToVec3(inputOp1(NdotL, ctx));
+	vec3 val = fillToVec3(inputOp_colorRamp(NdotL, ctx));
 	ReturnT res;
 	res.rgb = THIS_Color * THIS_Level * val * 2.0;
 	#ifdef THIS_Uselightcolor
