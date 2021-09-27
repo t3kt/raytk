9dafa5944dbb59e6f34a6eda02bf1ccc4fea0c1d
t3kt <tekt@immerse.studio>
2021-09-26 23:02:08 -0700

    CHOP-based transforms for instance
    close #613

--- src/operators/filter/instance.glsl
+++ src/operators/filter/instance.glsl
@@ -1,11 +1,27 @@
+CoordT THIS_transform(CoordT p, int i) {
+	#ifdef THIS_HAS_TRANSLATE
+	p -= THIS_asCoordT(THIS_translates[i]);
+	#endif
+	#ifdef THIS_HAS_ROTATE
+	#ifdef THIS_COORD_TYPE_vec3
+	pRotateOnXYZ(p, radians(THIS_rotates[i]));
+	#elif defined(THIS_COORD_TYPE_vec2)
+	pR(p, radians(THIS_rotates[i].z));
+	#endif
+	#endif
+	return p;
+}
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	int n = int(THIS_Instancecount);
 	setIterationIndex(ctx, 0);
-	ReturnT res1 = inputOp1(p, ctx);
+	CoordT q = THIS_transform(p, 0);
+	ReturnT res1 = inputOp1(q, ctx);
 	float r = THIS_Radius;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i < n; i++) {
 		setIterationIndex(ctx, i);
-		ReturnT res2 = inputOp1(p, ctx);
+		q = THIS_transform(p, i);
+		ReturnT res2 = inputOp1(q, ctx);
 		#ifdef THIS_COMBINE_EXPR_IS_SDF
 		res1 = THIS_COMBINE_EXPR;
 		#else
