7ceaca07dae81ff46d373b7263499d2d38e19cdb
t3kt <tekt@immerse.studio>
2021-10-12 20:03:02 -0700

    added size field input in modulo1D
    see #695

--- src/operators/filter/modulo1D.glsl
+++ src/operators/filter/modulo1D.glsl
@@ -1,6 +1,19 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_COORD_TYPE_float
+	float q = p + THIS_Shift;
+	#else
 	float q = p.THIS_Axis + THIS_Shift;
+	#endif
 	float size = THIS_Size;
+	#ifdef THIS_HAS_INPUT_sizeField
+	{
+		#ifdef inputOp_sizeField_COORD_TYPE_float
+		size *= inputOp_sizeField(q, ctx);
+		#else
+		size *= inputOp_sizeField(p, ctx);
+		#endif
+	}
+	#endif
 	float halfsize = size*0.5;
 	float c = floor((q + halfsize)/size);
 	q = mod(q+halfsize, size) - halfsize;
@@ -21,7 +34,11 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q *= mod(c, 2.0)*2 - 1;
 	#endif
 
+	#ifdef THIS_COORD_TYPE_float
+	p = q - THIS_Offset;
+	#else
 	p.THIS_Axis = q - THIS_Offset;
+	#endif
 	#if defined(THIS_Iterationtype_cellcoord)
 	setIterationIndex(ctx, c);
 	#elif defined(THIS_Iterationtype_alternatingcoord)
