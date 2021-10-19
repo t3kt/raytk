fac6d467a7363b87d5e50c06fd8dd093cc711741
t3kt <tekt@immerse.studio>
2021-10-18 22:53:22 -0700

    field inputs for modulo2D
    see #695

--- src/operators/filter/modulo2D.glsl
+++ src/operators/filter/modulo2D.glsl
@@ -1,8 +1,30 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec2 q = p.THIS_PLANE + THIS_Shift;
+	vec2 q = p.THIS_PLANE;
 	vec2 size = THIS_Size;
+	#ifdef THIS_HAS_INPUT_sizeField
+	{
+		#if defined(inputOp_sizeField_COORD_TYPE_vec2)
+		vec2 q1 = q;
+		#else
+		inputOp_sizeField_CoordT q1 = inputOp_sizeField_asCoordT(p);
+		#endif
+		size *= fillToVec2(inputOp_sizeField(q1, ctx));
+	}
+	#endif
 	vec2 halfsize = size * 0.5;
 
+	vec2 sh = THIS_Shift;
+	#ifdef THIS_HAS_INPUT_shiftField
+	{
+		#if defined(inputOp_shiftField_COORD_TYPE_vec2)
+		vec2 q1 = q;
+		#else
+		inputOp_shiftField_CoordT q1 = inputOp_shiftField_asCoordT(p);
+		#endif
+		sh += fillToVec2(inputOp_shiftField(q1, ctx));
+	}
+	#endif
+	q += sh;
 	vec2 c = floor((q + halfsize)/size);
 	q = mod(q + halfsize, size) - halfsize;
 	#if defined(THIS_Limittype_both) || defined(THIS_Limittype_start)
@@ -25,7 +47,6 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	c = floor(c/2);
 	#endif
 
-	p.THIS_PLANE = q - THIS_Offset;
 	#if defined(THIS_Iterationtype_cellcoord)
 	setIterationCell(ctx, c);
 	#elif defined(THIS_Iterationtype_tiledquadrant)
@@ -33,5 +54,20 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#elif defined(THIS_Iterationtype_alternatingcoord)
 	setIterationCell(ctx, mod(c, 2.));
 	#endif
+	// offset field can use iteration
+	vec2 o = THIS_Offset;
+	#ifdef THIS_HAS_INPUT_offsetField
+	{
+
+		#if defined(inputOp_offsetField_COORD_TYPE_vec2)
+		vec2 q1 = q;
+		#else
+		inputOp_offsetField_CoordT q1 = inputOp_offsetField_asCoordT(p);
+		#endif
+		o += fillToVec2(inputOp_offsetField(q1, ctx));
+	}
+	#else
+	#endif
+	p.THIS_PLANE = q - o;
 	return inputOp1(p, ctx);
 }
