3e43e9e2068843345fde57c8508897c5c3053a29
t3kt <tekt@immerse.studio>
2021-10-31 00:32:44 -0700

    more code filtering
    see #710

--- src/operators/field/hsvColorField.glsl
+++ src/operators/field/hsvColorField.glsl
@@ -1,16 +1,16 @@
 vec4 thismap(CoordT p, ContextT ctx) {
 	vec3 hsv = vec3(THIS_Hueoffset * TAU, THIS_Saturation, THIS_Value);
-	#ifdef THIS_HAS_INPUT_hueField
+	#pragma r:if THIS_HAS_INPUT_hueField
 	hsv.x += inputOp_hueField(p, ctx);
-	#else
+	#pragma r:else
 	hsv.x += extractOrUseAsX(p);
-	#endif
-	#ifdef THIS_HAS_INPUT_saturationField
+	#pragma r:endif
+	#pragma r:if THIS_HAS_INPUT_saturationField
 	hsv.y *= inputOp_saturationField(p, ctx);
-	#endif
-	#ifdef THIS_HAS_INPUT_valueField
+	#pragma r:endif
+	#pragma r:if THIS_HAS_INPUT_valueField
 	hsv.z *= inputOp_valueField(p, ctx);
-	#endif
+	#pragma r:endif
 
 	return vec4(TDHSVToRGB(hsv), 1.0);
 }
