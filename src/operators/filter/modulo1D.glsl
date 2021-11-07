b4974baa6ffc527c68261f227571b76892a6811a
t3kt <tekt@immerse.studio>
2021-11-06 17:07:46 -0700

    added shift and offset field inputs in modulo1D
    close #722

--- src/operators/filter/modulo1D.glsl
+++ src/operators/filter/modulo1D.glsl
@@ -1,8 +1,8 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	#pragma r:if THIS_COORD_TYPE_float
-	float q = p + THIS_Shift;
+	float q = p;
 	#pragma r:else
-	float q = p.THIS_Axis + THIS_Shift;
+	float q = p.THIS_Axis;
 	#pragma r:endif
 	float size = THIS_Size;
 	#pragma r:if THIS_HAS_INPUT_sizeField
@@ -15,6 +15,21 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	}
 	#pragma r:endif
 	float halfsize = size*0.5;
+
+
+	float sh = THIS_Shift;
+	#pragma r:if THIS_HAS_INPUT_shiftField
+	{
+		#pragma r:if inputOp_shiftField_COORD_TYPE_float
+		float q1 = q;
+		#pragma r:else
+		inputOp_shiftField_CoordT q1 = inputOp_shiftField_asCoordT(p);
+		#pragma r:endif
+		sh += inputOp_shiftField(q1, ctx);
+	}
+	#pragma r:endif
+	q += sh;
+
 	float c = floor((q + halfsize)/size);
 	q = mod(q+halfsize, size) - halfsize;
 	#pragma r:if THIS_Uselimit
@@ -34,15 +49,30 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q *= mod(c, 2.0)*2 - 1;
 	#pragma r:endif
 
-	#pragma r:if THIS_COORD_TYPE_float
-	p = q - THIS_Offset;
-	#pragma r:else
-	p.THIS_Axis = q - THIS_Offset;
-	#pragma r:endif
 	#pragma r:if THIS_Iterationtype_cellcoord
 	setIterationIndex(ctx, c);
 	#pragma r:elif THIS_Iterationtype_alternatingcoord
 	setIterationIndex(ctx, mod(c, 2.));
 	#pragma r:endif
+
+	// offset field can use iteration
+	float o = THIS_Offset;
+	#pragma r:if THIS_HAS_INPUT_offsetField
+	{
+		#pragma r:if inputOp_offsetField_COORD_TYPE_float
+		float q1 = q;
+		#pragma r:else
+		inputOp_offsetField_CoordT q1 = inputOp_offsetField_asCoordT(p);
+		#pragma r:endif
+		o += inputOp_offsetField(q1, ctx);
+	}
+	#pragma r:endif
+
+	#pragma r:if THIS_COORD_TYPE_float
+	p = q - o;
+	#pragma r:else
+	p.THIS_Axis = q - o;
+	#pragma r:endif
+
 	return inputOp1(p, ctx);
 }
