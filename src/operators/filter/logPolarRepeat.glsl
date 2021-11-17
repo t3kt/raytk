73a45ec55e6934518ec3d0b2819d611f99b84750
t3kt <tekt@immerse.studio>
2021-11-16 23:21:57 -0800

    added mirroring and iteration in logPolarRepeat
    and mark it as beta.
    
    see #738

--- src/operators/filter/logPolarRepeat.glsl
+++ src/operators/filter/logPolarRepeat.glsl
@@ -10,22 +10,32 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float scale = THIS_Radialreps/TAU;
 	float scaleAdj = 1.0;
 	vec2 cell = vec2(0.);
+	vec2 size = vec2(THIS_Distspacing, 1.);
 
 	#if defined(THIS_Mode_logpolar)
 	float r = length(q);
 	q = vec2(log(r), atan(q.y, q.x));
 	q -= vec2(THIS_Rhooffset, THIS_Thetaoffset);
-	q *= scale;
-//	q = fract(q) - 0.5;
-	cell = pModMirror2(q, vec2(1.));
+	q.y *= scale;
 	scaleAdj = r / scale;
+	#endif
+	#if defined(THIS_Mirrortype_none)
+	cell = pMod2(q, size);
+	#elif defined(THIS_Mirrortype_mirror)
+	cell = pModMirror2(q, size);
+	#elif defined(THIS_Mirrortype_grid)
+	cell = pModGrid2(q, size);
+	#endif
+
+	#if defined(THIS_Iterationtype_cellcoord)
+	setIterationCell(ctx, cell);
 	#endif
 
 	#ifdef THIS_COORD_TYPE_vec2
 	p = q;
 	#else
 	p.THIS_PLANE = q;
-	p.THIS_AXIS / scaleAdj;
+	p.THIS_AXIS /= scaleAdj;
 	#endif
 	ReturnT res = inputOp1(p, ctx);
 	#ifdef THIS_RETURN_TYPE_Sdf
