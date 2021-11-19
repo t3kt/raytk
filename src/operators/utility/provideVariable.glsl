47e2dbf8c814db94d8c32a7c05228cc16cc456e7
t3kt <tekt@immerse.studio>
2021-11-18 23:37:35 -0800

    got variable references working!
    see #712, #574

--- src/operators/utility/provideVariable.glsl
+++ src/operators/utility/provideVariable.glsl
@@ -1,9 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#ifdef THIS_EXPOSE_var
+	#ifdef THIS_EXPOSE_VAR
 	#ifdef THIS_Datatype_vec4
-	THIS_var = THIS_Value;
-	#elif THIS_Datatype_float
-	THIS_var = THIS_Value1;
+	THIS_VAR = THIS_Value;
+	#elif defined(THIS_Datatype_float)
+	THIS_VAR = THIS_Value1;
 	#endif
 	#endif
 	return inputOp1(p, ctx);
