758b96e915754a10a4022ea7bf02decab8d30641
t3kt <tekt@immerse.studio>
2021-11-22 13:58:51 -0800

    variable support for instance and iteratedTransform
    see #712

--- src/operators/filter/instance.glsl
+++ src/operators/filter/instance.glsl
@@ -19,6 +19,12 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res1 = inputOp1(q, ctx);
 	for (int i = 1; i < n; i++) {
 		setIterationIndex(ctx, i);
+		#pragma r:if THIS_EXPOSE_index
+		THIS_index = i;
+		#pragma r:endif
+		#pragma r:if THIS_EXPOSE_normindex
+		THIS_normindex = float(i) / float(n - 1);
+		#pragma r:endif
 		q = THIS_transform(p, i);
 		ReturnT res2 = inputOp1(q, ctx);
 		#pragma r:if THIS_HAS_INPUT_radiusField
