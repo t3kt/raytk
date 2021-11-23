4b2b392dee38e1df3b7ec3e3821c57e64ce05656
t3kt <tekt@immerse.studio>
2021-11-23 12:06:14 -0800

    field inputs in spikeSdf2d
    see #588

--- src/operators/sdf2d/spikeSdf2d.glsl
+++ src/operators/sdf2d/spikeSdf2d.glsl
@@ -3,7 +3,13 @@
 
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p.y -= THIS_Offset;
+	#ifdef THIS_HAS_INPUT_offsetField
+	p.y -= inputOp_offsetField(p, ctx);
+	#endif
 	float h = THIS_Height;
+	#ifdef THIS_HAS_INPUT_heightField
+	h *= inputOp_heightField(p, ctx);
+	#endif
 	float d = p.y - (h*0.1)/(abs(p.x)+0.1);
 	d = min(d, length(p - vec2(0., min(h, p.y))));
 	float d2 = abs(p.x) - ((h*0.1)-0.1*p.y)/p.y;
