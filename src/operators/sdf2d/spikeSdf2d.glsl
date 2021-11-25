5540adec6b5b58324d433cf3a553c308a31a8066
t3kt <tekt@immerse.studio>
2021-11-24 17:34:47 -0800

    add direction and center parameters to spikeSdf

--- src/operators/sdf2d/spikeSdf2d.glsl
+++ src/operators/sdf2d/spikeSdf2d.glsl
@@ -2,18 +2,21 @@
 // https://www.shadertoy.com/view/3ssSR7
 
 ReturnT thismap(CoordT p, ContextT ctx) {
-	p.y -= THIS_Offset;
+	vec2 q;
+	CONVERSION();
+
+	q -= vec2(THIS_Center, THIS_Offset);
 	#ifdef THIS_HAS_INPUT_offsetField
-	p.y -= inputOp_offsetField(p, ctx);
+	q.y -= inputOp_offsetField(p, ctx);
 	#endif
 	float h = THIS_Height;
 	#ifdef THIS_HAS_INPUT_heightField
 	h *= inputOp_heightField(p, ctx);
 	#endif
-	float d = p.y - (h*0.1)/(abs(p.x)+0.1);
-	d = min(d, length(p - vec2(0., min(h, p.y))));
-	float d2 = abs(p.x) - ((h*0.1)-0.1*p.y)/p.y;
-	if (p.y<h && d>0.0)
+	float d = q.y - (h*0.1)/(abs(q.x)+0.1);
+	d = min(d, length(q - vec2(0., min(h, q.y))));
+	float d2 = abs(q.x) - ((h*0.1)-0.1*q.y)/q.y;
+	if (q.y<h && d>0.0)
 	d = min(d, d2);
 	return createSdf(d);
 }
