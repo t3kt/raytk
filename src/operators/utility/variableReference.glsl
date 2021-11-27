0edd20dd642bfd5ffb22410690a6eaee153fb94e
t3kt <tekt@immerse.studio>
2021-11-26 23:39:00 -0800

    added vector part extraction in variableReference
    at least for now it's not worth adding float -> vector conversion settings, since that's a less common use case and it can be done using other operators.
    
    close #754
    see #574

--- src/operators/utility/variableReference.glsl
+++ src/operators/utility/variableReference.glsl
@@ -1,7 +1,13 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#ifdef THIS_HAS_REF_var
-	return THIS_asReturnT(THIS_var);
+	ReturnT res;
+	#if !defined(THIS_HAS_REF_var)
+	res = THIS_asReturnT(THIS_Defaultval);
 	#else
-	return THIS_asReturnT(THIS_Defaultval);
+	#if defined(THIS_Datatype_vec4) && defined(THIS_RETURN_TYPE_float)
+	res = adaptAsVec4(THIS_var).THIS_Part;
+	#else
+	res = THIS_asReturnT(THIS_var);
 	#endif
+	#endif
+	return res;
 }
