d896c1826163c94365a8e77a10fee3658d611fb7
t3kt <tekt@immerse.studio>
2021-11-27 15:43:41 -0800

    add variable support to cylinderSdf
    see #574

--- src/operators/sdf/cylinderSdf.glsl
+++ src/operators/sdf/cylinderSdf.glsl
@@ -1,7 +1,22 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Translate;
 	vec3 q = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
+	#ifdef THIS_EXPOSE_axispos
+	THIS_axispos = q.y;
+	#endif
 	float r = THIS_Radius;
+	#if !defined(THIS_Infiniteheight)
+	float h = THIS_Height;
+	#if defined(THIS_HAS_INPUT_heightField) && defined(inputOp_heightField_COORD_TYPE_float)
+	h *= inputOp_heightField(q.y, ctx);
+	#elif defined(THIS_HAS_INPUT_heightField) && defined(inputOp_heightField_COORD_TYPE_vec3)
+	h *= inputOp_heightField(p, ctx);
+	#endif
+	#ifdef THIS_EXPOSE_normOffset
+	THIS_normOffset = map01(q.y, -h*.5, h*.5);
+	#endif
+	#endif
+
 	#if defined(THIS_HAS_INPUT_radiusField) && defined(inputOp_radiusField_COORD_TYPE_float)
 	r *= inputOp_radiusField(q.y, ctx);
 	#elif defined(THIS_HAS_INPUT_radiusField) && defined(inputOp_radiusField_COORD_TYPE_vec3)
@@ -14,12 +29,6 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	}
 	#else
 	{
-		float h = THIS_Height;
-		#if defined(THIS_HAS_INPUT_heightField) && defined(inputOp_heightField_COORD_TYPE_float)
-		h *= inputOp_heightField(q.y, ctx);
-		#elif defined(THIS_HAS_INPUT_heightField) && defined(inputOp_heightField_COORD_TYPE_vec3)
-		h *= inputOp_heightField(p, ctx);
-		#endif
 		res = createSdf(fCylinder(q, r, h));
 	}
 	#endif
