1aa820d486d29deddcdfddaa5ed99b39b5dbe17c
t3kt <tekt@immerse.studio>
2021-12-26 15:15:47 -0800

    field inputs and variable support for pieSdf2d
    see #588, #574

--- src/operators/sdf2d/pieSdf2d.glsl
+++ src/operators/sdf2d/pieSdf2d.glsl
@@ -1,10 +1,21 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	pR(p, THIS_Rotate);
-	vec2 c = THIS_C;
-	float r = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_angleField
+	float a = radians(inputOp_angleField(p, ctx));
+	#else
+	float a = THIS_Angle;
+	#endif
+	#ifdef THIS_EXPOSE_normangle
+	THIS_normangle = map01(atan(p.y, p.x), a, 0.);
+	#endif
+	vec2 c = vec2(sin(a * .5), cos(a * .5));
+	vec2 r = vec2(THIS_Radius, THIS_Innerradius);
+	#ifdef THIS_HAS_INPUT_radiusField
+	r *= fillToVec2(inputOp_radiusField(p, ctx));
+	#endif
 	p.x = abs(p.x);
-	float l = length(p) - r;
-	float m = length(p-c*clamp(dot(p,c),0.0,r)); // c = sin/cos of the aperture
+	float l = length(p) - r.x;
+	float m = length(p-c*clamp(dot(p,c),0.0,r.x));
 	float d = max(l,m*sign(c.y*p.x-c.x*p.y));
 	BODY();
 	return createSdf(d);
