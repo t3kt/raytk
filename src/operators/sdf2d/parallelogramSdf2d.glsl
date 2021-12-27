0ebbb5aacc1479e8f3ca97f47d3e49b6fcf911fe
t3kt <tekt@immerse.studio>
2021-12-26 17:36:25 -0800

    field inputs for parallelogramSdf2d
    see #588

--- src/operators/sdf2d/parallelogramSdf2d.glsl
+++ src/operators/sdf2d/parallelogramSdf2d.glsl
@@ -1,16 +1,22 @@
 // https://iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
 // https://www.shadertoy.com/view/7dlGRf
 ReturnT thismap(CoordT p, ContextT ctx) {
-	float wi = THIS_Width;
-	float he = THIS_Height;
+	vec2 size = vec2(THIS_Width, THIS_Height);
+	#ifdef THIS_HAS_INPUT_sizeField
+	size *= fillToVec2(inputOp_sizeField(p, ctx));
+	#endif
+	#ifdef THIS_HAS_INPUT_skewField
+	float sk = inputOp_skewField(p, ctx);
+	#else
 	float sk = THIS_Skew;
-	vec2 e = vec2(sk,he);
+	#endif
+	vec2 e = vec2(sk,size.y);
 	p = (p.y<0.0)?-p:p;
-	vec2  w = p - e; w.x -= clamp(w.x,-wi,wi);
+	vec2  w = p - e; w.x -= clamp(w.x,-size.x,size.x);
 	vec2  d = vec2(dot(w,w), -w.y);
 	float s = p.x*e.y - p.y*e.x;
 	p = (s<0.0)?-p:p;
-	vec2  v = p - vec2(wi,0); v -= e*clamp(dot(v,e)/dot(e,e),-1.0,1.0);
-	d = min( d, vec2(dot(v,v), wi*he-abs(s)));
+	vec2  v = p - vec2(size.x,0); v -= e*clamp(dot(v,e)/dot(e,e),-1.0,1.0);
+	d = min( d, vec2(dot(v,v), size.x*size.y-abs(s)));
 	return createSdf(sqrt(d.x)*sign(-d.y));
 }
