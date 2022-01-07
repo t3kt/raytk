82ece5bf6aae82b19b3b79f47ea0444cd5827b8c
t3kt <tekt@immerse.studio>
2022-01-06 18:59:26 -0800

    field inputs for arcSdf2d and horseshoeSdf2d
    see #588

--- src/operators/sdf2d/arcSdf2d.glsl
+++ src/operators/sdf2d/arcSdf2d.glsl
@@ -1,10 +1,24 @@
 // https://iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
 // https://www.shadertoy.com/view/wl23RK
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec2 sca = THIS_Sc.xz;
-	vec2 scb = THIS_Sc.yw;
+	vec2 oa = vec2(THIS_Orientation, THIS_Aperture);
+	#ifdef THIS_HAS_INPUT_orientationField
+	oa.x += radians(inputOp_orientationField(p, ctx));
+	#endif
+	#ifdef THIS_HAS_INPUT_apertureField
+	oa.y = radians(inputOp_apertureField(p, ctx));
+	#endif
+	vec4 sc = vec4(sin(oa), cos(oa));
+	vec2 sca = sc.xz;
+	vec2 scb = sc.yw;
 	float ra = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_radiusField
+	ra *= inputOp_radiusField(p, ctx);
+	#endif
 	float rb = THIS_Thickness;
+	#ifdef THIS_HAS_INPUT_thicknessField
+	rb *= inputOp_thicknessField(p, ctx);
+	#endif
 	p *= mat2(sca.x,sca.y,-sca.y,sca.x);
 	p.x = abs(p.x);
 	float k = (scb.y*p.x>scb.x*p.y) ? dot(p,scb) : length(p);
--- src/operators/sdf2d/horseshoeSdf2d.glsl
+++ src/operators/sdf2d/horseshoeSdf2d.glsl
@@ -1,9 +1,23 @@
 // https://iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
 // https://www.shadertoy.com/view/WlSGW1
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec2 c = THIS_Sc;
+	#ifdef THIS_HAS_INPUT_angleField
+	float a = radians(inputOp_angleField(p, ctx));
+	#else
+	float a = THIS_Angle;
+	#endif
+	vec2 c = vec2(sin(a), cos(a));
 	float r = THIS_Radius;
+	#ifdef THIS_HAS_INPUT_radiusField
+	r *= inputOp_radiusField(p, ctx);
+	#endif
 	vec2 w = vec2(THIS_Length, THIS_Thickness);
+	#ifdef THIS_HAS_INPUT_lengthField
+	w.x *= inputOp_lengthField(p, ctx);
+	#endif
+	#ifdef THIS_HAS_INPUT_thicknessField
+	w.y *= inputOp_thicknessField(p, ctx);
+	#endif
 	p.x = abs(p.x);
 	float l = length(p);
 	p = mat2(-c.x, c.y, c.y, c.x)*p;
