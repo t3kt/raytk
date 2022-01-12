a33865c8938477938d82db8aa519fee764bad9f0
t3kt <tekt@immerse.studio>
2022-01-11 23:32:40 -0800

    added side flipping to mirrorAxes
    close #641, #668

--- src/operators/filter/mirrorAxes.glsl
+++ src/operators/filter/mirrorAxes.glsl
@@ -1,3 +1,19 @@
+// todo: parts of this could be shared across instances
+float THIS_getSideMask(vec3 p, int side) {
+	// todo: code-gen like codeSwitcher but shared across axes in reusable function
+	switch (side) {
+		case 0: /* none */ return 0.;
+		// todo: there is definitely a much more efficient way to do this
+		case 1: /* x+ */ return p.x > 0. ? 1. : 0.;
+		case 2: /* x- */ return p.x <= 0. ? 1. : 0.;
+		case 3: /* y+ */ return p.y > 0. ? 1. : 0.;
+		case 4: /* y- */ return p.y <= 0. ? 1. : 0.;
+		case 5: /* z+ */ return p.z > 0. ? 1. : 0.;
+		case 6: /* z- */ return p.z > 0. ? 0. : 1.;
+	}
+	return 0.;
+}
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 c = THIS_Center;
 	vec3 q = adaptAsVec3(p) - c;
@@ -5,6 +21,13 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#ifdef THIS_EXPOSE_sides
 	THIS_sides = sides;
 	#endif
+	vec3 flipMask = vec3(
+		THIS_getSideMask(p, int(THIS_Flipsidex)),
+		THIS_getSideMask(p, int(THIS_Flipsidey)),
+		THIS_getSideMask(p, int(THIS_Flipsidez))
+	);
+	q = mix(q, -q, flipMask);
+
 	vec3 dir = THIS_dir;
 	#ifdef THIS_HAS_INPUT_directionField
 	dir *= sgn(fillToVec3(inputOp_directionField(p, ctx)));
