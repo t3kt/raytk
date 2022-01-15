4357fe25bc57953288e467f471fe5023db22cc44
t3kt <tekt@immerse.studio>
2022-01-15 14:00:44 -0800

    fix variable handling and add more inputs in transformLoop
    see #791

--- src/operators/filter/transformLoop.glsl
+++ src/operators/filter/transformLoop.glsl
@@ -1,19 +1,19 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	int n = int(THIS_Iterations);
-	#pragma r:if THIS_EXPOSE_index
-	THIS_index = 0;
+	#pragma r:if THIS_EXPOSE_step
+	THIS_step = 0;
 	#pragma r:endif
-	#pragma r:if THIS_EXPOSE_normindex
-	THIS_normindex = 0;
+	#pragma r:if THIS_EXPOSE_normstep
+	THIS_normstep = 0;
 	#pragma r:endif
 
 	vec4 q = adaptAsVec4(p);
 	for (int i = 0; i < n; i++) {
-		#pragma r:if THIS_EXPOSE_index
-		THIS_index = i;
+		#pragma r:if THIS_EXPOSE_step
+		THIS_step = i;
 		#pragma r:endif
-		#pragma r:if THIS_EXPOSE_normindex
-		THIS_normindex = float(i) / float(n - 1);
+		#pragma r:if THIS_EXPOSE_normstep
+		THIS_normstep = float(i) / float(n - 1);
 		#pragma r:endif
 
 		BODY();
