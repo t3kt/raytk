8b85d0801defcea0ce7b20a8a33423c7b880c8ba
t3kt <tekt@immerse.studio>
2022-01-19 17:48:45 -0800

    fix missing typing import

--- src/tools/editorTools/editorActions.py
+++ src/tools/editorTools/editorActions.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Union
 from editorToolsCommon import Action, ActionContext, ActionGroup, ActionManager, ActionUtils, ROPState
 from raytkUtil import showPromptDialog
 
