b47458da25fd9e8e894f32ce2a4bd7c0220a88a4
t3kt <tekt@immerse.studio>
2022-01-21 18:02:08 -0800

    fix translate axis swapping in discSdf

--- src/operators/sdf/discSdf.glsl
+++ src/operators/sdf/discSdf.glsl
@@ -1,15 +1,15 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	CoordT q = p;
-	p = vec3(p.THIS_PLANE_P1, p.THIS_AXIS, p.THIS_PLANE_P2);
+	CoordT q = p - THIS_Translate;
+	q = vec3(q.THIS_PLANE_P1, q.THIS_AXIS, q.THIS_PLANE_P2);
 	#pragma r:if THIS_HAS_INPUT_radiusField
-	float r = THIS_Radius * inputOp_radiusField(q, ctx);
+	float r = THIS_Radius * inputOp_radiusField(p, ctx);
 	#pragma r:else
 	float r = THIS_Radius;
 	#pragma r:endif
 	#pragma r:if THIS_HAS_INPUT_thicknessField
-	float t = THIS_Thickness * inputOp_thicknessField(q, ctx);
+	float t = THIS_Thickness * inputOp_thicknessField(p, ctx);
 	#pragma r:else
 	float t = THIS_Thickness;
 	#pragma r:endif
-	return createSdf(fDisc(p - THIS_Translate, r) - t);
+	return createSdf(fDisc(q, r) - t);
 }
