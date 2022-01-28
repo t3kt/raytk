94ff0a7e98b438d5094a122b05f5d89c6e0e513a
t3kt <tekt@immerse.studio>
2022-01-27 23:02:38 -0800

    field input for provideVariable
    see #770, #574

--- src/operators/utility/provideVariable.glsl
+++ src/operators/utility/provideVariable.glsl
@@ -1,10 +1,21 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	#ifdef THIS_EXPOSE_VAR
-	#ifdef THIS_Datatype_vec4
-	THIS_VAR = THIS_Value;
-	#elif defined(THIS_Datatype_float)
-	THIS_VAR = THIS_Value1;
-	#endif
-	#endif
+	{
+		#ifdef THIS_HAS_INPUT_valueField
+		#ifdef THIS_Datatype_vec4
+		vec4 val = adaptAsVec4(inputOp_valueField(p, ctx));
+		#elif defined(THIS_Datatype_float)
+		float val = adaptAsFloat(inputOp_valueField(p, ctx));
+		#endif
+		#else
+		#ifdef THIS_Datatype_vec4
+		vec4 val = THIS_Value;
+		#elif defined(THIS_Datatype_float)
+		float val = THIS_Value1;
+		#endif
+		#endif
+		THIS_VAR = val;
+		#endif
+	}
 	return inputOp1(p, ctx);
 }
