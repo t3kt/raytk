9ad3b8fe514ccd819be474029b640b57ec8f37fa
t3kt <tekt@immerse.studio>
2022-02-28 20:08:28 -0800

    runtime bypass for limitField
    see #755

--- src/operators/filter/limitField.glsl
+++ src/operators/filter/limitField.glsl
@@ -1,29 +1,30 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT val = inputOp1(p, ctx);
-	ReturnT low = THIS_asReturnT(THIS_Low);
-	ReturnT high = THIS_asReturnT(THIS_High);
-	#pragma r:if THIS_Limittype_off
-		return val;
-	#pragma r:elif THIS_Limittype_clamp
-		#pragma r:if THIS_RETURN_TYPE_Sdf
-		val.x = clamp(val.x, low, high);
+	if (THIS_Enable >= 0.5) {
+		ReturnT low = THIS_asReturnT(THIS_Low);
+		ReturnT high = THIS_asReturnT(THIS_High);
+		#pragma r:if THIS_Limittype_off
+		#pragma r:elif THIS_Limittype_clamp
+			#pragma r:if THIS_RETURN_TYPE_Sdf
+			val.x = clamp(val.x, low, high);
+			#pragma r:else
+			val = clamp(val, low, high);
+			#pragma r:endif
+		#pragma r:elif THIS_Limittype_zigzag
+			#pragma r:if THIS_RETURN_TYPE_Sdf
+			val.x = modZigZag(val.x, low, high);
+			#pragma r:else
+			val = modZigZag(val, low, high);
+			#pragma r:endif
+		#pragma r:elif THIS_Limittype_loop
+			#pragma r:if THIS_RETURN_TYPE_Sdf
+			val.x = wrapRange(val.x, low, high);
+			#pragma r:else
+			val = wrapRange(val, low, high);
+			#pragma r:endif
 		#pragma r:else
-		val = clamp(val, low, high);
+			#error invalidLimitType
 		#pragma r:endif
-	#pragma r:elif THIS_Limittype_zigzag
-		#pragma r:if THIS_RETURN_TYPE_Sdf
-		val.x = modZigZag(val.x, low, high);
-		#pragma r:else
-		val = modZigZag(val, low, high);
-		#pragma r:endif
-	#pragma r:elif THIS_Limittype_loop
-		#pragma r:if THIS_RETURN_TYPE_Sdf
-		val.x = wrapRange(val.x, low, high);
-		#pragma r:else
-		val = wrapRange(val, low, high);
-		#pragma r:endif
-	#pragma r:else
-		#error invalidLimitType
-	#pragma r:endif
+	}
 	return val;
 }
