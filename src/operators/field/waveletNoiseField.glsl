d6b7bb491c7c7a59b6792342cb112f2372e04459
t3kt <tekt@immerse.studio>
2022-03-11 13:29:33 -0800

    coord field input to waveletNoiseField and cleanup
    see #829

--- src/operators/field/waveletNoiseField.glsl
+++ src/operators/field/waveletNoiseField.glsl
@@ -12,69 +12,39 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float k = 1/ (THIS_Scalefactor==0.?0.00001:THIS_Scalefactor);
 	float z = THIS_Phase * k;
 	#ifdef THIS_HAS_INPUT_phaseField
-	{
-		#if defined(inputOp_phaseField_COORD_TYPE_vec2)
-		{
-			#ifdef THIS_COORD_TYPE_vec2
-			z += inputOp_phaseField(p, ctx);
-			#else
-			#error mismatchedCoordType
-			#endif
-		}
-		#elif defined(inputOp_phaseField_COORD_TYPE_vec3)
-		{
-			#ifdef THIS_COORD_TYPE_vec3
-			z += inputOp_phaseField(p, ctx);
-			#else
-			#error mismatchedCoordType
-			#endif
-		}
-		#else
-		#error invalidCoordType
-		#endif
-	}
+	z += inputOp_phaseField(p, ctx);
+	#endif
+	#ifdef THIS_HAS_INPUT_coordField
+	p = THIS_asCoordT(inputOp_coordField(p, ctx));
 	#endif
 	float d=0.,s=1.,m=0., a;
-	#if defined(THIS_COORD_TYPE_vec2)
-	p -= vec2(THIS_Translate1, THIS_Translate2);
-	p /= vec2(THIS_Period1, THIS_Period2);
-
+	p -= THIS_asCoordT(THIS_Translate);
+	p /= THIS_asCoordT(THIS_Period);
+	CoordT seed = THIS_asCoordT(vec3(123.34,233.53,314.15));
 	for(int i=0; i<THIS_Iterations; i++) {
-		vec2 q = p*s, g=fract(floor(q)*vec2(123.34,233.53));
-		g += dot(g, g+23.234);
-		a = fract(g.x*g.y)*1e3;
-		#ifdef THIS_Enablevorticity
-		a += z*(mod(g.x+g.y, 2.)-1.); // add vorticity
-		#endif
-		q = (fract(q)-.5)*mat2(cos(a),-sin(a),sin(a),cos(a));
-		d += sin(q.x*10.+z)*smoothstep(.25, .0, dot(q,q))/s;
-		p = p*mat2(.54,-.84, .84, .54)+i;
-		m += 1./s;
-		s *= k;
-	}
-	#elif defined(THIS_COORD_TYPE_vec3)
-	p -= THIS_Translate;
-	p /= THIS_Period;
-
-	for(int i=0; i<THIS_Iterations; i++) {
-		vec3 q = p*s, g=fract(floor(q)*vec3(123.34,233.53,314.15));
+		CoordT q = p*s, g=fract(floor(q)*seed);
 		g += dot(g, g+23.234);
 		a = fract(g.x*g.y)*1e3;
 		#ifdef THIS_Enablevorticity
 		a += z*(mod(g.x+g.y, 2.)-1.); // add vorticity
 		#endif
 		q = (fract(q)-.5);
+	#if defined(THIS_COORD_TYPE_vec2)
+		q *= rotateMatrix2d(a);
+		d += sin(q.x*10.+z)*smoothstep(.25, .0, dot(q,q))/s;
+		p = p*mat2(.54,-.84, .84, .54)+i;
+	#elif defined(THIS_COORD_TYPE_vec3)
 		//random rotation in 3d. the +.1 is to fix the rare case that g == vec3(0)
 		//https://suricrasia.online/demoscene/functions/#rndrot
 		q = THIS_erot(q, normalize(tan(g+.1)), a);
 		d += sin(q.x*10.+z)*smoothstep(.25, .0, dot(q,q))/s;
 		p = THIS_erot(p,normalize(vec3(-1,1,0)),atan(sqrt(2.)))+i; //rotate along the magic angle
-		m += 1./s;
-		s *= k;
-	}
 	#else
 	#error invalidCoordType
 	#endif
+		m += 1./s;
+		s *= k;
+	}
 
 	float val = d/m;
 	return (val * THIS_Amplitude) + THIS_Offset;
