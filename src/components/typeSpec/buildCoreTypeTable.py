0570abaa9a60e9406c1c20b8e19899a9fbf19970
t3kt <tekt@immerse.studio>
2022-03-11 16:03:20 -0800

    start a new approach to organizing types

---
+++ src/components/typeSpec/buildCoreTypeTable.py
@@ -0,0 +1,9 @@
+from raytkTypes import buildCoreTypeTable
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+def onCook(dat: 'scriptDAT'):
+	buildCoreTypeTable(dat)
