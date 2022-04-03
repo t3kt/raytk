cfca08e88fce1b003bca30e2789b440f0938c10e
t3kt <tekt@immerse.studio>
2022-04-03 15:07:55 -0700

    coord field input for axisDistanceField
    see #829

--- src/operators/field/axisDistanceField.glsl
+++ src/operators/field/axisDistanceField.glsl
@@ -1,15 +1,8 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	ReturnT res;
-	#pragma r:if THIS_COORD_TYPE_float
-	res = abs(p - THIS_Center);
+	#pragma r:if THIS_HAS_INPUT_coordField
+	vec3 q = adaptAsVec3(inputOp_coordField(p, ctx));
 	#pragma r:else
-	float a;
-	switch (int(THIS_Axis)) {
-		case 0: a = p.x; break;
-		case 1: a = p.y; break;
-		case 2: a = adaptAsVec3(p).z; break;
-	}
-	res = length(a - THIS_Center);
+	vec3 q = adaptAsVec3(p);
 	#pragma r:endif
-	return res;
+	return length(getAxis(q, int(THIS_Axis)) - THIS_Center);
 }
