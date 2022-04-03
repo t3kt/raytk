7f11188e5924c6bfb68473672c4fcfb0613de5ad
t3kt <tekt@immerse.studio>
2022-04-02 22:33:36 -0700

    index variable support in arrange

--- src/operators/combine/arrange.glsl
+++ src/operators/combine/arrange.glsl
@@ -2,6 +2,12 @@ void THIS_merge(inout ReturnT res1, in ReturnT res2, float r, float n, float o) 
 MERGE_BODY();
 }
 
+void THIS_exposeIndex(int i) {
+	#ifdef THIS_EXPOSE_index
+	THIS_index = i;
+	#endif
+}
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	MERGE_PREP();
 	ReturnT res1 = createNonHitSdf();
--- src/operators/combine/arrange_stepTemplate.glsl
+++ src/operators/combine/arrange_stepTemplate.glsl
@@ -1,3 +1,4 @@
 if (THIS_Enable$ > 0.5) {
+  THIS_exposeIndex($);
   THIS_merge(res1, inputOp$(p - THIS_asCoordT(THIS_Translate$), ctx), r, n, o);
 }
