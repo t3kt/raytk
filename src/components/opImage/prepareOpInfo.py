ae72f254cec13deb2f5f52666842b3170dc0a8fb
t3kt <tekt@immerse.studio>
2022-04-20 20:37:30 -0700

    adjust the size of opImages to fit the default COMP size

--- src/components/opImage/prepareOpInfo.py
+++ src/components/opImage/prepareOpInfo.py
@@ -8,7 +8,7 @@ if False:
 def onCook(dat: 'DAT'):
 	dat.clear()
 	info = ROPInfo(parent().par.Definitionop.eval())
-	dat.appendRow(['res', 256, 256])
+	dat.appendRow(['res', 320, 256])
 	dat.appendRow(['name', ''])
 	dat.appendRow(['category', ''])
 	dat.appendRow(['toolkitVersion', ''])
