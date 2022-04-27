4fb76cb464f4e040b745a8fcefce9684ee10b27c
t3kt <tekt@immerse.studio>
2022-04-26 17:38:49 -0700

    support many inputs for combineFields

--- src/operators/combine/combineFields.glsl
+++ src/operators/combine/combineFields.glsl
@@ -1,13 +1,18 @@
-ReturnT thismap(CoordT p, ContextT ctx) {
-	ReturnT a = ReturnT(inputOp1(p, ctx));
-	if (IS_FALSE(THIS_Enable)) { return a; }
-	ReturnT b = ReturnT(inputOp2(p, ctx));
+ReturnT THIS_combine(ReturnT a, ReturnT b) {
+	ReturnT res;
 	if (IS_TRUE(THIS_Swaporder)) {
 		ReturnT tmp = a;
 		a = b;
 		b = tmp;
 	}
-	ReturnT res;
 	BODY();
+	return res;
+}
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	ReturnT a = ReturnT(inputOp1(p, ctx));
+	if (IS_FALSE(THIS_Enable)) { return a; }
+	ReturnT res;
+	AGGREGATE_BODY();
 	return res;
 }
