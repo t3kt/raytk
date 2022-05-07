cfeb4de6c26ed6dfeb97e14d239da49b8dcb4905
t3kt <tekt@immerse.studio>
2022-05-06 17:10:50 -0700

    index options and extend modes for switch indices (warning)
    note that this changes how indices are handled when some inputs are empty and other later inputs are not.
    
    close #860

--- src/operators/combine/switch.glsl
+++ src/operators/combine/switch.glsl
@@ -1,54 +1,24 @@
+float THIS_applyExtend(float i, float n) {
+	EXTEND();
+	return i;
+}
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res;
 	#if THIS_INPUT_COUNT == 0
 	initDefVal(res);
 	#else
 		#ifdef THIS_HAS_INPUT_indexField
-		int source = int(inputOp_indexField(p, ctx));
+		float source = inputOp_indexField(p, ctx);
 		#else
-		int source = int(THIS_Source);
+		float source = THIS_Source;
 		#endif
-		int i = clamp(source, 0, THIS_INPUT_COUNT - 1);
-		if (i == 0) {
-			res = THIS_INPUT_1(p, ctx);
-		}
-		#if THIS_INPUT_COUNT > 1
-		else if (i == 1) {
-			res = THIS_INPUT_2(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 2
-		else if (i == 2) {
-			res = THIS_INPUT_3(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 3
-		else if (i == 3) {
-			res = THIS_INPUT_4(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 4
-		else if (i == 4) {
-			res = THIS_INPUT_5(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 5
-		else if (i == 5) {
-			res = THIS_INPUT_6(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 6
-		else if (i == 6) {
-			res = THIS_INPUT_7(p, ctx);
-		}
-		#endif
-		#if THIS_INPUT_COUNT > 7
-		else if (i == 7) {
-			res = THIS_INPUT_8(p, ctx);
-		}
-		#endif
-		else {
-			initDefVal(res);
+		int n = int(THIS_lastInput);
+		RESCALEINDEX();
+		int i = int(THIS_applyExtend(source, n)) + 1;
+		initDefVal(res);
+		switch (i) {
+			AGGREGATE_BODY();
 		}
 	#endif
 	return res;
