a5315cb066301df3755c69e4b54c2d9f6baa6ba0
t3kt <tekt@immerse.studio>
2022-05-21 15:39:20 -0700

    added kochSnowflakeSdf2d
    close #884

---
+++ src/operators/sdf2d/kochSnowflakeSdf2d.glsl
@@ -0,0 +1,22 @@
+// https://www.shadertoy.com/view/NljfRG
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	const vec2 k = vec2(1.7320508076,0.5773502692);
+	p = abs(p);
+	p -= vec2(1.0-p.y,p.x)*k.y;
+	p *= k.x;
+	float scale = 0.5;
+	int n = int(THIS_Steps);
+	for (int i = 0; i<n ; i++ ) {
+		p = vec2(p.x-1.0,abs(p.y));
+		p += vec2(p.y,-p.x)*k.y;  // rotates 30° scales sqrt(3)/2
+		p = 1.5*vec2(-p.x,p.y-2.0*k.y);
+		scale *= k.y;
+	}
+	float d = sign(p.x)*length(p-vec2(0.,clamp(p.y,-k.x,k.x)));
+	if (n % 2 == 1) {
+		d *= -1.;
+	}
+	d *= scale;
+	return createSdf(d);
+}
