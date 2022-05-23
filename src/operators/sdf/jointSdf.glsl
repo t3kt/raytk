41958c8b433925f989e54f1a6e5962ac27690432
t3kt <tekt@immerse.studio>
2022-05-22 17:03:42 -0700

    Add field inputs in jointSdf
    see #527, #693

--- src/operators/sdf/jointSdf.glsl
+++ src/operators/sdf/jointSdf.glsl
@@ -1,9 +1,21 @@
 // https://www.shadertoy.com/view/3ld3DM
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec4 distAndUV;
+	#ifdef THIS_HAS_INPUT_lengthField
+	float le = inputOp_lengthField(p, ctx);
+	#else
 	float le = THIS_Length;
+	#endif
+	#ifdef THIS_HAS_INPUT_angleField
+	float an = radians(inputOp_angleField(p, ctx));
+	#else
 	float an = THIS_Angle;
+	#endif
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float wi = inputOp_thicknessField(p, ctx);
+	#else
 	float wi = THIS_Thickness;
+	#endif
 	BODY();
 	ReturnT res = createSdf(distAndUV.x);
 	distAndUV.z /= le;
