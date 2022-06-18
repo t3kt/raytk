c22eed1347343ad49760dd7c425c96c21ba2fd3c
t3kt <tekt@immerse.studio>
2022-06-18 16:54:13 -0700

    added field inputs in jointSdf2d
    see #588

--- src/operators/sdf2d/jointSdf2d.glsl
+++ src/operators/sdf2d/jointSdf2d.glsl
@@ -1,8 +1,20 @@
 // https://www.shadertoy.com/view/WldGWM
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_bendField
+	float a = inputOp_bendField(p, ctx) * PI;
+	#else
 	float a = THIS_Bend*PI;
+	#endif
+	#ifdef THIS_HAS_INPUT_lengthField
+	float l = inputOp_lengthField(p, ctx);
+	#else
 	float l = THIS_Length;
+	#endif
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float w = inputOp_thicknessField(p, ctx);
+	#else
 	float w = THIS_Thickness;
+	#endif
 	vec3 distAndUV;
 	BODY();
 	ReturnT res;
