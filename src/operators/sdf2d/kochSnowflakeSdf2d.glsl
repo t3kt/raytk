cb83cf89c5293268dfe50120ac214034eb96a864
t3kt <tekt@immerse.studio>
2022-06-19 13:24:15 -0700

    steps field input for kochSnowflakeSdf
    see #588

--- src/operators/sdf2d/kochSnowflakeSdf2d.glsl
+++ src/operators/sdf2d/kochSnowflakeSdf2d.glsl
@@ -1,12 +1,17 @@
 // https://www.shadertoy.com/view/NljfRG
 
 ReturnT thismap(CoordT p, ContextT ctx) {
+	CoordT p0 = p;
 	const vec2 k = vec2(1.7320508076,0.5773502692);
 	p = abs(p);
 	p -= vec2(1.0-p.y,p.x)*k.y;
 	p *= k.x;
 	float scale = 0.5;
+	#ifdef THIS_HAS_INPUT_stepsField
+	int n = int(round(inputOp_stepsField(p0, ctx)));
+	#else
 	int n = int(THIS_Steps);
+	#endif
 	for (int i = 0; i<n ; i++ ) {
 		p = vec2(p.x-1.0,abs(p.y));
 		p += vec2(p.y,-p.x)*k.y;  // rotates 30° scales sqrt(3)/2
