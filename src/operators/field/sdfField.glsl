498f19fcc6416bc9bb3624a1129ab293b8064df0
t3kt <tekt@immerse.studio>
2022-06-20 15:42:02 -0700

    option to return uvs in sdfField

--- src/operators/field/sdfField.glsl
+++ src/operators/field/sdfField.glsl
@@ -1,24 +1,13 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#if defined(inputOp1_RETURN_TYPE_Sdf) || defined(THIS_Fieldtype_uv)
+	Sdf sdfRes = adaptAsSdf(inputOp1(p, ctx));
+	float d = sdfRes.x - THIS_Offset;
+	#else
 	float d = adaptAsFloat(inputOp1(p, ctx)) - THIS_Offset;
-	#ifndef THIS_Fieldtype_dist
-	{
-		float t = THIS_Thickness/2.;
-		float b = THIS_Blending;
-		#if defined(THIS_Fieldtype_inside)
-		d = smoothstep(0., b, -d);
-		#elif defined(THIS_Fieldtype_outside)
-		d = smoothstep(0., b, d);
-		#elif defined(THIS_Fieldtype_surface)
-		d = 1.0-smoothstep(0., b, abs(d) - t);
-		#elif defined(THIS_Fieldtype_surfaceinside)
-		d = (1.0 - smoothstep(0., b, abs(d) - t)) * (1.0-step(0., d));
-		#elif defined(THIS_Fieldtype_surfaceoutside)
-		d = (1.0 - smoothstep(0., b, abs(d) - t)) * (1.0-step(0., -d));
-		#else
-		#error invalidFieldType
-		#endif
-	}
 	#endif
-
-	return d;
+	float t = THIS_Thickness/2.;
+	float b = THIS_Blending;
+	ReturnT res;
+	BODY();
+	return res;
 }
