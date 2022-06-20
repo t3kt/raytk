378e707f55f3437e269294df493bf6646e97acb6
t3kt <tekt@immerse.studio>
2022-06-20 14:49:49 -0700

    added parameterized uvs for circleSdf
    see #903

--- src/operators/sdf2d/circleSdf.glsl
+++ src/operators/sdf2d/circleSdf.glsl
@@ -4,7 +4,8 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#pragma r:if THIS_HAS_INPUT_radiusField
 	r *= inputOp_radiusField(p, ctx);
 	#pragma r:endif
-	ReturnT res = createSdf(length(p)-r);
+	float d = length(p) - r;
+	ReturnT res = createSdf(d);
 	#pragma r:if THIS_Uvmode_cartesian
 	assignUV(res, vec3(map01(p, -vec2(r/2.), vec2(r/2.)), 0.));
 	#pragma r:elif THIS_Uvmode_polar
@@ -13,6 +14,19 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		atan(p.y, p.x),
 		0.
 	));
+	#pragma r:elif THIS_Uvmode_extparam || THIS_Uvmode_normextparam
+	// https://www.shadertoy.com/view/WldSWX
+	float band = THIS_Externalbandsize;
+	float ra = band*round(d/band);
+	float l = (res.x+ra)*(atan(p.x,p.y)+PI);
+	{
+		#pragma r:if THIS_Uvmode_extparam
+		assignUV(res, vec3(l, d-ra, 0.));
+		#pragma r:elif THIS_Uvmode_normextparam
+		float total = TAU*(d+ra);
+		assignUV(res, vec3(l / total, d-ra, 0.));
+		#pragma r:endif
+	}
 	#pragma r:endif
 	return res;
 }
