b41a67e344d6d681943967211bea91e1241f0412
t3kt <tekt@immerse.studio>
2022-06-20 17:12:16 -0700

    added rotate field input in rotateNormals
    see #812

--- src/operators/filter/rotateNormals.glsl
+++ src/operators/filter/rotateNormals.glsl
@@ -1,6 +1,11 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	if (THIS_Enable >= 0.5) {
 		vec3 n = ctx.normal;
+		#ifdef THIS_HAS_INPUT_rotateField
+		vec3 rotate = radians(fillToVec3(inputOp_rotateField(p, ctx)));
+		#else
+		vec3 rotate = THIS_Rotate;
+		#endif
 		TRANSFORM_CODE();
 		ctx.normal = n;
 	}
