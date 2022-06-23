3f45053df86558744e908c6f3ac69f824325e57f
t3kt <tekt@immerse.studio>
2022-06-23 16:09:54 -0700

    support blending in switch
    and deprecate the blend operator.
    
    close #905

--- src/operators/combine/switch.glsl
+++ src/operators/combine/switch.glsl
@@ -3,6 +3,16 @@ float THIS_applyExtend(float i, float n) {
 	return i;
 }
 
+ReturnT THIS_getInput(float source, int n, CoordT p, ContextT ctx) {
+	ReturnT res;
+	initDefVal(res);
+	int i = int(THIS_applyExtend(source, n)) + 1;
+	switch (i) {
+		AGGREGATE_BODY();
+	}
+	return res;
+}
+
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res;
 	#if THIS_INPUT_COUNT == 0
@@ -15,11 +25,17 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		#endif
 		int n = int(THIS_lastInput);
 		RESCALEINDEX();
-		int i = int(THIS_applyExtend(source, n)) + 1;
-		initDefVal(res);
-		switch (i) {
-			AGGREGATE_BODY();
+		#ifndef THIS_Blend
+		res = THIS_getInput(source, n, p, ctx);
+		#else
+		float source1 = floor(source);
+		float source2 = ceil(source);
+		res = THIS_getInput(source1, n, p, ctx);
+		if (source1 != source2) {
+			ReturnT res2 = THIS_getInput(source2, n, p, ctx);
+			res = mixVals(res, res2, fract(source));
 		}
+		#endif
 	#endif
 	return res;
 }
