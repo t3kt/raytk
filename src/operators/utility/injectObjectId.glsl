37c9d437fe32fb63a857aefadde0a578a18f0eb2
t3kt <tekt@immerse.studio>
2022-06-25 18:03:38 -0700

    added id field input in injectObjectId

--- src/operators/utility/injectObjectId.glsl
+++ src/operators/utility/injectObjectId.glsl
@@ -1,5 +1,9 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT res = inputOp1(p, ctx);
-	res.objectId = vec4(THIS_Objectid, 0, 0, 0);
+	#ifdef THIS_HAS_INPUT_idField
+	res.objectId = vec4(inputOp_idField(p, ctx), 0., 0., 0.);
+	#else
+	res.objectId = vec4(THIS_Objectid, 0., 0., 0.);
+	#endif
 	return res;
 }
