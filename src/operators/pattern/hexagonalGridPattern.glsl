1b333f53965e1d16895a0cf05cba7d27c38054c4
t3kt <tekt@immerse.studio>
2022-07-10 11:28:10 -0700

    field inputs and an improved thickness setting in hexagonalGridPattern
    see #921, #254

--- src/operators/pattern/hexagonalGridPattern.glsl
+++ src/operators/pattern/hexagonalGridPattern.glsl
@@ -2,6 +2,17 @@
 // https://www.shadertoy.com/view/4dfyzf
 
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float t = adaptAsFloat(inputOp_thicknessField(p, ctx));
+	#else
+	float t = THIS_Thickness;
+	#endif
+	#ifdef THIS_HAS_INPUT_blendingField
+	float b = adaptAsFloat(inputOp_blendingField(p, ctx));
+	#else
+	float b = THIS_Blending;
+	#endif
+	b = max(0., b);
 	#ifdef THIS_HAS_INPUT_coordField
 	vec2 q = adaptAsVec2(inputOp_coordField(p, ctx));
 	#else
@@ -14,5 +25,5 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q.y += 0.5 * mod(floor(q.x), 2.0);
 	q = abs(fract(q) - 0.5);
 	float d = abs(max(q.x*1.5 + q.y, q.y*2.0) - 1.0);
-	return smoothstep(0.0, THIS_Thickness, d);
+	return smoothstep(t, t+b, d);
 }
