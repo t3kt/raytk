ac8e72b887e19cdd218da51f3b2e916165d6d9f1
t3kt <tekt@immerse.studio>
2022-07-11 13:50:10 -0700

    added variations to the hexagonalGridPattern
    see #254

--- src/operators/pattern/hexagonalGridPattern.glsl
+++ src/operators/pattern/hexagonalGridPattern.glsl
@@ -1,18 +1,30 @@
 // From 2d Procedural Pattern by gPlatl
 // https://www.shadertoy.com/view/4dfyzf
 
+#ifdef THIS_USE_COLORIZE
+vec3 THIS_colorize(float v) {
+	if (v == 0.5) return THIS_Color2;
+	if (v == 1.0) return THIS_Color3;
+	return THIS_Color1;
+}
+#endif
+
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_USE_THICKNESS
 	#ifdef THIS_HAS_INPUT_thicknessField
 	float t = adaptAsFloat(inputOp_thicknessField(p, ctx));
 	#else
 	float t = THIS_Thickness;
 	#endif
+	#endif
+	#ifdef THIS_USE_BLENDING
 	#ifdef THIS_HAS_INPUT_blendingField
 	float b = adaptAsFloat(inputOp_blendingField(p, ctx));
 	#else
 	float b = THIS_Blending;
 	#endif
 	b = max(0., b);
+	#endif
 	#ifdef THIS_HAS_INPUT_coordField
 	vec2 q = adaptAsVec2(inputOp_coordField(p, ctx));
 	#else
@@ -21,10 +33,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q -= THIS_Translate;
 	q /= THIS_Size;
 
-	q.x *= 0.57735 * 2.0;
-	q.y += 0.5 * mod(floor(q.x), 2.0);
-	q = abs(fract(q) - 0.5);
-	float d = abs(max(q.x*1.5 + q.y, q.y*2.0) - 1.0);
+	ReturnT res;
 	BODY();
-	return d;
+	return res;
 }
