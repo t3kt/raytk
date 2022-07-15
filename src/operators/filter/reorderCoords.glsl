b1600f7924fd7ec5b3da698023d8f331147ae0bb
t3kt <tekt@immerse.studio>
2022-07-15 15:51:39 -0700

    option to zero out axes in reorderCoords
    close #213

--- src/operators/filter/reorderCoords.glsl
+++ src/operators/filter/reorderCoords.glsl
@@ -1,6 +1,6 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	if (THIS_Enable >= 0.5) {
-		p = p.THIS_SWIZZLE;
+		p = THIS_asCoordT(THIS_MASK * adaptAsVec3(p).THIS_SWIZZLE);
 	}
 	return inputOp1(p, ctx);
 }
