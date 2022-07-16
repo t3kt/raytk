829e6d127ff8708e4bd11ba3cb30dfefb8d06e6f
t3kt <tekt@immerse.studio>
2022-07-16 14:27:37 -0700

    field inputs and variables for mengerSpongeSdf
    see #693, close #698

--- src/operators/sdf/mengerSpongeSdf.glsl
+++ src/operators/sdf/mengerSpongeSdf.glsl
@@ -1,35 +1,35 @@
 // Based on Klems code https://www.shadertoy.com/view/XljSWm
 
-#ifndef RAYTK_MENGER_SPONGE_HELPER
-#define RAYTK_MENGER_SPONGE_HELPER
-
-// distance to a menger sponge of n = 1
-float _mengerCrossDist( in vec3 p, float crossScale, float boxScale ) {
-	vec3 absp = abs(p);
-	// get the distance to the closest axis
-	float maxyz = max(absp.y, absp.z);
-	float maxxz = max(absp.x, absp.z);
-	float maxxy = max(absp.x, absp.y);
-	float cr = crossScale - (step(maxyz, absp.x)*maxyz+step(maxxz, absp.y)*maxxz+step(maxxy, absp.z)*maxxy);
-	// cube
-	float cu = max(maxxy, absp.z) - boxScale;
-	// remove the cross from the cube
-	return max(cr, cu);
-}
-
-#endif
-
 ReturnT thismap(CoordT p, ContextT ctx) {
 	p -= THIS_Translate;
-	const int n = int(THIS_Steps);
+	int n = int(THIS_Steps);
 	float scale = THIS_Scale;
-	float crossScale = THIS_Crossscale;
-	float boxScale = THIS_Boxscale;
 	float dist = 0.0;
 	for (int i = 0; i < n; i++) {
-		dist = max(dist, _mengerCrossDist(p, crossScale, boxScale)*scale);
+		#pragma r:if THIS_EXPOSE_step
+		THIS_step = i;
+		#pragma r:endif
+		#pragma r:if THIS_EXPOSE_normstep
+		THIS_normstep = float(i) / float(n - 1);
+		#pragma r:endif
+		#pragma r:if THIS_HAS_INPUT_crossScaleField
+		float crossScale = inputOp_crossScaleField(p, ctx);
+		#pragma r:else
+		float crossScale = THIS_Crossscale;
+		#pragma r:endif
+		#pragma r:if THIS_HAS_INPUT_boxScaleField
+		float boxScale = inputOp_boxScaleField(p, ctx);
+		#pragma r:else
+		float boxScale = THIS_Boxscale;
+		#pragma r:endif
+		dist = max(dist, mengerCrossDist(p, crossScale, boxScale)*scale);
 		p = fract((p-1.0)*0.5) * 6.0 - 3.0;
 		scale /= 3.0;
+		#pragma r:if THIS_HAS_INPUT_stepOffsetField
+		p -= fillToVec3(inputOp_stepOffsetField(p, ctx));
+		#pragma r:else
+		p -= THIS_Stepoffset;
+		#pragma r:endif
 	}
 	return createSdf(dist);
 }
