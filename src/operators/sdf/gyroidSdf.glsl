d0a71cce7a773bd11b4ddcab207a027fa57027f5
t3kt <tekt@immerse.studio>
2022-07-16 12:58:57 -0700

    field inputs for gyroidSdf
    see #693

--- src/operators/sdf/gyroidSdf.glsl
+++ src/operators/sdf/gyroidSdf.glsl
@@ -1,9 +1,16 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	CoordT p0 = p;
 	p -= THIS_asCoordT(THIS_Translate);
 	CoordT q = p;
 	#pragma r:if THIS_Enablephase
 	p += THIS_asCoordT(THIS_Phase1);
 	q += THIS_asCoordT(THIS_Phase2);
+	#pragma r:endif
+	#pragma r:if THIS_HAS_INPUT_phase1Field
+	p += THIS_asCoordT(fillToVec3(inputOp_phase1Field(p0, ctx)));
+	#pragma r:endif
+	#pragma r:if THIS_HAS_INPUT_phase2Field
+	q += THIS_asCoordT(fillToVec3(inputOp_phase2Field(p0, ctx)));
 	#pragma r:endif
 	#pragma r:if THIS_Enableperiod
 	CoordT per1 = THIS_asCoordT(THIS_Period1);
@@ -18,11 +25,21 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#pragma r:else
 	CoordT c = q.zxy;
 	#pragma r:endif
-	float d = dot(sin(p), cos(c)) - THIS_Bias;
+	#pragma r:if THIS_HAS_INPUT_biasField
+	float b = adaptAsFloat(inputOp_biasField(p0, ctx));
+	#pragma r:else
+	float b = THIS_Bias;
+	#pragma r:endif
+	float d = dot(sin(p), cos(c)) - b;
 	d = abs(d);
 	d /= length(s);
 	#pragma r:if THIS_Enableperiod
 	d /= length(per1) * length(per2);
 	#pragma r:endif
-	return createSdf(d - THIS_Thickness);
+	#pragma r:if THIS_HAS_INPUT_thicknessField
+	float th = adaptAsFloat(inputOp_thicknessField(p0, ctx));
+	#pragma r:else
+	float th = THIS_Thickness;
+	#pragma r:endif
+	return createSdf(d - th);
 }
