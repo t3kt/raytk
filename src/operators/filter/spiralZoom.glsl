8fb42a3b8561b023cbacf0bfba27b297a030c4c4
t3kt <tekt@immerse.studio>
2022-07-22 17:25:07 -0700

    field inputs for spiralZoom
    see #812

--- src/operators/filter/spiralZoom.glsl
+++ src/operators/filter/spiralZoom.glsl
@@ -14,10 +14,22 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		float a = atan(q.y, q.x)/TAU;
 		float r = log(length(q));
 		float n = THIS_Branches;
+		#ifdef THIS_HAS_INPUT_twistField
+		vec2 twist = inputOp_twistField(p, ctx).xy;
+		float spiral = twist.x;
+		float zoom = twist.y;
+		#else
 		float spiral = THIS_Twist1;
 		float zoom = THIS_Twist2;
+		#endif
 
-		q = vec2(a*n + r*spiral, -r*zoom + a) + THIS_Phase;
+		#ifdef THIS_HAS_INPUT_phaseField
+		vec2 ph = inputOp_phaseField(p, ctx).xy;
+		#else
+		vec2 ph = THIS_Phase;
+		#endif
+
+		q = vec2(a*n + r*spiral, -r*zoom + a) + ph;
 
 		#if defined(THIS_COORD_TYPE_vec2)
 		p = q;
