c1c77b72905462d897ea597d040edade9bcc9aee
t3kt <tekt@immerse.studio>
2022-07-29 18:44:36 -0700

    move input type validation from inputHandler to opDefinition
    see #933 , #935

--- src/components/inputHandler/inputHandler.py
+++ src/components/inputHandler/inputHandler.py
@@ -59,30 +59,3 @@ def buildValidationErrors(dat: 'DAT', inputDef: 'DAT'):
 		dat.appendRow([parent().path, 'error', msg])
 
 	resolveSourceParDefinition(_addError)
-	if parent().par.Required and inputDef.numRows < 2:
-		_addError('Required input is missing')
-	_checkTableTypes(inputDef, _addError, ignoreEmpty=True)
-
-def _checkType(typeName: str, typeCategory: str, onError: 'Optional[Callable[[str], None]]', ignoreEmpty: bool):
-	if not typeName:
-		if ignoreEmpty:
-			return True
-		if onError:
-			onError(f'Invalid input {typeCategory}: {typeName!r}')
-		return False
-	supported = tdu.split(op('supportedTypes')[typeCategory, 'types'] or '')
-	if ' ' in typeName:
-		if any(t in supported for t in typeName.split(' ')):
-			return True
-	else:
-		if typeName in supported:
-			return True
-	if onError:
-		onError(f'Input does not support {typeCategory} {typeName}')
-	return False
-
-def _checkTableTypes(dat: 'DAT', onError: 'Optional[Callable[[str], None]]', ignoreEmpty: bool):
-	validCoord = _checkType(str(dat[1, 'coordType'] or ''), 'coordType', onError, ignoreEmpty)
-	validContext = _checkType(str(dat[1, 'contextType'] or ''), 'contextType', onError, ignoreEmpty)
-	validReturn = _checkType(str(dat[1, 'returnType'] or ''), 'returnType', onError, ignoreEmpty)
-	return validCoord and validContext and validReturn
