90ef73388d9c62df8aefdf534b06ab386aae277a
t3kt <tekt@immerse.studio>
2022-08-04 23:02:18 -0700

    generate opState in opDefinition
    see #939

---
+++ src/components/opDefinition/buildOpState.py
@@ -0,0 +1,320 @@
+from dataclasses import dataclass
+import math
+from raytkState import RopState, Macro, Texture, Reference, Variable, Dispatch, Buffer, ValidationError
+import re
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+	from raytkUtil import OpDefParsT
+	from typing import List, Optional
+
+class _Builder:
+	def __init__(
+			self,
+			typeTable: 'DAT',
+	):
+		# noinspection PyTypeChecker
+		self.defPar = parent().par  # type: OpDefParsT
+		self.hostOp = self.defPar.Hostop.eval()
+		self.paramsOp = self.defPar.Paramsop.eval() or self.hostOp
+		self.opState = RopState(
+			opType=self.defPar.Raytkoptype.eval(),
+			toolkitVersion=self.defPar.Raytkversion.eval(),
+			name=self.defPar.Name.eval(),
+
+			coordType=typeTable['coordType', 1].val,
+			contextType=typeTable['contextType', 1].val,
+			returnType=typeTable['returnType', 1].val,
+		)
+		self.opName = self.opState.name
+		self.namePrefix = self.opName + '_'
+		if self.defPar.Materialcode:
+			self.opState.materialId = 'MAT_' + self.opState.name
+		self.inputs = []  # type: List[_InputInfo]
+		# self.replacements = {}  # type: Dict[str, str]
+
+	def loadInputs(self, inDats: 'List[DAT]'):
+		for i, inDat in enumerate(inDats + self.defPar.Inputdefs.evalOPs()):
+			if not inDat[1, 'name']:
+				continue
+			func = str(inDat[1, 'input:alias'] or f'inputOp{i + 1}')
+			self.inputs.append(_InputInfo(
+				inputFunc=func,
+				name=str(inDat[i, 'name']),
+				path=str(inDat[i, 'path']),
+				coordType=str(inDat[i, 'coordType']),
+				contextType=str(inDat[i, 'contextType']),
+				returnType=str(inDat[i, 'returnType']),
+				placeholder=f'inputOp_{func}' if not func.startswith('inputOp') else func,
+				vars=str(inDat[i, 'input:vars']),
+				varInputs=str(inDat[i, 'input:varInputs']),
+			))
+
+	def addError(self, message: str, path: 'Optional[str]' = None, level: str = 'error'):
+		self.opState.validationErrors.append(ValidationError(
+			path=path or parent(2).path, level=level, message=message))
+
+	def loadCode(
+			self,
+			functionCode: 'DAT',
+			materialCode: 'DAT',
+			initCode: 'DAT',
+			opGlobals: 'DAT',
+	):
+		self.opState.functionCode = functionCode.text
+		self.opState.materialCode = materialCode.text
+		self.opState.initCode = initCode.text
+		self.opState.opGlobals = opGlobals.text
+
+	def loadMacros(self, paramSpecTable: 'DAT', paramTupletTable: 'DAT', opElementTable: 'DAT'):
+		macros = []
+		for inputInfo in self.inputs:
+			macros.append(Macro(_getHasInputMacroName(inputInfo.inputFunc)))
+		macroParams = []
+		angleParamNames = []
+		for i in range(1, paramSpecTable.numRows):
+			if paramSpecTable[i, 'handling'] != 'macro':
+				continue
+			par = self.paramsOp.par[paramSpecTable[i, 'localName']]
+			if par is None:
+				continue
+			macroParams.append(par)
+			if paramSpecTable[i, 'conversion'] == 'angle':
+				angleParamNames.append(par.name)
+		macroParamVals = {}
+		for par in macroParams:
+			name = par.name
+			val = par.eval()
+			if name in angleParamNames:
+				val = math.radians(val)
+			macroParamVals[name] = val
+			style = par.style
+			if style in ('Menu', 'Str', 'StrMenu'):
+				macros.append(Macro(f'{self.namePrefix}{name}_{val}'))
+				macros.append(Macro(self.namePrefix + name, val))
+			elif style == 'Toggle':
+				if val:
+					macros.append(Macro(self.namePrefix + name, 1))
+			else:
+				macros.append(Macro(self.namePrefix + name, val))
+		for i in range(1, paramTupletTable.numRows):
+			if paramTupletTable[i, 'handling'] != 'macro':
+				continue
+			size = int(paramTupletTable[i, 'size'])
+			if size < 2:
+				continue
+			tuplet = paramTupletTable[i, 'tupletLocalName'].val
+			parts = [
+				repr(macroParamVals[p])
+				for p in paramTupletTable[i, 'localNames'].val.split(' ')
+			]
+			partsJoined = ','.join(parts)
+			macros.append(Macro(self.namePrefix + tuplet, f'vec{size}({partsJoined})'))
+		tables = [self.defPar.Macrotable.eval()] + self.defPar.Generatedmacrotables.evalOPs() + [
+			op(c) for c in opElementTable.col('macroTable')[1:]
+		]
+		for table in tables:
+			if not table or not table.numCols or not table.numRows:
+				continue
+			if table.numCols == 3:
+				for row in table.rows():
+					macros.append(Macro(row[1].val, row[2].val, not _isFalseStr(row[0])))
+			elif table.numCols == 1:
+				for cell in table.col(0):
+					macros.append(Macro(cell.val))
+			elif table.numCols == 2:
+				for row in table.rows():
+					macros.append(Macro(row[0].val, row[1].val))
+			else:
+				for row in table.rows():
+					macros.append(Macro(row[1].val, ' '.join([c.val for c in row[2:]]), not _isFalseStr(row[0])))
+		self.opState.macros = macros
+
+	def loadTextures(self):
+		self.opState.textures = []
+		table = self.defPar.Texturetable.eval()
+		if not table or table.numRows < 2:
+			return
+		for i in range(1, table.numRows):
+			if _isFalseStr(table[i, 'enable']):
+				continue
+			name = table[i, 'name']
+			path = table[i, 'path']
+			if not name or not path:
+				continue
+			self.opState.textures.append(Texture(
+				name=self.namePrefix + name.val,
+				path=path.val,
+				type=str(table[i, 'type'] or '2d')
+			))
+
+	def loadBuffers(self):
+		self.opState.buffers = []
+		table = self.defPar.Buffertable.eval()
+		if not table or table.numRows < 2:
+			return
+		for i in range(1, table.numRows):
+			if _isFalseStr(table[i, 'enable']):
+				continue
+			name = str(table[i, 'name'] or '')
+			path = str(table[i, 'chop'] or '')
+			expr1 = str(table[i, 'expr1'] or '')
+			expr2 = str(table[i, 'expr2'] or '')
+			expr3 = str(table[i, 'expr3'] or '')
+			expr4 = str(table[i, 'expr4'] or '')
+			if not name:
+				continue
+			if not path and not expr1 and not expr2 and not expr3 and not expr4:
+				continue
+			self.opState.buffers.append(Buffer(
+				name=self.namePrefix + name,
+				type=str(table[i, 'type'] or 'vec4'),
+				chop=path,
+				uniformType=str(table[i, 'uniformType'] or 'uniformarray'),
+				length=int(table[i, 'length']),
+				expr1=expr1,
+				expr2=expr2,
+				expr3=expr3,
+				expr4=expr4,
+			))
+
+	def loadReferences(self):
+		self.opState.references = []
+		table = self.defPar.Referencetable.eval()
+		if not table or table.numRows < 2:
+			return
+		for i in range(1, table.numRows):
+			localName = str(table[1, 'name'])
+			sourcePath = str(table[1, 'sourcePath'])
+			if localName == 'none' or not localName or not sourcePath:
+				continue
+			sourceOp = op(sourcePath)
+			if not sourceOp:
+				self.addError(f'Invalid source path for reference {localName}')
+				continue
+			self.opState.references.append(Reference(
+				name=self.namePrefix + localName,
+				localName=localName,
+				sourcePath=sourcePath,
+				sourceName=str(table[i, 'sourceName']),
+				dataType=str(table[i, 'dataType']),
+				owner=self.opName,
+			))
+
+	def loadVariables(self):
+		self.opState.variables = []
+		table = self.defPar.Variabletable.eval()
+		if not table or table.numRows < 2:
+			return
+		for i in range(1, table.numRows):
+			if _isFalseStr(table[i, 'enable']):
+				continue
+			localName = table[i, 'name'].val
+			self.opState.variables.append(Variable(
+				name=self.namePrefix + localName,
+				localName=localName,
+				label=table[i, 'label'].val or localName,
+				dataType=table[i, 'dataType'].val,
+				owner=self.opName,
+				macros=str(table[i, 'macros'] or ''),
+			))
+
+	def loadDispatchBlocks(self):
+		self.opState.dispatchBlocks = []
+		table = self.defPar.Dispatchtable.eval()
+		if not table or table.numRows < 2:
+			return
+		for i in range(1, table.numRows):
+			name = str(table[i, 'name'] or '')
+			if not name or _isFalseStr(table[i, 'enable']):
+				continue
+			self.opState.dispatchBlocks.append(Dispatch(
+				name=self.namePrefix + name,
+				category=table[i, 'category'].val,
+				code=table[i, 'code'].val,
+			))
+
+	def loadInputNames(self, definition: 'DAT'):
+		self.opState.inputNames = tdu.split(definition[1, 'inputNames'])
+
+	# def prepareReplacements(
+	# 		self,
+	# 		inputTable: 'DAT',
+	# 		elementTable: 'DAT',
+	# ):
+	# 	self.replacements = {
+	# 		'thismap': self.opState.name,
+	# 		'THIS_': self.opState.name + '_',
+	# 	}
+	# 	if self.opState.materialId:
+	# 		self.replacements['THISMAT'] = self.opState.materialId
+	# 	if inputTable.numRows > 1:
+	# 		for i in range(1, inputTable.numRows):
+	# 			self.replacements[inputTable[i, 'placeholder'].val] = inputTable[i, 'name'].val
+	# 	if elementTable.numRows > 1:
+	# 		for phCol in elementTable.cells(0, 'placeholder*'):
+	# 			i = tdu.digits(phCol.val)
+	# 			for row in range(1, elementTable.numRows):
+	# 				placeholder = elementTable[row, phCol].val
+	# 				if not placeholder:
+	# 					continue
+	# 				codeDat = op(elementTable[row, f'code{i}'])
+	# 				self.replacements[placeholder] = codeDat.text if codeDat else ''
+
+_typePattern = re.compile(r'\b[CR][a-z]+T\b')
+_typeRepls = {'CoordT': 'THIS_CoordT', 'ContextT': 'THIS_ContextT', 'ReturnT': 'THIS_ReturnT'}
+def _typeRepl(m): return _typeRepls.get(m.group(0), m.group(0))
+
+def _isFalseStr(val):
+	return val in ('0', 'False')
+
+def _getHasInputMacroName(inputAlias: str):
+	if tdu.base(inputAlias) == 'inputOp':
+		i = tdu.digits(inputAlias)
+		if i is not None:
+			return f'THIS_HAS_INPUT_{i}'
+	return f'THIS_HAS_INPUT_{inputAlias}'
+
+@dataclass
+class _InputInfo:
+	inputFunc: str
+	name: str
+	path: str
+	coordType: str
+	contextType: str
+	returnType: str
+	placeholder: str
+	vars: str
+	varInputs: str
+
+def onSetupParameters(dat: 'scriptDAT'):
+	page = dat.appendCustomPage('Custom')
+	page.appendToggle('Pretty')
+
+def onCook(dat: 'DAT'):
+	dat.clear()
+	builder = _Builder(
+		typeTable=op('types'),
+	)
+	# builder.loadCode(
+	# 	functionCode=op('function'),
+	# 	materialCode=op('materialCode'),
+	# 	initCode=op('initCode'),
+	# 	opGlobals=op('opGlobals'),
+	# )
+	builder.loadMacros(
+		paramSpecTable=op('paramSpecTable'),
+		paramTupletTable=op('param_tuplets'),
+		opElementTable=op('opElements'),
+	)
+	builder.loadTextures()
+	builder.loadBuffers()
+	builder.loadReferences()
+	builder.loadVariables()
+	builder.loadDispatchBlocks()
+	builder.loadInputNames(op('definition'))
+
+	dat.write(builder.opState.toJson(pretty=dat.par.Pretty.eval()))
+	# dat.write(repr(builder.opState))
---
+++ src/lib/raytkState.py
@@ -0,0 +1,143 @@
+from dataclasses import dataclass, field
+import dataclasses
+import json
+from typing import List, Optional, Union
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+_TypeSpecT = Union[str, List[str]]
+
+@dataclass
+class _StateObject:
+	def toDict(self):
+		d = {}
+		for f in dataclasses.fields(self):
+			val = getattr(self, f.name, None)
+			if _shouldInclude(val) and val != f.default:
+				if isinstance(val, list) and isinstance(val[0], _StateObject):
+					val = [v.toDict() for v in val]
+				elif isinstance(val, _StateObject):
+					val = val.toDict()
+				d[f.name] = val
+		return d
+
+	def toJson(self, pretty=False):
+		obj = self.toDict()
+		return json.dumps(obj, indent='  ' if pretty else None, sort_keys=True)
+
+	@classmethod
+	def fromJson(cls, text: str):
+		if not text:
+			return None
+		obj = json.loads(text)
+		return cls(**obj)
+
+def _shouldInclude(val):
+	if val is None or val == '':
+		return False
+	if isinstance(val, (list, dict)) and not val:
+		return False
+	return True
+
+@dataclass
+class RopState(_StateObject):
+	opType: str
+	toolkitVersion: str
+	name: str
+
+	coordType: _TypeSpecT
+	contextType: _TypeSpecT
+	returnType: _TypeSpecT
+
+	functionCode: Optional[str] = None
+	materialCode: Optional[str] = None
+	initCode: Optional[str] = None
+	opGlobals: Optional[str] = None
+
+	macros: Optional[List['Macro']] = None
+	textures: Optional[List['Texture']] = None
+	buffers: Optional[List['Buffer']] = None
+	references: Optional[List['Reference']] = None
+	variables: Optional[List['Variable']] = None
+	dispatchBlocks: Optional[List['Dispatch']] = None
+
+	materialId: Optional[str] = None
+
+	inputNames: Optional[List[str]] = None
+	libraryNames: Optional[List[str]] = None
+
+	paramSource: Optional[str] = None
+
+	validationErrors: Optional[List['ValidationError']] = field(default_factory=list)
+
+@dataclass
+class ValidationError(_StateObject):
+	path: str
+	level: str
+	message: str
+
+@dataclass
+class Macro(_StateObject):
+	name: str
+	value: Union[None, str, int, bool, float] = None
+	enable: bool = True
+
+@dataclass
+class Texture(_StateObject):
+	name: str
+	path: str
+	type: str
+
+@dataclass
+class Reference(_StateObject):
+	name: str
+	localName: str
+	sourcePath: str
+	sourceName: str
+	dataType: str
+	owner: str
+
+@dataclass
+class Variable(_StateObject):
+	name: str
+	localName: str
+	label: str
+	dataType: str
+	owner: str
+	macros: str
+
+@dataclass
+class Dispatch(_StateObject):
+	name: str
+	category: str
+	code: str
+
+@dataclass
+class Buffer(_StateObject):
+	name: str
+	type: str
+	chop: str
+	uniformType: str
+	length: int
+	expr1: Optional[str] = None
+	expr2: Optional[str] = None
+	expr3: Optional[str] = None
+	expr4: Optional[str] = None
+
+@dataclass
+class ParamTuplet(_StateObject):
+	name: str
+	localName: str
+	source: str
+	size: int
+	part1: Optional[str] = None
+	part2: Optional[str] = None
+	part3: Optional[str] = None
+	part4: Optional[str] = None
+	status: Optional[str] = None
+	conversion: Optional[str] = None
+	handling: str = 'runtime'
+	localNames: Optional[List[str]] = None
