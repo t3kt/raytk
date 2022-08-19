70f3c2b886d878b176ac9d857d35ee23cae735e7
t3kt <tekt@immerse.studio>
2022-08-19 15:30:52 -0700

    colors, variables, etc for hexagonalWeavePattern
    close #278, see #254

--- src/operators/pattern/hexagonalWeavePattern.glsl
+++ src/operators/pattern/hexagonalWeavePattern.glsl
@@ -15,13 +15,11 @@ vec4 THIS_getHex(vec2 p){
 	: vec4(b, hC.zw+.5);
 }
 
-float THIS_mask( vec2 p, float thickness) {
+float THIS_getMask( vec2 p, float thickness) {
 	float ia = floor( atan(p.y,p.x) *3./TAU) + .5;
 	pR(p, ia * TAU / 3.);
 	p.x -= 1.;
-	float rad2 = .666;
-
-	rad2 = mix(.85, .49, thickness);
+	float rad2 = mix(.85, .49, thickness);
 	return length(p) - rad2;
 }
 
@@ -30,6 +28,11 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float thickness = adaptAsFloat(inputOp_thicknessField(p, ctx));
 	#else
 	float thickness = THIS_Thickness;
+	#endif
+	#ifdef THIS_HAS_INPUT_blendingField
+	float b = adaptAsFloat(inputOp_blendingField(p, ctx));
+	#else
+	float b = THIS_Blending;
 	#endif
 	#ifdef THIS_HAS_INPUT_coordField
 	vec2 q = adaptAsVec2(inputOp_coordField(p, ctx));
@@ -40,27 +43,65 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q /= THIS_Size;
 
 	vec2 R = vec2(100); // REPLACE THIS IT WAS RESOLUTION
-	vec4 O = vec4(0.);
 	vec4 hp = THIS_getHex(q);
 
 	vec2 hp2 = hp.xy;
 	pR(hp2, -TAU/6.);
-	vec2 D = vec2( THIS_mask(hp.xy, thickness), THIS_mask(hp2, thickness) );      // axial distance of the 2 layers
+	vec2 D = vec2( THIS_getMask(hp.xy, thickness), THIS_getMask(hp2, thickness) );      // axial distance of the 2 layers
+	#ifdef THIS_EXPOSE_axialdist
+	THIS_axialdist = D;
+	#endif
 	vec2 M = smoothstep(-5./R.y,5./R.y, D);                       // masks
-	vec2 B = smoothstep(0.,.05, D);                               // border decoration
+	vec2 B = smoothstep(0.,b, D);                               // border decoration
 	// B = smoothstep(0., 1., (.65 + D*D*4.)*B);                     // shaded relief
 	#ifdef THIS_Randomize
 	float m = THIS_rnd(hp.zw) > .5 ? M.x : 1.-M.y;                     // layer-preserving random control
 	#else
 	float m = M.x;
 	#endif
+	#ifdef THIS_EXPOSE_mask
+	THIS_mask = vec3(M, m);
+	#endif
 
-	// O += mix( B.y, B.x, m );                                      // simple display
-	O += mix( B.y*.5, B.x, m );                                   // 2-tints display
+	#ifdef THIS_HAS_INPUT_color1Field
+	vec4 col1 = fillToVec4(inputOp_color1Field(p, ctx));
+	#else
+	vec4 col1 = vec4(THIS_Color1, 1.);
+	#endif
+	#ifdef THIS_HAS_INPUT_color2Field
+	vec3 col2 = fillToVec3(inputOp_color2Field(p, ctx));
+	#else
+	vec3 col2 = THIS_Color2;
+	#endif
+	#ifdef THIS_HAS_INPUT_bgColorField
+	vec3 bgCol = fillToVec3(inputOp_bgColorField(p, ctx));
+	#else
+	vec3 bgCol = THIS_Bgcolor;
+	#endif
+
+	vec4 res = vec4(0.);
+	#ifdef THIS_Format_customcolor
+	res = col1;
+	#else
+	// res += mix( B.y, B.x, m );                                      // simple display
+//	res += mix( B.y*.5, B.x, m );                                   // 2-tints display
+	res.rgb += mix(col1.rgb*B.y, col2*B.x, m);
+	res.rgb += bgCol * (1.0-max(M.x, M.y));
+//	res.rgb += mix(bgCol, mix(col1.rgb*B.y, col2*B.x, m), max(M.x,M.y));
+//	res.rgb += mix(
+//		mix(bgCol, col1*B.y, M.y),
+//		mix(bgCol, col2*B.x, M.x),
+//		m);
+//	res.rgb += mix(
+//		bgCol,
+//		mix(col1*B.y, col2*B.x, m),
+//		max(M.x, M.y)
+//	);
 	// --- for next ones uncomment original random control
-	// O += mix( -4.*D.x*B.y, B.x, M.x);                             // shadows
-	// O += mix( -D.x*B.y, (1.+D.y)*B.x, M.x);                       // pseudo-Z
-	return O;
+	// res += mix( -4.*D.x*B.y, B.x, M.x);                             // shadows
+	// res += mix( -D.x*B.y, (1.+D.y)*B.x, M.x);                       // pseudo-Z
+	#endif
+	return res;
 }
 
 #else
