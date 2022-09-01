803017c00f648ff5c891280585bd373e5efb4ddf
t3kt <tekt@immerse.studio>
2022-09-01 12:00:12 -0700

    cleanup and improvements in vectorToFloat
    use codeSwitcher, and add option for length(XY)

--- src/operators/convert/vectorToFloat.glsl
+++ src/operators/convert/vectorToFloat.glsl
@@ -1,8 +1,7 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#pragma r:if THIS_USE_LENGTH
-	return length(inputOp1(p, ctx).THIS_PARTS);
-	#pragma r:else
-	return inputOp1(p, ctx).THIS_PART;
-	#pragma r:endif
+	vec4 val = inputOp1(p, ctx);
+	float res;
+	BODY();
+	return res;
 }
 
