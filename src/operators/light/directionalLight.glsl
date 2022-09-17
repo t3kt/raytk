f8fb38611b9bf8eb3d9d1067dc7427f7180f594d
t3kt <tekt@immerse.studio>
2022-09-16 17:03:55 -0700

    fix shadow distance bug in directionalLight
    close #960

--- src/operators/light/directionalLight.glsl
+++ src/operators/light/directionalLight.glsl
@@ -1,5 +1,5 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec3 pos = p + THIS_Direction;
+	vec3 pos = p + normalize(THIS_Direction) * RAYTK_MAX_DIST;
 	vec3 col = THIS_Color * THIS_Intensity * clamp(dot(ctx.normal, THIS_Direction), 0., 1.);
 	Light light = createLight(pos, col);
 	light.supportShadow = IS_TRUE(THIS_Enableshadow);
