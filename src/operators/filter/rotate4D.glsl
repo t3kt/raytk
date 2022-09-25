7ebcf26358b2202b691f8e6a14cbd5510ae6a4ab
t3kt <tekt@immerse.studio>
2022-09-25 12:43:52 -0700

    fields and variables for rotate4D
    see #966

--- src/operators/filter/rotate4D.glsl
+++ src/operators/filter/rotate4D.glsl
@@ -1,10 +1,24 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 p3 = adaptAsVec3(p);
 	float f = length(p3);
-	float r = THIS_Rotate;
-	vec4 piv = vec4(THIS_Pivot, THIS_Pivotw);
 
 	vec4 p4 = inverseStereographic(p3);
+
+	#ifdef THIS_EXPOSE_pos4d
+	THIS_pos4d = p4;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_rotateField
+	float r = radians(adaptAsFloat(inputOp_rotateField(p, ctx)));
+	#else
+	float r = THIS_Rotate;
+	#endif
+	#ifdef THIS_HAS_INPUT_pivotField
+	vec4 piv = inputOp_pivotField(p, ctx);
+	#else
+	vec4 piv = vec4(THIS_Pivot, THIS_Pivotw);
+	#endif
+
 	p4 -= piv;
 	pR(p4.THIS_Plane, r);
 	p4 += piv;
