8ece43f935821ecf7361a45377ce3a2c5180699e
t3kt <tekt@immerse.studio>
2022-09-26 16:39:10 -0700

    rotation option for matCapContrib

--- src/operators/material/matCapContrib.glsl
+++ src/operators/material/matCapContrib.glsl
@@ -2,7 +2,14 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float level = THIS_Level * THIS_Enable;
 	vec3 reflected = reflect(ctx.ray.dir, ctx.normal);
 	float m = 2.8284271247461903 * sqrt( reflected.z+1.0 );
-	vec2 uv = reflected.xy / m + 0.5;
+	vec2 uv = reflected.xy / m;
+	#ifdef THIS_HAS_INPUT_rotateField
+	float r = radians(inputOp_rotateField(p, ctx));
+	#else
+	float r = THIS_Rotate;
+	#endif
+	pR(uv, r);
+	uv += 0.5;
 	uv = clamp(uv, vec2(0.), vec2(1.));
 	#if defined(RAYTK_LOD_IN_MATERIAL_CONTEXT) && defined(THIS_CONTEXT_TYPE_MaterialContext)
 	vec4 value = textureLod(THIS_texture, uv, ctx.lod);
