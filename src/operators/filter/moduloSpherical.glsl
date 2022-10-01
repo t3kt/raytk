3a5864bc6af28673efa15134655c50b6fe4ce523
t3kt <tekt@immerse.studio>
2022-09-30 17:11:00 -0700

    add variables and some cleanup in moduloSpherical
    close #746

--- src/operators/filter/moduloSpherical.glsl
+++ src/operators/filter/moduloSpherical.glsl
@@ -5,15 +5,26 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 		vec3 pivot = THIS_Pivot;
 		p -= pivot;
 		vec2 sh = THIS_Shift * TAU;
-		vec2 r = THIS_Repetitions * vec2(1., 2.);
-		vec2 a = TAU / r;
-		vec2 ha = 0.5 * a;
 		pR(p.xz, sh.x);
-		p.xz = sin(mod(atan(p.z, p.x) + ha.x, a.x) - ha.x + vec2(PHI, 0.0)) * length(p.xz);
+		vec2 r = THIS_Repetitions;
+		vec2 a = TAU / (r * vec2(1., 2.));
+		vec2 ha = 0.5 * a;
+		vec2 cell = vec2(0.);
+		float a2 = atan(p.z, p.x) + ha.x;
+		cell.y = floor(a2 / a.x);
+		p.xz = sin(mod(a2, a.x) - ha.x + vec2(PHI, 0.0)) * length(p.xz);
 		pR(p.xy, sh.y);
-		p.xy = sin(mod(atan(p.y, p.x) + ha.y, a.y) - ha.y + vec2(PHI, 0.0)) * length(p.xy);
+		a2 = atan(p.y, p.x) + ha.y;
+		cell.x = floor(a2 / a.y);
+		p.xy = sin(mod(a2, a.y) - ha.y + vec2(PHI, 0.0)) * length(p.xy);
 		p.xy -= THIS_Offset;
 		p += pivot;
+		#ifdef THIS_EXPOSE_cell
+		THIS_cell = cell;
+		#endif
+		#ifdef THIS_EXPOSE_normcell
+		THIS_normcell = cell / r;
+		#endif
 	}
 	return inputOp1(p, ctx);
 }
