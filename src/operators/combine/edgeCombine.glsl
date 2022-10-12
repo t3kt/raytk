a73597d3ffa914a3254f150f2d9668e8f9ca2c29
t3kt <tekt@immerse.studio>
2022-10-11 18:23:47 -0700

    cleanup in edgeCombine

--- src/operators/combine/edgeCombine.glsl
+++ src/operators/combine/edgeCombine.glsl
@@ -6,16 +6,16 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#else
 	ReturnT res1 = inputOp1(p, ctx);
 	ReturnT res2 = inputOp2(p, ctx);
-	if (THIS_Swapinputs > 0.) {
+	if (IS_TRUE(THIS_Swapinputs)) {
 		swap(res1, res2);
 	}
-	#ifdef THIS_HAS_INPUT_3
-	float r = inputOp3(p, ctx);
+	#ifdef THIS_HAS_INPUT_radiusField
+	float r = inputOp_radiusField(p, ctx);
 	#else
 	float r = THIS_Radius;
 	#endif
-	#ifdef THIS_HAS_INPUT_4
-	float d = inputOp4(p, ctx);
+	#ifdef THIS_HAS_INPUT_depthField
+	float d = inputOp_depthField(p, ctx);
 	#else
 	float d = THIS_Depth;
 	#endif
