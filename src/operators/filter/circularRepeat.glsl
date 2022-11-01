f00357f62737c022456e9d546c99923ca3426577
t3kt <tekt@immerse.studio>
2022-11-01 12:10:06 -0700

    3D support and a snippet for circularRepeat
    see #962

--- src/operators/filter/circularRepeat.glsl
+++ src/operators/filter/circularRepeat.glsl
@@ -5,8 +5,19 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float cs = THIS_Cellsize;
 	float ra = THIS_Radius/cs;
 
+	vec2 q;
+	#ifdef THIS_COORD_TYPE_vec2
+	q = p;
+	#else
+	switch (THIS_Plane) {
+		case THISTYPE_Plane_xy: q = p.xy; break;
+		case THISTYPE_Plane_yz: q = p.yz; break;
+		case THISTYPE_Plane_zx: q = p.zx; break;
+	}
+	#endif
+
 	// make grid
-	vec2 id0 = round(p/cs);
+	vec2 id0 = round(q/cs);
 
 	// snap to circle
 	if (dot2(id0) > ra*ra) id0 = round(normalize(id0) * ra);
@@ -20,8 +31,6 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	for (int i = -window; i <= window; i++) {
 		vec2 id = id0 + vec2(i, j);
 		if (dot2(id) <= ra*ra) {
-			vec2 q = p - cs*id;
-
 			#ifdef THIS_EXPOSE_cellcoord
 			THIS_cellcoord = ivec2(id);
 			#endif
@@ -29,7 +38,18 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 			THIS_cellchecker = mod(id.x+id.y, 2.) > .5;
 			#endif
 
-			res = cmb_simpleUnion(res, inputOp1(q, ctx));
+			vec2 q1 = q - cs*id;
+			#ifdef THIS_COORD_TYPE_vec2
+			vec2 localP = q1;
+			#else
+			CoordT localP = p;
+			switch (THIS_Plane) {
+				case THISTYPE_Plane_xy: localP.xy = q1; break;
+				case THISTYPE_Plane_yz: localP.yz = q1; break;
+				case THISTYPE_Plane_zx: localP.zx = q1; break;
+			}
+			#endif
+			res = cmb_simpleUnion(res, inputOp1(localP, ctx));
 		}
 	}
 
---
+++ src/operators/filter/circularRepeat.md
@@ -0,0 +1,20 @@
+Repeat an SDF to fill a 2D circular area.
+
+## Parameters
+
+* `Enable`
+* `Plane`
+  * `xy`
+  * `yz`
+  * `zx`
+* `Radius`: Size of the area to fill.
+* `Cellsize`: Spacing between copies.
+
+## Inputs
+
+* `definition_in`: 
+
+## Variables
+
+* `cellcoord`: 
+* `cellchecker`: 
