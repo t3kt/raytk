a03e193cc2f6d477273cdf8c2752265dfdc4882b
t3kt <tekt@immerse.studio>
2022-11-04 19:06:56 -0700

    field inputs for triangleSdf2d
    see #588

--- src/operators/sdf2d/triangleSdf2d.glsl
+++ src/operators/sdf2d/triangleSdf2d.glsl
@@ -1,4 +1,14 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_radiusField
+	float r = inputOp_radiusField(p, ctx);
+	#else
+	float r = THIS_Radius;
+	#endif
+	#ifdef THIS_HAS_INPUT_sizeField
+	vec2 sz = fillToVec2(inputOp_sizeField(p, ctx));
+	#else
+	vec2 sz = vec2(THIS_Width, THIS_Height);
+	#endif
 	float d;
 	BODY();
 	return createSdf(d);
