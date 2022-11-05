d9322ac5502b23a1c19b0ccabc25119a25ce0945
t3kt <tekt@immerse.studio>
2022-11-04 19:28:31 -0700

    fields and parameters for circleWaveSdf2d
    see #588

--- src/operators/sdf2d/circleWaveSdf2d.glsl
+++ src/operators/sdf2d/circleWaveSdf2d.glsl
@@ -2,8 +2,30 @@
 // https://www.shadertoy.com/view/stGyzt
 
 ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_curlField
+	float tb = inputOp_curlField(p, ctx);
+	#else
 	float tb = THIS_Curl;
+	#endif
+	#ifdef THIS_HAS_INPUT_radiusField
+	float ra = inputOp_radiusField(p, ctx);
+	#else
 	float ra = THIS_Radius;
+	#endif
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float th = inputOp_thicknessField(p, ctx);
+	#else
+	float th = THIS_Thickness;
+	#endif
+	#ifdef THIS_HAS_INPUT_offsetField
+	float of = inputOp_offsetField(p, ctx);
+	#else
+	float of = THIS_Offset;
+	#endif
+
+	AXIS_BODY();
+
+	p.x -= of;
 
 	tb = PI*5.0/6.0 * max(tb,0.0001);
 	vec2 co = ra*vec2(sin(tb),cos(tb));
@@ -15,6 +37,6 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float d1 = ((co.y*p1.x>co.x*p1.y) ? length(p1-co) : abs(length(p1)-ra));
 	float d2 = ((co.y*p2.x>co.x*p2.y) ? length(p2-co) : abs(length(p2)-ra));
 
-	float d = min( d1, d2) - THIS_Thickness;
+	float d = min( d1, d2) - th;
 	return createSdf(d);
 }
