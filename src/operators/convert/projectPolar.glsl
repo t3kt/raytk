0fc9ef591ac0ccfb912a3f51058f2e4ef520e0e2
t3kt <tekt@immerse.studio>
2022-11-09 15:28:48 -0800

    docs and scaling for projectPolar

--- src/operators/convert/projectPolar.glsl
+++ src/operators/convert/projectPolar.glsl
@@ -1,10 +1,11 @@
-float THIS_ang(float a) { return a / TAU; }
+float THIS_ang(float a) { return a / TAU + 0.5; }
 
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 p3 = adaptAsVec3(p);
 	AXIS_BODY();
-	float ra = THIS_Radius;
-	float th = THIS_Thickness;
+	float r = THIS_Radius;
+	float t = THIS_Thickness;
+	float h = THIS_Height;
 	vec3 q;
 	PROJECT_BODY();
 	return inputOp1(inputOp1_asCoordT(q), ctx);
