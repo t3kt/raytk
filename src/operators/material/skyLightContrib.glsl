9270452a1e6defe468f8e10d1917bb03cdf39dcd
t3kt <tekt@immerse.studio>
2022-11-17 12:49:09 -0800

    uselight and useray tags
    see #982, #981

--- src/operators/material/skyLightContrib.glsl
+++ src/operators/material/skyLightContrib.glsl
@@ -1,6 +1,4 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	vec3 lightDir = normalize(p - ctx.light.pos);
-	vec3 viewDir = normalize(-ctx.ray.dir);
 	vec3 norm = normalize(ctx.normal);
 	float amount = clamp(0.5+0.5*dot(normalize(ctx.normal), THIS_Dir), 0, 1) * THIS_Level;
 	#if defined(THIS_RETURN_TYPE_float)
