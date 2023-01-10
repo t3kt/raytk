2931fca342cf037234d20bc07c0866cb78ef215e
t3kt <tekt@immerse.studio>
2023-01-10 14:45:13 -0800

    fix missing source file for extrude
    not sure why i removed it...

---
+++ src/operators/convert/extrude.glsl
@@ -0,0 +1,49 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	float axisPos;
+	vec2 planePos;
+	switch (int(THIS_Axis)) {
+		case THISTYPE_Axis_x: axisPos = p.x; planePos = p.yz; break;
+		case THISTYPE_Axis_y: axisPos = p.y; planePos = p.zx; break;
+		case THISTYPE_Axis_z: axisPos = p.z; planePos = p.xy; break;
+	}
+	#ifdef THIS_EXPOSE_axispos
+	THIS_axispos = axisPos;
+	#endif
+	ReturnT res;
+	if (IS_TRUE(THIS_Infiniteheight)) {
+		if (THIS_Iterationtype == THISTYPE_Iterationtype_ratio) {
+			setIterationIndex(ctx, axisPos);
+		}
+		#ifdef THIS_EXPOSE_normoffset
+		THIS_normoffset = axisPos;
+		#endif
+		res = inputOp_crossSection(planePos, ctx);
+	} else {
+		#ifdef THIS_HAS_INPUT_heightField
+		float h = inputOp_heightField(p, ctx);
+		#else
+		float h = THIS_Height;
+		#endif
+		#ifdef THIS_HAS_INPUT_offsetField
+		float o = inputOp_offsetField(p, ctx);
+		#else
+		float o = THIS_Offset;
+		#endif
+		float ratio = map01(axisPos - o, -h/2., h/2.);
+		if (THIS_Iterationtype == THISTYPE_Iterationtype_ratio) {
+			setIterationIndex(ctx, ratio);
+		}
+		#ifdef THIS_EXPOSE_normoffset
+		THIS_normoffset = ratio;
+		#endif
+		res = inputOp_crossSection(planePos, ctx);
+		vec2 w = vec2(res.x, abs(axisPos - o) - h);
+		res.x = min(max(w.x, w.y), 0.) + length(max(w, 0.));
+		if (THIS_Uvmode == THISTYPE_Uvmode_depth) {
+			#ifdef RAYTK_USE_UV
+			res.uv.y = mix(res.uv.y, w.y, ratio);
+			#endif
+		}
+	}
+	return res;
+}
