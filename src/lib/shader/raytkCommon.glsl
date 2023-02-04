80a30b30180d2a41dfc2526e52d665e937aad375
t3kt <tekt@immerse.studio>
2023-02-04 15:33:45 -0800

    initial 4d conversion stuff
    see #965

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -431,3 +431,14 @@ void setAxisPlane(inout vec3 p, int axis, vec2 val) {
 vec3 bToC(vec3 A, vec3 B, vec3 C, vec3 barycentric) {
 	return barycentric.x * A + barycentric.y * B + barycentric.z * C;
 }
+
+// https://www.shadertoy.com/view/fdfSDH
+// https://www.shadertoy.com/view/lsGyzm
+vec4 inverseStereographic(vec3 p) {
+  float k = 2.0/(1.0+dot(p,p));
+  return vec4(k*p,k-1.0);
+}
+vec3 stereographic(vec4 p4) {
+  float k = 1.0/(1.0+p4.w);
+  return k*p4.xyz;
+}
