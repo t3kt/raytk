e469ae1eb560face47ead36ca4f8b96f5925888f
t3kt <tekt@immerse.studio>
2023-02-14 13:04:56 -0800

    field inputs, variables for branchingTreeSdf2d
    close #1015

--- src/operators/sdf2d/branchingTreeSdf2d.glsl
+++ src/operators/sdf2d/branchingTreeSdf2d.glsl
@@ -3,20 +3,40 @@
 
 ReturnT thismap(CoordT p, ContextT ctx) {
 	float b = THIS_Branches;
+	#ifdef THIS_HAS_INPUT_radiusField
+	float r = inputOp_radiusField(p, ctx);
+	#else
 	float r = THIS_Radius;
+	#endif
 	float f = THIS_Forks;
+
 	vec2 polarP = vec2((atan(p.x,p.y)/TAU) * b, length(p) * f / r);
+	#ifdef THIS_EXPOSE_normdist
+	THIS_normdist = polarP.y / f;
+	#endif
+	#ifdef THIS_EXPOSE_forkindex
+	THIS_forkindex = int(polarP.y);
+	#endif
+	#ifdef THIS_EXPOSE_normforkindex
+	THIS_normforkindex = floor(polarP.y) / f;
+	#endif
+
+	float e = THIS_Exponent;
 
 	float Y,y,X;
 	// the magic number hoha
 	Y = exp2( floor(polarP.y));
-	y = .5+( pow(fract(polarP.y),.85 ))*.5+.5;
+	y = .5+( pow(fract(polarP.y),e))*.5+.5;
 	X = abs(polarP.x* Y);
 	polarP.x = abs(fract(X-y*.5)-.5)*2.;
 	polarP.x = max(polarP.x,abs(fract(X+ y*.5)-.5)*2. );
 
 	float width;
-	width = mapRange(polarP.y, 0., f, THIS_Thicknessinner, THIS_Thicknessouter);
+	#ifdef THIS_HAS_INPUT_thicknessField
+	width = inputOp_thicknessField(p, ctx);
+	#else
+	width = mix(THIS_Thicknessinner, THIS_Thicknessouter, polarP.y / f);
+	#endif
 	float outerlimit = polarP.y-f;
 	float scaleAdjusted= 1.5*(((1.-polarP.x)/Y)*polarP.y);
 	float d = scaleAdjusted-width;
