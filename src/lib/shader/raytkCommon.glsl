fab53a9ce73d3d6a1525377812898c967fc0c5ed
t3kt <tekt@immerse.studio>
2023-03-01 14:01:46 -0800

    added lookAtRotate
    close #1024

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -97,6 +97,15 @@ mat4 lookAtViewMatrix(vec3 eye, vec3 center, vec3 up) {
 	vec4(0.0, 0.0, 0.0, 1)
 	);
 }
+// https://github.com/glslify/glsl-look-at/
+mat3 calcLookAtMatrix(vec3 origin, vec3 target, float roll) {
+  vec3 rr = vec3(sin(roll), cos(roll), 0.0);
+  vec3 ww = normalize(target - origin);
+  vec3 uu = normalize(cross(ww, rr));
+  vec3 vv = normalize(cross(uu, ww));
+
+  return mat3(uu, vv, ww);
+}
 
 float ndot(vec2 a, vec2 b ) { return a.x*b.x - a.y*b.y; }
 
