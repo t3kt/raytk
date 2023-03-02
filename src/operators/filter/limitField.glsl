9fd08d05dfbb7d5a8ce9099c1ae2c425ecd22d9f
t3kt <tekt@immerse.studio>
2023-03-02 12:09:09 -0800

    field inputs for limitField
    see #812

--- src/operators/filter/limitField.glsl
+++ src/operators/filter/limitField.glsl
@@ -1,8 +1,16 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	ReturnT val = inputOp1(p, ctx);
 	if (IS_TRUE(THIS_Enable)) {
+		#ifdef THIS_HAS_INPUT_lowField
+		ReturnT low = THIS_asReturnT(fillToVec4(inputOp_lowField(p, ctx)));
+		#else
 		ReturnT low = THIS_asReturnT(THIS_Low);
+		#endif
+		#ifdef THIS_HAS_INPUT_highField
+		ReturnT high = THIS_asReturnT(fillToVec4(inputOp_highField(p, ctx)));
+		#else
 		ReturnT high = THIS_asReturnT(THIS_High);
+		#endif
 		BODY();
 	}
 	return val;
