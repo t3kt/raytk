54d13a3153144197ac88b9c4daf08805da466387
t3kt <tekt@immerse.studio>
2023-03-02 15:37:12 -0800

    added channel selection in dataTextureField

--- src/operators/field/dataTextureField.glsl
+++ src/operators/field/dataTextureField.glsl
@@ -1,5 +1,16 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec2 uv = vUV.st;
 	vec4 val = texture(THIS_texture, uv);
-	return THIS_asReturnT(val);
+	ReturnT res;
+	#ifdef THIS_RETURN_TYPE_vec4
+	res = val;
+	#else
+	switch (int(THIS_Channel)) {
+		case 0: res = val.x; break;
+		case 1: res = val.y; break;
+		case 2: res = val.z; break;
+		case 3: res = val.w; break;
+	}
+	#endif
+	return res;
 }
