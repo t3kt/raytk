c55d76e7ffafff99fc307c93c1a5911a8141436c
t3kt <tekt@immerse.studio>
2023-03-16 17:13:55 -0700

    implemented hashField
    see #1041

---
+++ src/operators/field/hashField_buildConversionTable.py
@@ -0,0 +1,14 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+from raytkTypes import getAllTypes
+
+def onCook(dat: 'scriptDAT'):
+	dat.clear()
+	dat.appendRow(['outputType', 'returnAs'])
+	for dataType in getAllTypes():
+		if dataType.vectorLength is None:
+			continue
+		dat.appendRow([dataType.name, dataType.returnAsType])
