f046e467d6261b3a8488e4ddf7d4eca5b0372f79
t3kt <tekt@immerse.studio>
2023-03-22 14:55:58 -0700

    face UV mode for boxSdf
    see #526

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -481,3 +481,25 @@ vec3 stereographic(vec4 p4) {
   float k = 1.0/(1.0+p4.w);
   return k*p4.xyz;
 }
+
+// https://www.shadertoy.com/view/3djBDh
+//the following functions assume that p is inside the cube of radius 1 centered at the origin
+//closest vertex of the cube to p
+vec3 nearestVertex(vec3 p) {
+	return max(sign(p), vec3(0))*2.-1.;
+}
+//closest face of the cube to p
+vec3 nearestFace(vec3 p) {
+	vec3 ap = abs(p);
+	if (ap.x>=max(ap.z, ap.y)) return vec3(sign(p.x), 0., 0.);
+	if (ap.y>=max(ap.z, ap.x)) return vec3(0., sign(p.y), 0.);
+	if (ap.z>=max(ap.x, ap.y)) return vec3(0., 0., sign(p.z));
+	return vec3(0);
+}
+//closest edge of the cube to p
+vec3 nearestEdge(vec3 p) {
+	vec3 mask = vec3(1)-abs(nearestFace(p));
+	vec3 v = nearestVertex(p);
+	vec3 a = v*mask.zxy, b = v*mask.yzx;
+	return distance(p, a)<distance(p, b)?a:b;
+}
--- src/operators/sdf/boxSdf.glsl
+++ src/operators/sdf/boxSdf.glsl
@@ -7,9 +7,22 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	Sdf res;
 	int infAxis = int(THIS_Infiniteaxis);
 	if (infAxis == 0) {
-		res = createSdf(THIS_BOX_FUNC_3D(p, scale));;
-		if (THIS_Uvmode == THISTYPE_Uvmode_bounds) {
-			assignUV(res, map01(p, -scale/2., scale/2.));
+		res = createSdf(THIS_BOX_FUNC_3D(p, scale));
+		switch (int(THIS_Uvmode)) {
+			case THISTYPE_Uvmode_bounds:
+				assignUV(res, map01(p, -scale/2., scale/2.));
+				break;
+			case THISTYPE_Uvmode_faces:
+				vec3 pNorm = p / scale;
+				vec3 boxFaces = nearestFace(pNorm);
+				if (boxFaces.x != 0.) {
+					assignUV(res, vec3(pNorm.y * boxFaces.x, pNorm.z, 0.));
+				} else if(boxFaces.y != 0.) {
+					assignUV(res, vec3(pNorm.z * boxFaces.y, pNorm.x, 0.));
+				} else if (boxFaces.z != 0.) {
+					assignUV(res, vec3(pNorm.x * boxFaces.z, pNorm.y, 0.));
+				}
+				break;
 		}
 	} else {
 		vec2 q;
