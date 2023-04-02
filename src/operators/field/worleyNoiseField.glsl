d3ab179cf51b91046ddf8cfd670962804f9df704
t3kt <tekt@immerse.studio>
2023-04-01 17:48:36 -0700

    added worleyNoiseField
    close #400
    
    https://github.com/Erkaman/glsl-worley

---
+++ src/operators/field/worleyNoiseField.glsl
@@ -0,0 +1,27 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	#ifdef THIS_HAS_INPUT_jitterField
+	float j = inputOp_jitterField(p, ctx);
+	#else
+	float j = THIS_Jitter;
+	#endif
+	bool md = false;
+	DISTANCE_TYPE_BODY();
+	#ifdef THIS_HAS_INPUT_coordField
+	p = THIS_asCoordT(inputOp_coordField(p, ctx));
+	#endif
+	THIS_NOISE_COORD_TYPE q;
+	#if defined(THIS_COORD_TYPE_vec2)
+		q = THIS_asNoiseCoordT(p);
+	#elif defined(THIS_COORD_TYPE_vec3)
+		#ifdef THIS_NOISE_COORD_vec2
+		q = getAxisPlane(p, int(THIS_Axis));
+		#else
+		q = THIS_asNoiseCoordT(p);
+		#endif
+	#endif
+	q -= THIS_asNoiseCoordT(THIS_Translate);
+	q /= THIS_asNoiseCoordT(THIS_Scale);
+	ReturnT res;
+	BODY();
+	return (res * THIS_Amplitude) + THIS_Offset;
+}
