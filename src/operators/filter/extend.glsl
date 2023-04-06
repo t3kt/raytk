d771201b6fb5b488ba26fe6bfc0fad0a665cfc43
t3kt <tekt@immerse.studio>
2023-04-06 11:08:16 -0700

    axis limits for extend

--- src/operators/filter/extend.glsl
+++ src/operators/filter/extend.glsl
@@ -1,16 +1,20 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	if (IS_TRUE(THIS_Enable)) {
 		#ifdef THIS_HAS_INPUT_sizeField
-		CoordT size = THIS_asCoordT(inputOp_sizeField(p, ctx));
+		vec3 size = adaptAsVec3(inputOp_sizeField(p, ctx));
 		#else
-		CoordT size = THIS_asCoordT(THIS_Size);
+		vec3 size = THIS_Size;
 		#endif
 		#ifdef THIS_HAS_INPUT_centerField
-		CoordT center = THIS_asCoordT(inputOp_centerField(p, ctx));
+		vec3 center = adaptAsVec3(inputOp_centerField(p, ctx));
 		#else
-		CoordT center = THIS_asCoordT(THIS_Center);
+		vec3 center = THIS_Center;
 		#endif
-		p = clamp(p, THIS_asCoordT(center - size * 0.5), THIS_asCoordT(center + size * 0.5));
+		vec3 q = adaptAsVec3(p);
+		q = clamp(q, center - size * 0.5, center + size * 0.5);
+		vec3 mask = vec3(1.);
+		AXES_BODY();
+		p = THIS_asCoordT(mix(adaptAsVec3(p), q, mask));
 	}
 	return inputOp1(p, ctx);
 }
