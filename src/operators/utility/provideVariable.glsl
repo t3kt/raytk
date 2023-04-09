457a63d3e727f516a056c9dd16b02575de98d179
t3kt <tekt@immerse.studio>
2023-04-09 13:42:05 -0700

    provideVariable support for capturing primary input

--- src/operators/utility/provideVariable.glsl
+++ src/operators/utility/provideVariable.glsl
@@ -1,12 +1,27 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
-	#ifdef THIS_EXPOSE_VAR
+	ReturnT res;
+	#ifdef THIS_Valuesource_primary
 	{
-		#ifdef THIS_HAS_INPUT_valueField
-			THIS_VAR = THIS_VAR_asVarT(inputOp_valueField(p, ctx));
-		#else
-			THIS_VAR = THIS_VAR_asVarT(THIS_Value);
+		res = inputOp1(p, ctx);
+		#ifdef THIS_EXPOSE_VAR
+		{
+			THIS_VAR = THIS_VAR_asVarT(res);
+		}
 		#endif
 	}
+	#else
+	{
+		#ifdef THIS_EXPOSE_VAR
+		{
+				#ifdef THIS_HAS_INPUT_valueField
+					THIS_VAR = THIS_VAR_asVarT(inputOp_valueField(p, ctx));
+				#else
+					THIS_VAR = THIS_VAR_asVarT(THIS_Value);
+				#endif
+		}
+		#endif
+		res = inputOp1(p, ctx);
+	}
 	#endif
-	return inputOp1(p, ctx);
+	return res;
 }
