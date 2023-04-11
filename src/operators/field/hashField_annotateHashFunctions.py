463cd21fdb549b08ca69a05f4cd30d5ea3f60116
t3kt <tekt@immerse.studio>
2023-04-11 16:55:14 -0700

    attempting to clarify hashField function labels
    see #1057

---
+++ src/operators/field/hashField_annotateHashFunctions.py
@@ -0,0 +1,23 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+def onCook(dat: 'scriptDAT'):
+	dat.copy(dat.inputs[0])
+	typeLabels = {
+		'uint': 'X[u]',
+		'uvec2': 'XY[u]',
+		'uvec3': 'XYZ[u]',
+		'uvec4': 'XYZW[u]',
+		'float': 'X',
+		'vec2': 'XY',
+		'vec3': 'XYZ',
+		'vec4': 'XYZW',
+	}
+	for i in range(1, dat.numRows):
+		inLabel = dat[i, 'inputType'].val
+		inLabel = typeLabels.get(inLabel) or inLabel
+		outLabel = dat[i, 'outputType'].val
+		outLabel = typeLabels.get(outLabel) or outLabel
+		dat[i, 'label'] += f'  ({inLabel} -> {outLabel})'
