3786e6ded2a1b5d7901e8d3c4fd1c6c2974e523f
t3kt <tekt@immerse.studio>
2023-04-11 16:37:44 -0700

    some work on voronoiField

--- src/operators/field/voronoiField.glsl
+++ src/operators/field/voronoiField.glsl
@@ -4,6 +4,13 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#else
 	vec3 q = adaptAsVec3(p);
 	#endif
+	switch (int(THIS_Axis)) {
+		case 0: q = q.yzx; break;
+		case 1: q = q.xzy; break;
+		case 2: q = q.xyz; break;
+	}
+	q -= THIS_Translate;
+	q /= THIS_Scale;
 	vec2 hashShift = THIS_Hashoffset;
 	vec3 data;
 	BODY();
