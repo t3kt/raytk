be6c8ec8d0302bd17d9e296c54c0064eaceeaf98
t3kt <tekt@immerse.studio>
2023-04-18 17:11:51 -0700

    light direction variable in lights

--- src/operators/light/directionalLight.glsl
+++ src/operators/light/directionalLight.glsl
@@ -4,6 +4,9 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	pRotateOnXYZ(dir, THIS_Rotate);
 	vec3 col = THIS_Color * THIS_Intensity * clamp(dot(ctx.normal, dir), 0., 1.);
 	Light light = createLight(pos, col);
+	#ifdef THIS_EXPOSE_lightdir
+	THIS_lightdir = normalize(light.pos - p);
+	#endif
 	light.supportShadow = IS_TRUE(THIS_Enableshadow);
 	#ifdef THIS_HAS_INPUT_colorField
 	light.color *= fillToVec3(inputOp_colorField(p, ctx));
