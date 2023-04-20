14d6f23fa10ac6b7e3dcb9edd418ca797e65e2d5
t3kt <tekt@immerse.studio>
2023-04-19 23:37:24 -0700

    partial implementation of textSdf2d
    see #1061

---
+++ src/operators/sdf2d/textSdf2d.glsl
@@ -0,0 +1,13 @@
+void THIS_char(inout float d, vec2 p, int c) {
+	vec2 uv = (p + vec2(float(c%16), float(15-c/16)) + .5) / 16.;
+	float dOuter = max(abs(p.x) - .25, max(p.y - .35, -.38 - p.y));
+	float d1 = max(dOuter, textureLod(THIS_data, uv, 0.).w - 127./255.);
+	d = min(d, d1);
+}
+
+ReturnT thismap(CoordT p, ContextT ctx) {
+	float w = 0.45;
+	float d = RAYTK_MAX_DIST;
+	BODY();
+	return createSdf(d);
+}
