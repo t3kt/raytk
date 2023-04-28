93f3b5b0ade496d828020a66e79a43a1dc831e42
t3kt <tekt@immerse.studio>
2023-04-27 17:12:40 -0700

    added mirror in moduloLine
    see #1054

--- src/operators/filter/moduloLine.glsl
+++ src/operators/filter/moduloLine.glsl
@@ -34,6 +34,9 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	float stop = 0.;
 	if (c < start) applyModLimit(q, c, size, start);
 	if (c > stop) applyModLimit(q, c, size, stop);
+	if (THIS_Mirrortype == THISTYPE_Mirrortype_mirror) {
+		q *= mod(c, 2.0)*2 - 1;
+	}
 
 	p3.z = q;
 	p = THIS_asCoordT(p3.zyx);
