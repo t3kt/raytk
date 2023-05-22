3a86dcff48f7b670054a6f60303c829a36a26ef8
t3kt <tekt@immerse.studio>
2023-05-22 16:20:05 -0700

    got basics of triPlanarCombine working
    see #1079

---
+++ src/operators/combine/triPlanarCombine_determineReturnType.py
@@ -0,0 +1,21 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+def onCook(dat):
+	dat.clear()
+	dat.appendRow([_getType(dat)])
+
+def _getType(dat: 'DAT'):
+	typeSetting = parent().par.Returntype.eval()
+	if typeSetting != 'auto':
+		return typeSetting
+	xyType = str(dat.inputs[0][1, 'returnType'] or '')
+	yzType = str(dat.inputs[1][1, 'returnType'] or '')
+	zxType = str(dat.inputs[2][1, 'returnType'] or '')
+	if not any([xyType, yzType, zxType]):
+		return 'vec4'
+	if 'vec4' in xyType or 'vec4' in yzType or 'vec4' in zxType:
+		return 'vec4'
+	return 'float'
