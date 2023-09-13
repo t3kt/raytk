7d3f1c9188730263f318994f1e18b5577f77011f
t3kt <tekt@immerse.studio>
2023-09-13 14:29:40 -0700

    more widget updates

--- devel/toolkitEditor/ropEditor/inputsPanel/inputEditor/typeSpecEditor.py
+++
@@ -1,8 +0,0 @@
-# noinspection PyUnreachableCode
-if False:
-	# noinspection PyUnresolvedReferences
-	from _stubs import *
-
-class TypeSpecEditor:
-	def __init__(self, ownerComp: 'COMP'):
-		self.ownerComp = ownerComp
