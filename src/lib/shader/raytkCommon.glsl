1c40db3c0ed3c243adec390138fa87b0146c77b5
t3kt <tekt@immerse.studio>
2023-12-10 15:06:12 -0800

    move more stuff out of raytkCommon

--- src/lib/shader/raytkCommon.glsl
+++ src/lib/shader/raytkCommon.glsl
@@ -331,9 +331,6 @@ vec3 fillToVec3(vec4 p) { return p.xyz; }
 vec4 fillToVec4(float p) { return vec4(p); }
 vec4 fillToVec4(vec4 p) { return p; }
 
-void setFromFloat(inout float x, float val) { x = val; }
-void setFromFloat(inout Sdf x, float val) { x.x = val; }
-
 void swap(inout Sdf a, inout Sdf b) {
 	Sdf tmp = a;
 	a = b;
@@ -456,35 +453,6 @@ vec4 inverseStereographic(vec3 p) {
 vec3 stereographic(vec4 p4) {
   float k = 1.0/(1.0+p4.w);
   return k*p4.xyz;
-}
-
-// https://www.shadertoy.com/view/3djBDh
-//the following functions assume that p is inside the cube of radius 1 centered at the origin
-//closest vertex of the cube to p
-vec3 nearestVertex(vec3 p) {
-	return max(sign(p), vec3(0))*2.-1.;
-}
-//closest face of the cube to p
-vec3 nearestFace(vec3 p) {
-	vec3 ap = abs(p);
-	if (ap.x>=max(ap.z, ap.y)) return vec3(sign(p.x), 0., 0.);
-	if (ap.y>=max(ap.z, ap.x)) return vec3(0., sign(p.y), 0.);
-	if (ap.z>=max(ap.x, ap.y)) return vec3(0., 0., sign(p.z));
-	return vec3(0);
-}
-//closest edge of the cube to p
-vec3 nearestEdge(vec3 p) {
-	vec3 mask = vec3(1)-abs(nearestFace(p));
-	vec3 v = nearestVertex(p);
-	vec3 a = v*mask.zxy, b = v*mask.yzx;
-	return distance(p, a)<distance(p, b)?a:b;
-}
-// https://www.shadertoy.com/view/3lcBD2
-// closest edge of 2D square to p
-vec2 nearestEdge(vec2 p) {
-	vec2 p2 = abs(p);
-	if (p2.x > p2.y) return vec2((p.x < 0.) ? -1. : 1., 0.);
-	else return vec2(0., (p.y < 0.) ? -1. : 1.);
 }
 
 // https://lygia.xyz/math/pow5
