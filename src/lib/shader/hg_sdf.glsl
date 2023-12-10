0d192b10184f80366136ab7c962c8b8b5b06ba7f
t3kt <tekt@immerse.studio>
2023-12-10 15:31:23 -0800

    move stuff out of hg_sdf

--- src/lib/shader/hg_sdf.glsl
+++ src/lib/shader/hg_sdf.glsl
@@ -449,23 +449,3 @@ float fOpUnionSoft(float a, float b, float r) {
 }
 #endif
 
-// produces a cylindical pipe that runs along the intersection.
-// No objects remain, only the pipe. This is not a boolean operator.
-float fOpPipe(float a, float b, float r) {
-	return length(vec2(a, b)) - r;
-}
-
-// first object gets a v-shaped engraving where it intersect the second
-float fOpEngrave(float a, float b, float r) {
-	return max(a, (a + r - abs(b))*sqrt(0.5));
-}
-
-// first object gets a capenter-style groove cut out
-float fOpGroove(float a, float b, float ra, float rb) {
-	return max(a, min(a + ra, rb - abs(b)));
-}
-
-// first object gets a capenter-style tongue attached
-float fOpTongue(float a, float b, float ra, float rb) {
-	return min(a, max(a - ra, abs(b) - rb));
-}
