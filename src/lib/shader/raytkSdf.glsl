49ebe51ed844b392f530c2a4fc30fb6c91584ca0
t3kt <tekt@immerse.studio>
2023-12-10 18:10:08 -0800

    move rounded rect out of shared libs

--- src/lib/shader/raytkSdf.glsl
+++ src/lib/shader/raytkSdf.glsl
@@ -12,14 +12,6 @@ float sdTetrahedron(vec3 p) {
 		-1.)/sqrt(3.0);
 }
 
-float sdRoundedBox( in vec2 p, in vec2 b, in vec4 r )
-{
-	r.xy = (p.x>0.0)?r.xy : r.zw;
-	r.x  = (p.y>0.0)?r.x  : r.y;
-	vec2 q = abs(p)-b+r.x;
-	return min(max(q.x,q.y),0.0) + length(max(q,0.0)) - r.x;
-}
-
 float fCapsule(vec2 p, vec2 a, vec2 b) {
 	vec2 pa = p - a;
 	vec2 ba = b - a;
---
+++ src/operators/sdf2d/roundedRectangleSdf2d_lib.glsl
@@ -0,0 +1,7 @@
+float sdRoundedBox( in vec2 p, in vec2 b, in vec4 r )
+{
+	r.xy = (p.x>0.0)?r.xy : r.zw;
+	r.x  = (p.y>0.0)?r.x  : r.y;
+	vec2 q = abs(p)-b+r.x;
+	return min(max(q.x,q.y),0.0) + length(max(q,0.0)) - r.x;
+}
