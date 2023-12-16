660d6d6370195e80cbdd9543339a85e3b1cc15ac
t3kt <tekt@immerse.studio>
2023-12-16 11:02:48 -0800

    field inputs for sphereGridSdf

--- src/operators/sdf/sphereGridSdf.glsl
+++ src/operators/sdf/sphereGridSdf.glsl
@@ -1,11 +1,22 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	float rows = float(THIS_Rows);
 	float cols = float(THIS_Cols);
-	float th = THIS_Thickness;
-	float radius = THIS_Radius;
 
-	float d = longitudes(p, cols, radius);
-	d = min(d, latitudes(p, rows, radius));
+	#ifdef THIS_HAS_INPUT_radiusField
+	float radius = inputOp_radiusField(p, ctx);
+	#else
+	float radius = THIS_Radius;
+	#endif
+
+	float d;
+	BODY();
+
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float th = inputOp_thicknessField(p, ctx);
+	#else
+	float th = THIS_Thickness;
+	#endif
+
 	d -= th;
 	return createSdf(d);
 }
