411c42ad92c6f911d0a246e36f05a23131f711ff
t3kt <tekt@immerse.studio>
2023-12-22 16:38:36 -0800

    geo material stuff
    see #1010

--- src/operators/geo/geoMaterial_vertexBody.glsl
+++ src/operators/geo/geoMaterial_vertexBody.glsl
@@ -1,8 +1,16 @@
-out vec4 fragColor;
-void main()
+// Example Vertex Shader
+
+void main() 
 {
-	TDCheckDiscard();
-	vec4 color = vec4(1.0);
-	TDAlphaTest(color.a);
-	fragColor = TDOutputSwizzle(color);
+    vec3 pos = TDPos();
+    vec4 worldSpacePos = TDDeform(pos);
+	gl_Position = TDWorldToProj(worldSpacePos);
+#ifndef TD_PICKING_ACTIVE
+#else // TD_PICKING_ACTIVE
+	// This will automatically write out the nessessary values
+	// for this shader to work with picking.
+	// See the documentation if you want to write custom values for picking.
+	TDWritePickingValues();
+#endif // TD_PICKING_ACTIVE
 }
+
