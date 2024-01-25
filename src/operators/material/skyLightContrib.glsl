f2023ff6236fa5e8b6a956d2c9d356d9d5246789
t3kt <tekt@immerse.studio>
2024-01-25 14:07:39 -0800

    Added rotation in skyLightContrib
    Close #1163

--- src/operators/material/skyLightContrib.glsl
+++ src/operators/material/skyLightContrib.glsl
@@ -1,6 +1,8 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
 	vec3 norm = normalize(ctx.normal);
-	float amount = clamp(0.5+0.5*dot(normalize(ctx.normal), THIS_Dir), 0, 1) * THIS_Level;
+	vec3 dir = normalize(THIS_Dir);
+	pRotateOnXYZ(dir, THIS_Rotate);
+	float amount = clamp(0.5+0.5*dot(normalize(ctx.normal), dir), 0, 1) * THIS_Level;
 	#if defined(THIS_RETURN_TYPE_float)
 	return amount;
 	#elif defined(THIS_RETURN_TYPE_vec4)
