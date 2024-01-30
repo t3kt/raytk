e9ac3c785da113616918b2166f98a1956fd01888
t3kt <tekt@immerse.studio>
2024-01-29 17:07:16 -0800

    rename to renderEngineHost
    see #1158

---
+++ src/operators/output/renderEngineHost_engineCallbacks.py
@@ -0,0 +1,25 @@
+# me - this DAT.
+# engineComp - the connected Engine COMP
+
+# onInitialize(): if return value > 0, it will be
+# called again after the returned number of frames.
+# callCount increments with each attempt, starting at 1
+
+def onInitialize(engineComp, callCount):
+	return 0
+
+def onReady(engineComp):
+	return
+
+def onStart(engineComp):
+	return
+
+def whileRunning(engineComp):
+	return
+
+def onDone(engineComp):
+	return
+
+def onError(engineComp):
+	return
+	
