7b6d862fe1d04bb8a074599f1e0cc3f676248eb8
t3kt <tekt@immerse.studio>
2024-02-25 16:48:52 -0800

    more opElements RopState stuff

--- devel/toolkitEditor/ropEditor/elementsPanel/elementsPanel.py
+++ devel/toolkitEditor/ropEditor/elementsPanel/elementsPanel.py
@@ -1,8 +1,17 @@
+from raytkState import RopState
+
 # noinspection PyUnreachableCode
 if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 	from _typeAliases import *
+	from _typeAliases import *
+
+	class _InspectorCorePars(ParCollection):
+		Hastarget: 'BoolParamT'
+		Definitiontable: 'DatParamT'
+		Targetcomp: 'CompParamT'
+	ipar.inspectorCore = _InspectorCorePars()
 
 	class _StatePar(ParCollection):
 		XYZ: 'BoolParamT'
@@ -13,11 +22,20 @@ class ElementsPanel:
 		self.ownerComp = ownerComp
 
 	@staticmethod
-	def buildItemTable(dat: DAT, elementTable: DAT):
+	def buildItemTable(dat: DAT):
 		dat.clear()
 		dat.appendRow(['name', 'label', 'hostOp', 'parName', 'datName', 'evalDatName', 'fileSuffix'])
-		for row in range(1, elementTable.numRows):
-			elementRoot = op(elementTable[row, 'elementRoot'])
+		rop = ipar.inspectorCore.Targetcomp.eval()
+		if not rop:
+			return
+		stateDat = rop.op('opDefinition/opState')
+		if not stateDat:
+			return
+		state = RopState.fromJson(stateDat.text)
+		if not state or not state.opElements:
+			return
+		for element in state.opElements:
+			elementRoot = op(element.elementRoot)
 			if not elementRoot:
 				continue
 			for par in elementRoot.customPars:
