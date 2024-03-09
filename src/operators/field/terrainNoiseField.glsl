d853cc75a2b4b1d7b0d66c05457fcdaa14797c33
t3kt <tekt@immerse.studio>
2024-03-08 23:05:32 -0800

    field inputs for terrainNoiseField
    still needs cleanup for param enable  states and inclusion expressions.
    
    see #1183, #1182

--- src/operators/field/terrainNoiseField.glsl
+++ src/operators/field/terrainNoiseField.glsl
@@ -12,12 +12,41 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	q -= THIS_Translate;
 	q /= THIS_Scale;
 
+	#ifdef THIS_HAS_INPUT_incrementField
+	float h = inputOp_incrementField(p0, ctx);
+	#else
 	float h = THIS_Increment;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_lacunarityField
+	float lac = inputOp_lacunarityField(p0, ctx);
+	#else
 	float lac = THIS_Lacunarity;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_frequencyField
+	float freq = inputOp_frequencyField(p0, ctx);
+	#else
 	float freq = THIS_Frequency;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_octavesField
+	float oct = inputOp_octavesField(p0, ctx);
+	#else
 	float oct = THIS_Octaves;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_stepOffsetField
+	float stepOff = inputOp_stepOffsetField(p0, ctx);
+	#else
 	float stepOff = THIS_Stepoffset;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_gainField
+	float gain = inputOp_gainField(p0, ctx);
+	#else
 	float gain = THIS_Gain;
+	#endif
 
 	ReturnT res;
 
