ce16728c3651e7c58451e211103e4afa1c3bba3c
t3kt <tekt@immerse.studio>
2024-03-10 14:55:27 -0700

    minor code reduction
    see #730

--- src/components/typeSpec/typeSpec.py
+++ src/components/typeSpec/typeSpec.py
@@ -3,16 +3,9 @@ if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
-def _typeTable() -> DAT:
-	return op('typeTable')
-
 def _getAllTypesInCategory(typeTable: DAT, category: str):
 	filterColumn = 'is' + category[0].upper() + category[1:]
-	return [
-		typeTable[row, 'name'].val
-		for row in range(1, typeTable.numRows)
-		if typeTable[row, filterColumn] == '1'
-	]
+	return [typeTable[row, 'name'].val for row in range(1, typeTable.numRows) if typeTable[row, filterColumn] == '1']
 
 class _Category:
 	def __init__(self, name: str):
@@ -20,25 +13,17 @@ class _Category:
 		self.togglePrefix = name.capitalize()
 		self.allToggle = 'All' + name.lower()
 		self.useInputToggle = 'Useinput' + name.lower()
-		self.allTypes = _getAllTypesInCategory(_typeTable(), name)
+		self.allTypes = _getAllTypesInCategory(op('typeTable'), name)
 
 	def getSpec(self, parHost: OP):
 		isAll = bool(parHost and parHost.par[self.allToggle])
 		return TypeSpec(
 			useInput=bool(parHost and parHost.par[self.useInputToggle]),
 			supportsAll=isAll,
-			supported=[] if isAll else [
-				t
-				for t in self.allTypes
-				if parHost and parHost.par[self.togglePrefix + t.lower()]
-			],
+			supported=[] if isAll else [t for t in self.allTypes if parHost and parHost.par[self.togglePrefix + t.lower()]],
 		)
 
-_categories = [
-	_Category('coordType'),
-	_Category('contextType'),
-	_Category('returnType'),
-]
+_categories = [_Category('coordType'), _Category('contextType'), _Category('returnType')]
 
 def buildSupportedTypeTable(dat: scriptDAT, parHost: OP):
 	dat.clear()
@@ -52,18 +37,6 @@ def buildSupportedTypeTable(dat: scriptDAT, parHost: OP):
 		])
 
 class TypeSpec:
-	"""
-	A specification for a category of types (such as coordinate types) that an operator/input/etc supports using.
-	Each ROP has 3 of these:
-	* Coordinate type spec
-	* Context type spec
-	* Return type spec
-
-	A type spec is either:
-	* A single type (e.g. 'vec2')
-	* A set of several types (e.g. 'vec2|vec3')
-	* All available types ('*')
-	"""
 	useInput: bool = False
 	supportsAll: bool = False
 	supported: list[str] | None = None
@@ -76,25 +49,16 @@ class TypeSpec:
 	def expandedTypes(self, allTypes: list[str]) -> list[str]:
 		if self.supportsAll:
 			return list(allTypes)
-		return [
-			t
-			for t in allTypes
-			if self.supported and t in self.supported
-		]
+		return [t for t in allTypes if self.supported and t in self.supported]
 
 	def formatSpec(self, allTypes: list[str]) -> str:
-		types = self.expandedTypes(allTypes)
-		if self.supportsAll:
-			spec = '*'
-		else:
-			spec = ' '.join(types)
+		spec = '*' if self.supportsAll else ' '.join(self.expandedTypes(allTypes))
 		if self.useInput:
 			return f'useinput|{spec}'
 		return spec
 
 	def formatTypes(self, allTypes: list[str]) -> str:
-		types = self.expandedTypes(allTypes)
-		return ' '.join(types)
+		return ' '.join(self.expandedTypes(allTypes))
 
 	@classmethod
 	def parseSpec(cls, spec: str):
@@ -110,5 +74,5 @@ class TypeSpec:
 def _parseUseInput(spec: str) -> tuple[bool, str]:
 	useInput = spec.startswith('useinput|')
 	if useInput:
-		spec = spec[9:]  # len('useinput|')
+		spec = spec[9:]
 	return useInput, spec
