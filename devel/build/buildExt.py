0c2d38bf5e85e5a68cefcf5480573455650086dc
t3kt <tekt@immerse.studio>
2024-03-18 22:55:20 -0700

    remove unnecessary build cleanup
    these components appear to have finally fully gone away

--- devel/build/buildExt.py
+++ devel/build/buildExt.py
@@ -475,10 +475,6 @@ class ToolkitBuilder(_BuilderBase):
 		self.context.resetCustomPars(comp)
 		self.context.lockROPPars(comp)
 
-		# This really shouldn't be necessary but there's something strange with old cloned components...
-		self.context.safeDestroyOp(comp.op('opDefinition/paramHelpEditor'))
-		self.context.safeDestroyOp(comp.op('shaderBuilder/supportDetector'))
-
 		# self.context.moveNetworkPane(comp)
 		self.processOperatorSubCompChildrenOf(comp)
 		self.context.consolidateOperatorPythonModules(comp)
