68573c172051392829ffde5a55d096323e23b918
t3kt <tekt@immerse.studio>
2024-03-20 17:11:20 -0700

    axis setting for sphereGridSdf
    close #1178

--- src/operators/sdf/sphereGridSdf.glsl
+++ src/operators/sdf/sphereGridSdf.glsl
@@ -1,9 +1,16 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	CoordT p0 = p;
 	float rows = float(THIS_Rows);
 	float cols = float(THIS_Cols);
 
+	switch (int(THIS_Axis)) {
+		case 0: p = p.yzx; break;
+		case 1: p = p.zxy; break;
+		case 2: p = p.xyz; break;
+	}
+
 	#ifdef THIS_HAS_INPUT_radiusField
-	float radius = inputOp_radiusField(p, ctx);
+	float radius = inputOp_radiusField(p0, ctx);
 	#else
 	float radius = THIS_Radius;
 	#endif
@@ -12,7 +19,7 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	BODY();
 
 	#ifdef THIS_HAS_INPUT_thicknessField
-	float th = inputOp_thicknessField(p, ctx);
+	float th = inputOp_thicknessField(p0, ctx);
 	#else
 	float th = THIS_Thickness;
 	#endif
