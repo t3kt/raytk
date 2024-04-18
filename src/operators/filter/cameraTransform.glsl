6e0dc9b2cd53010fa0a5603877f27991b97a0d86
t3kt <tekt@immerse.studio>
2024-04-18 12:48:48 -0700

    fix lookat bug in cameraTransform
    see #841

--- src/operators/filter/cameraTransform.glsl
+++ src/operators/filter/cameraTransform.glsl
@@ -2,21 +2,21 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	// initial ray used for position for fields
 	ReturnT ray = inputOp_camera(p, ctx);
 	if (IS_TRUE(THIS_Enable)) {
+		vec3 initialPos = ray.pos;
 		vec3 posOffset = THIS_Translate;
 		#ifdef THIS_HAS_INPUT_translateField
-		posOffset += inputOp_translateField(ray.pos + posOffset, ctx).xyz;
+		posOffset += inputOp_translateField(initialPos + posOffset, ctx).xyz;
 		#endif
 		ctx.posOffset = posOffset;
-		ray.pos += posOffset;
 		vec3 lookAtOffset = THIS_Lookattranslate;
 		#ifdef THIS_HAS_INPUT_lookAtTranslateField
-		lookAtOffset += adaptAsVec3(inputOp_lookAtTranslateField(ray.pos, ctx));
+		lookAtOffset += adaptAsVec3(inputOp_lookAtTranslateField(initialPos + posOffset, ctx));
 		#endif
-		ctx.posOffset += posOffset;
+		ctx.lookAtOffset += lookAtOffset;
 		LOOK_AT_BODY();
 		vec3 dirRot = THIS_Dirrotate;
 		#ifdef THIS_HAS_INPUT_dirRotateField
-		dirRot += radians(inputOp_dirRotateField(ray.pos, ctx).xyz);
+		dirRot += radians(inputOp_dirRotateField(initialPos + posOffset, ctx).xyz);
 		#endif
 		ctx.rotation = dirRot;
 		// recalculate the ray with lookat and rotation
