132dab7d8c6781c62f72055a282d206037812b16
t3kt <tekt@immerse.studio>
2024-05-28 15:45:39 -0700

    some incomplete work on customRender
    see #1220

--- src/operators/output/customRender_defaultBodyTemplate.glsl
+++ src/operators/output/customRender_defaultBodyTemplate.glsl
@@ -14,7 +14,7 @@ void main() {
 	#if defined(THIS_RETURN_TYPE_float)
 	vec4 color = vec4(thismap(p, createDefaultContext()));
 	#elif defined(THIS_RETURN_TYPE_vec4)
-	vec4 color = thismap(p, createDefaultContext);
+	vec4 color = thismap(p, createDefaultContext());
 	#else
 	vec4 color = vec4(0.);
 	#endif
