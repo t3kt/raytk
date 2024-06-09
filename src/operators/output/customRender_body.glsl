a74205465c066a214644aafc2121538d9803af0e
t3kt <tekt@immerse.studio>
2024-06-08 18:13:23 -0700

    use struct for inputs to custom main functions
    see #1220

--- src/operators/output/customRender_body.glsl
+++ src/operators/output/customRender_body.glsl
@@ -4,6 +4,21 @@ void main() {
 	#endif
 	initOutputs();
 
+	CustomRenderInputs inputs;
+	inputs.uvPos = vec3(vUV.st, 0.);
+	inputs.resolution = vec3(uTDOutputInfo.res.zw, 1.);
+	#if defined(RAYTK_OUTPUT_TEXTURE_2D)
+	#elif defined(RAYTK_OUTPUT_TEXTURE_3D)
+	inputs.uvPos.z = uTDOutputInfo.depth.z; // depth offset
+	inputs.resolution.z = int(uTDOutputInfo.depth.y); // depth
+	#elif defined(RAYTK_OUTPUT_TEXTURE_2D_ARRAY)
+	inputs.resolution.z = int(uTDOutputInfo.depth.y);
+	if (inputs.resolution.z > 1) {
+		inputs.uvPos.z = uTDOutputInfo.depth.z / (inputs.resolution.z - 1.0);
+	}
+	#endif
+	inputs.pixelPos = ivec3(inputs.uvPos * inputs.resolution);
+
 	vec2 resolution = uTDOutputInfo.res.zw;
 	vec2 fragCoord = vUV.st;//*resolution;
 	fragCoord.x *= uTDOutputInfo.res.z/uTDOutputInfo.res.w;
@@ -13,5 +28,5 @@ void main() {
 
 	Context ctx = createDefaultContext();
 
-	customMain(fragCoord, p, ctx);
+	customMain(inputs, ctx);
 }
--- src/operators/output/customRender_defaultMainCode.glsl
+++ src/operators/output/customRender_defaultMainCode.glsl
@@ -1,4 +1,5 @@
-void customMain(vec2 fragCoord, vec2 p, Context ctx) {
+void customMain(CustomRenderInputs inputs, Context ctx) {
+	vec2 p = inputs.uvPos.xy * vec2(2.) - vec2(1.);
 	vec4 res1 = fillToVec4(inputOp1(p, ctx));
 	#ifdef OUTPUT_COLOR
 	colorOut = TDOutputSwizzle(res1);
