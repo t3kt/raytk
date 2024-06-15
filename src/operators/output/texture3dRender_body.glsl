07d2c16a8d1ee1ef9ab10ebfa64b45a6d919bb81
t3kt <tekt@immerse.studio>
2024-06-15 13:59:13 -0700

    got texture3dRender mostly working
    no material support yet for SDFs.
    
    see #1237

--- src/operators/output/texture3dRender_body.glsl
+++ src/operators/output/texture3dRender_body.glsl
@@ -84,93 +84,52 @@ vec3 calcNormal(THIS_CoordT pos)
 
 #endif
 
+//#define RES ivec3(uTDOutputInfo.res.zw,uTDOutputInfo.depth.y)
+
+layout (local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 void main() {
 	#ifdef RAYTK_HAS_INIT
 	init();
 	#endif
-	initOutputs();
-	vec2 resolution = uTDOutputInfo.res.zw;
-	vec4 posAndExists;
+//	initOutputs();
+	ivec3 resolution = ivec3(uTDOutputInfo.res.zw,uTDOutputInfo.depth.y);
+	ivec3 pixel = ivec3(gl_GlobalInvocationID.xyz);
 
-	#if defined(THIS_TEX_TYPE_texture2d)
-	vec2 fragCoord = vUV.st;
-	exposeDataPosition(resolution, fragCoord.xy);
-	posAndExists = texture(sTD2DInputs[0], fragCoord);
-	#elif defined(THIS_TEX_TYPE_texture3d)
-	// The center of the first slice is not located at 0, but rather halfway between 0 (the start of the first slice)
-	// and 1.0 / depth (the end of the first slice)
-	float firstSlice = uTD3DInfos[0].depth.x * 0.5;
+	vec3 pixelUV = vec3(pixel) / vec3(resolution);
 
-	// now add the offset
-	firstSlice += uTD3DInfos[0].depth.z;
+	vec3 center = THIS_Center;
+	vec3 scale = THIS_Scale;
 
-	vec3 pos = vec3(vUV.st, firstSlice);
-	exposeDataPosition(resolution, pos.xy);
-	posAndExists = texture(sTD3DInputs[0], pos);
-	#else
-	#error invalidOutputTextureType
-	#endif
-
-	if (posAndExists.a == 0) {
-		return;
-	}
-	#if defined(THIS_COORD_TYPE_vec3)
-	vec3 p = posAndExists.xyz;
-	#elif defined(THIS_COORD_TYPE_vec2)
-	vec2 p = posAndExists.xy;
-	#else
-	#error invalidCoordType
-	#endif
+	vec3 p = mapRange(pixelUV, vec3(0.), vec3(1.), center - scale, center + scale);
 
 	#if defined(THIS_RETURN_TYPE_Sdf)
 
 		Sdf res = map(p);
 
-		MaterialContext matCtx = createMaterialContext();
-		#ifdef RAYTK_GLOBAL_POS_IN_CONTEXT
-			matCtx.globalPos = adaptAsVec3(p);
-		#endif
-		matCtx.ray = getViewRay(vec2(0.));
-		#if defined(OUTPUT_COLOR) || defined(OUTPUT_NORMAL) || defined(THIS_HAS_TAG_uselight)
-			matCtx.normal = calcNormal(p);
-		#endif
-		#ifdef THIS_HAS_TAG_uselight
-			LightContext lightCtx = createLightContext(res, matCtx.normal);
-			#ifdef RAYTK_GLOBAL_POS_IN_CONTEXT
-			lightCtx.globalPos = adaptAsVec3(p);
-			#endif
-			matCtx.light = getLight(p, lightCtx);
-		#endif
+
 
 		#ifdef OUTPUT_COLOR
-			matCtx.result = res;
-			float level = getLevel(adaptAsVec3(p), matCtx);
-			if (level <= 0.) {
-				colorOut = vec4(0.);
-			} else {
-				vec3 col = getColor(THIS_asCoordT(p), matCtx);
-				colorOut = vec4(col * level, level);
-			}
+		vec4 color = vec4(0);
+		if (!isNonHitSdf(res) && res.x <= 0.) {
+			#ifdef RAYTK_USE_SURFACE_COLOR
+			color = res.color;
+			#endif
+		}
+			imageStore(colorOut, pixel, color);
 		#endif
 
 		#ifdef OUTPUT_SDF
 			if (isNonHitSdf(res)) {
-				sdfOut = vec4(0.);
+				imageStore(sdfOut, pixel, vec4(0.));
 			} else {
-				sdfOut = vec4(vec3(res.x), 1.0);
+				imageStore(sdfOut, pixel, vec4(vec3(res.x), 1.0));
 			}
-		#endif
-		#if defined(OUTPUT_NORMAL)
-			normalOut = vec4(calcNormal(p), 0.);
-		#endif
-		#if defined(OUTPUT_OBJECTID) && defined(RAYTK_OBJECT_ID_IN_SDF)
-			objectIdOut += res.objectId;
 		#endif
 
 	#elif defined(THIS_RETURN_TYPE_vec4) || defined(THIS_RETURN_TYPE_float)
 
 		#ifdef OUTPUT_VALUE
-		valueOut = vec4(thismap(p, createDefaultContext()));
+		imageStore(valueOut, pixel, vec4(thismap(p, createDefaultContext())));
 		#endif
 
 	#else
--- src/operators/output/texture3dRender_func.glsl
+++ src/operators/output/texture3dRender_func.glsl
@@ -1,3 +1,7 @@
+#ifdef THIS_RETURN_TYPE_Sdf
+
+#endif
+
 ReturnT thismap(CoordT p, ContextT ctx) {
-	return inputOp1(p, ctx);
+	return inputOp_primary(p, ctx);
 }
