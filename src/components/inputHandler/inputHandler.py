b29485201538dbfc6b49baec736150500b305c73
t3kt <tekt@immerse.studio>
2024-07-19 16:47:17 -0700

    don't lock input configs in build
    close #1198

--- src/components/inputHandler/inputHandler.py
+++ src/components/inputHandler/inputHandler.py
@@ -51,6 +51,69 @@ class InputHandler:
 		dat.appendCol(['input:varInputs', config['varInputs', 1]])
 		dat.appendCol(['input:handler', self.handlerComp])
 
+	def BuildConfigTable(self, dat: DAT):
+		dat.clear()
+		host = self.handlerComp.par.Hostop.eval()
+		ownIn = self._getAttachedInDAT()
+		baseName, localName = self._parseHandlerName()
+		sourcePar = self.handlerComp.par.Source.bindMaster
+		index = _determineAutoIndex(host=host, ownIn=ownIn, baseName=baseName)
+		defaultName = f'inputOp{index}'
+		if sourcePar is not None:
+			name = sourcePar.name
+		elif localName:
+			name = localName
+		elif ownIn:
+			name = ownIn.name
+		else:
+			name = defaultName
+		if self.handlerComp.par.Label:
+			label = self.handlerComp.par.Label
+		elif ownIn and ownIn.par.label:
+			label = ownIn.par.label
+		elif sourcePar is not None:
+			label = sourcePar.label
+		else:
+			label = name.replace('_', ' ')
+		if self.handlerComp.par.Localalias:
+			alias = self.handlerComp.par.Localalias
+		else:
+			alias = defaultName
+		dat.appendRows([
+			['name', name],
+			['label', label],
+			['alias', alias],
+			['vars', self.handlerComp.par.Variables],
+			['varInputs', self.handlerComp.par.Variableinputs],
+		])
+
+	def _getAttachedInDAT(self) -> inDAT | None:
+		handler = self.handlerComp
+		host = handler.par.Hostop.eval()
+		if not host:
+			return
+		if len(handler.inputConnectors) < 1:
+			return
+		conn = handler.inputConnectors[0]
+		if len(conn.connections) < 1:
+			return
+		o = conn.connections[0].owner
+		if isinstance(o, inDAT):
+			return o
+		if isinstance(o, nullDAT) and o.inputs and isinstance(o.inputs[0], inDAT):
+			return o.inputs[0]
+
+	# returns e.g. ("inputHandler5", "stuffField")
+	def _parseHandlerName(self):
+		name = self.handlerComp.name
+		if name == 'inputHandler':
+			return 'None', None
+		if not name.startswith('inputHandler'):
+			return None, name
+		if '_' in name:
+			return name.split('_', maxsplit=1)
+		return name, None
+
 def _isValidDefinitionDat(o: OP | DAT | None):
 	if not o or not o.isDAT:
 		return False
@@ -58,3 +121,23 @@ def _isValidDefinitionDat(o: OP | DAT | None):
 
 def _restrictExpandedTypes(expandedTypes: str, supportedTypes: list[str]) -> str:
 	return ' '.join([t for t in expandedTypes.split(' ') if t in supportedTypes])
+
+def _determineAutoIndex(
+		host: COMP | None,
+		ownIn: inDAT | None,
+		baseName: str | None):
+	if not host:
+		return 0
+	# Look for digits in the first part of the name, with support for:
+	# inputHandler3_foo5 -> 3
+	# inputHandler5 -> 5
+	if baseName:
+		i = tdu.digits(baseName)
+		if i is not None:
+			return i
+	# Check where the associated inDAT sits in the hosts inputs list
+	if ownIn:
+		for conn in host.inputConnectors:
+			if conn.inOP and conn.inOP.isDAT and ownIn is conn.inOP:
+				return 1 + conn.index
+	return 0
