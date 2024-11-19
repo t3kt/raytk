c4ad739450e5433c089bd99769898bf5ebfeade9
tekt <tekt@immerse.studio>
2024-11-18 16:24:52 -0800

    implement the rest of gridSdf2d

--- src/operators/sdf2d/gridSdf2d.glsl
+++ src/operators/sdf2d/gridSdf2d.glsl
@@ -1,14 +1,31 @@
 ReturnT thismap(CoordT p, ContextT ctx) {
+	CoordT p0 = p;
+	#ifdef THIS_HAS_INPUT_sizeField
+	float sz = inputOp_sizeField(p0, ctx);
+	#else
+	float sz = THIS_Size;
+	#endif
 	vec4 result;
 	vec2 id;
-	BODY();
+	p /= sz;
+	SHAPE_BODY();
 	// TODO: expose ID
-	// TODO: UV
 	float distBorder = result.x;
 	float distCenter = result.y;
 	vec2 uv = result.zw;
-	float th = THIS_Thickness;
-	Sdf res = createSdf(-distBorder + th);
+	#ifdef THIS_HAS_INPUT_spacingField
+	float sp = inputOp_spacingField(p0, ctx);
+	#else
+	float sp = THIS_Spacing;
+	#endif
+	#ifdef THIS_HAS_INPUT_radiusField
+	float r = inputOp_radiusField(p0, ctx);
+	#else
+	float r = THIS_Radius;
+	#endif
+	float d;
+	FORMAT_BODY();
+	Sdf res = createSdf(d * sz);
 	#ifdef RAYTK_USE_UV
 	assignUV(res, vec3(uv, 1.));
 	#endif
