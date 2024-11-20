845d997319dd84431a3d742bbdd8c6952a38cee2
tekt <tekt@immerse.studio>
2024-11-20 14:21:30 -0800

    added uneven mode to eggSdf2d

--- src/operators/sdf2d/eggSdf2d.glsl
+++ src/operators/sdf2d/eggSdf2d.glsl
@@ -1,6 +1,5 @@
 // https://www.shadertoy.com/view/Wdjfz3
 ReturnT thismap(CoordT p, ContextT ctx) {
-	const float k = sqrt(3.0);
 	#ifdef THIS_HAS_INPUT_radiusField
 	float ra = inputOp_radiusField(p, ctx);
 	#else
@@ -11,10 +10,22 @@ ReturnT thismap(CoordT p, ContextT ctx) {
 	#else
 	float rb = THIS_Rounding*ra;
 	#endif
-	p.x = abs(p.x);
-	float r = ra - rb;
-	float d = ((p.y<0.0) ? length(vec2(p.x,p.y))-r :
-		(k*(p.x+r)<p.y) ? length(vec2(p.x,p.y-k*r)) :
-		length(vec2(p.x+r,p.y)) - 2.0*r)-rb;
+	#ifdef THIS_HAS_INPUT_point1Field
+	vec2 a = adaptAsVec2(inputOp_point1Field(p, ctx));
+	#else
+	vec2 a = THIS_Point1;
+	#endif
+	#ifdef THIS_HAS_INPUT_point2Field
+	vec2 b = adaptAsVec2(inputOp_point2Field(p, ctx));
+	#else
+	vec2 b = THIS_Point2;
+	#endif
+	#ifdef THIS_HAS_INPUT_bulgeField
+	float tc = inputOp_bulgeField(p, ctx);
+	#else
+	float tc = THIS_Bulge;
+	#endif
+	float d;
+	BODY();
 	return createSdf(d);
 }
