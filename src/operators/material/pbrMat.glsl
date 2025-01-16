a15c0317f1a97688fbae20d25773c3c2ca3e5611
tekt <tekt@immerse.studio>
2025-01-16 11:58:07 -0800

    multiLight support for pbrMat
    close #1277 , see #636

--- src/operators/material/pbrMat.glsl
+++ src/operators/material/pbrMat.glsl
@@ -53,23 +53,105 @@ vec3 THIS_reflectanceMap(vec3 p, MaterialContext matCtx, vec3 refl, float roughn
 	return mix(reflectMap, blurMap, roughness);
 }
 
-//vec3 THIS_getColorForLight(
-//	CoordT p, MaterialContext matCtx,
-//	Light light, int lightIndex, float shadedLevel,
-//	float roughness, float metallic,
-//	vec3 baseColor
-//) {
-//	vec3 lightDir = normalize(p - light.pos);
-//	vec3 viewDir = normalize(-matCtx.ray.dir);
-//
-//	float roughness = THIS_Roughness;
-//	float metallic = THIS_Metallic;
-//	matCtx.light = light;
-//	matCtx.lightIndex = lightIndex;
-//	matCtx.shadedLevel = shadedLevel;
-//
-//	return vec3(0);
-//}
+vec3 THIS_getColorForLight(
+	CoordT p, vec3 mp, MaterialContext matCtx,
+	Light light, int lightIndex, float shadedLevel,
+	float roughness, float metallic,
+	vec3 albedo
+) {
+	#ifdef THIS_EXPOSE_lightcolor
+	THIS_lightcolor = light.color;
+	#endif
+	#ifdef THIS_EXPOSE_lightpos
+	THIS_lightpos = light.pos;
+	#endif
+	#ifdef THIS_EXPOSE_shadedlevel
+	{
+		THIS_shadedlevel = 1.;
+		#ifdef RAYTK_USE_SHADOW
+		if (matCtx.result.useShadow) {
+			THIS_shadedlevel = shadedLevel;
+		}
+		#endif
+	}
+	#endif
+
+	vec3 F0 = vec3(0.04);					// Base normal incidence for
+	// non-conductors (average of some materials)
+	F0 = mix(F0,albedo,metallic);	// If it is a metal, take the normal incidence (color)
+	// from the albedo as metals should not have albedo color
+
+	// TODO: CLEANUP ALIASES
+	vec3 eye = normalize(matCtx.ray.pos - p);
+	vec3 n = matCtx.normal;
+	vec3 r = reflect(-eye,n);
+	float ndv = max(dot(n,eye),0.0);
+
+	#ifdef THIS_EXPOSE_lightcolor
+	THIS_lightcolor = light.color;
+	#endif
+	#ifdef THIS_EXPOSE_lightpos
+	THIS_lightpos = light.pos;
+	#endif
+
+	vec3 lp = light.pos;
+	vec3 lc = light.color;
+	vec3 ld = normalize(lp - p);
+	vec3 h = normalize(ld + eye);
+	float ndl = max(dot(n,ld),0.0);
+	float ndh = max(dot(n,h),0.0);
+
+	vec3 finalCol = vec3(0.);
+
+	{
+		// Diffuse
+		vec3 diffuseBRDF = albedo / PI;
+
+		// Specular
+		float D = pbr_distribution(ndh,roughness);
+		float G = pbr_geometry(ndv,ndl,roughness);
+		vec3 F = pbr_fresnel(ndv,F0);
+
+		vec3 specularBRDFNom = D * G * F;
+		float specularBRDFDenom = 4.0 * max(ndv * ndl, 0.0) + 0.001; 	// add bias to prevent
+		// division by 0
+		vec3 specularBRDF = specularBRDFNom / specularBRDFDenom;
+
+		// Outgoing light can't exced 1
+		vec3 kS = F;
+		vec3 kD = 1.0 - kS;
+		kD *= 1.0 - metallic;
+
+		finalCol = (kD * diffuseBRDF + specularBRDF);
+		finalCol = finalCol * ndl * lc;
+	}
+
+	{
+		// IBL
+		vec3 F = pbr_fresnelRoughness(ndv,F0,roughness);
+		vec3 kS = F;
+		vec3 kD = 1.0 - kS;
+		kD *= 1.0 - metallic;
+		vec3 totalIBL = vec3(0.0);
+
+		// Diffuse IBL
+		vec3 irradiance = THIS_irradianceMap(mp, matCtx);
+		vec3 diffuseIBL = irradiance * albedo * kD * 1.0;
+
+		// Specular IBL
+		vec3 reflectance = THIS_reflectanceMap(mp, matCtx, r, roughness, irradiance);
+		vec3 specularIBL = reflectance * F;
+
+		totalIBL = kD * diffuseIBL + specularIBL;
+
+		finalCol += totalIBL;
+	}
+
+	#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
+	finalCol *= shadedLevel;
+	#endif
+	return finalCol;
+}
 
 vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
 	restoreIterationFromMaterial(matCtx, THIS_iterationCapture);
@@ -77,22 +159,6 @@ vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
 
 	#ifdef THIS_EXPOSE_normal
 	THIS_normal = matCtx.normal;
-	#endif
-	#ifdef THIS_EXPOSE_shadedlevel
-	{
-		THIS_shadedlevel = 1.;
-		#ifdef RAYTK_USE_SHADOW
-		if (matCtx.result.useShadow) {
-			THIS_shadedlevel = matCtx.shadedLevel;
-		}
-		#endif
-	}
-	#endif
-	#ifdef THIS_EXPOSE_lightcolor
-	THIS_lightcolor = matCtx.light.color;
-	#endif
-	#ifdef THIS_EXPOSE_lightpos
-	THIS_lightpos = matCtx.light.pos;
 	#endif
 	#ifdef THIS_EXPOSE_surfacecolor
 	{
@@ -112,9 +178,6 @@ vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
 		#endif
 	}
 	#endif
-
-	vec3 lightDir = normalize(p - matCtx.light.pos);
-	vec3 viewDir = normalize(-matCtx.ray.dir);
 
 	float roughness = THIS_Roughness;
 	float metallic = THIS_Metallic;
@@ -138,81 +201,14 @@ vec3 THIS_getColor(vec3 p, MaterialContext matCtx) {
 
 	vec3 albedo = baseColor * THIS_Albedo;
 
-	vec3 F0 = vec3(0.04);					// Base normal incidence for
-	// non-conductors (average of some materials)
-	F0 = mix(F0,albedo,metallic);	// If it is a metal, take the normal incidence (color)
-	// from the albedo as metals should not have albedo color
-
-	// TODO: CLEANUP ALIASES
-	vec3 eye = normalize(matCtx.ray.pos - p);
-	vec3 n = matCtx.normal;
-	vec3 r = reflect(-eye,n);
-	float ndv = max(dot(n,eye),0.0);
-
-	const int kLights = 1;
-	float sAcum = float(kLights);
-	vec3 acum = vec3(0.0);
-
-	// TODO clean up loop?
-	for (int i = 0; i < kLights; i++) {
-		// Per-light parameters
-//		vec3 lp = lights[i].Position;
-		vec3 lp = matCtx.light.pos;
-		vec3 ld = normalize(lp - p);
-		vec3 h = normalize(ld + eye);
-		float ndl = max(dot(n,ld),0.0);
-		float ndh = max(dot(n,h),0.0);
-
-		// Diffuse
-		vec3 diffuseBRDF = albedo / PI;
-
-		// Specular
-		float D = pbr_distribution(ndh,roughness);
-		float G = pbr_geometry(ndv,ndl,roughness);
-		vec3 F = pbr_fresnel(ndv,F0);
-
-		vec3 specularBRDFNom = D * G * F;
-		float specularBRDFDenom = 4.0 * max(ndv * ndl, 0.0) + 0.001; 	// add bias to prevent
-		// division by 0
-		vec3 specularBRDF = specularBRDFNom / specularBRDFDenom;
-
-		// Outgoing light can't exced 1
-		vec3 kS = F;
-		vec3 kD = 1.0 - kS;
-		kD *= 1.0 - metallic;
-
-		vec3 finalCol = (kD * diffuseBRDF + specularBRDF);
-		finalCol = finalCol * ndl * matCtx.light.color;
-		acum += finalCol;
-
-		// Shadow
-//		vec3 sDir = normalize(lp - p);
-//		sAcum -= Shadow(p,sDir);
-		#if defined(THIS_Enableshadow) && defined(RAYTK_USE_SHADOW)
-		sAcum *= matCtx.shadedLevel;
-		#endif
+	vec3 col = vec3(0.);
+	#if RAYTK_LIGHT_COUNT > 1
+	for (int i = 0; i < RAYTK_LIGHT_COUNT; i++) {
+		col += THIS_getColorForLight(p, mp, matCtx, matCtx.allLights[i], i, matCtx.allShadedLevels[i], roughness, metallic, albedo);
 	}
-
-	// IBL
-	vec3 F = pbr_fresnelRoughness(ndv,F0,roughness);
-	vec3 kS = F;
-	vec3 kD = 1.0 - kS;
-	kD *= 1.0 - metallic;
-	vec3 totalIBL = vec3(0.0);
-
-	// Diffuse IBL
-	vec3 irradiance = THIS_irradianceMap(mp, matCtx);
-	vec3 diffuseIBL = irradiance * albedo * kD * 1.0;
-
-	// Specular IBL
-	vec3 reflectance = THIS_reflectanceMap(mp, matCtx, r, roughness, irradiance);
-	vec3 specularIBL = reflectance * F;
-
-	totalIBL = kD * diffuseIBL + specularIBL;
-
-	// Shadowing
-	float finalShadow = max(sAcum / float(kLights),0.02);
-
-	return (acum + totalIBL) * max(finalShadow,0.15);
+	#else
+	col = THIS_getColorForLight(p, mp, matCtx, matCtx.light, 0, matCtx.shadedLevel, roughness, metallic, albedo);
+	#endif
+	return col;
 }
 
