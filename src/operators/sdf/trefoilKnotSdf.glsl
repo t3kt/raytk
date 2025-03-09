04f2cb3b0ddd9573654215ec7cf7e2e120fa9aa9
tekt <tekt@immerse.studio>
2025-03-09 16:00:00 -0700

    added trefoilKnotSdf
    close #1285

---
+++ src/operators/sdf/trefoilKnotSdf.glsl
@@ -0,0 +1,27 @@
+ReturnT thismap(CoordT p, ContextT ctx) {
+	CoordT p0 = p;
+	switch (THIS_Axis) {
+		case THISTYPE_Axis_x: p = p.zyx; break;
+		case THISTYPE_Axis_y: p = p.xzy; break;
+		case THISTYPE_Axis_z: p = p.yxz; break;
+	}
+
+	#ifdef THIS_EXPOSE_angle
+	THIS_angle = degrees(atan(p.y, p.x)) + 180.;
+	#endif
+	#ifdef THIS_EXPOSE_normangle
+	THIS_normangle = atan(p.y, p.x)/TAU + .5;
+	#endif
+
+	#ifdef THIS_HAS_INPUT_radiusField
+	float r = inputOp_radiusField(p0, ctx);
+	#else
+	float r = THIS_Radius;
+	#endif
+	#ifdef THIS_HAS_INPUT_thicknessField
+	float th = inputOp_thicknessField(p0, ctx);
+	#else
+	float th = THIS_Thickness;
+	#endif
+	return createSdf(sdTrefoilKnot(p, r, th));
+}
---
+++ src/operators/sdf/trefoilKnotSdf.md
@@ -0,0 +1,22 @@
+Trefoil knot SDF.
+
+Based on [Trefoil Knot Explained](https://www.shadertoy.com/view/M3tXW4) by ruudhelderman.
+
+## Parameters
+
+* `Axis`
+  * `x`
+  * `y`
+  * `z`
+* `Radius`
+* `Thickness`
+
+## Inputs
+
+* `radiusField`: 
+* `thicknessField`: 
+
+## Variables
+
+* `angle`: 
+* `normangle`: 
