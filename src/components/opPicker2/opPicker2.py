bcf9bd7af3a57161712e827bee673d549c9b5f7b
tekt <tekt@immerse.studio>
2025-07-28 15:38:21 -0700

    capitalize categories in picker

--- src/components/opPicker2/opPicker2.py
+++ src/components/opPicker2/opPicker2.py
@@ -62,7 +62,7 @@ class OpPicker2:
 			shortName=table[row, 'name'].val,
 			opType=table[row, 'opType'].val,
 			path=table[row, 'path'].val,
-			category=table[row, 'category'].val,
+			category=table[row, 'category'].val.capitalize(),
 			displayCategory=table[row, 'displayCategory'].val or table[row, 'category'].val.capitalize(),
 			isAlpha=table[row, 'status'] == 'alpha',
 			isBeta=table[row, 'status'] == 'beta',
